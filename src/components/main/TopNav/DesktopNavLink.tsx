9387b8abb7196322559ddcbe5ecec4a7cd4d4322
robin <robinnagpal.tiet@gmail.com>
2023-05-16 09:49:53 -0400

    DOM exception fix

--- src/components/main/TopNav/DesktopNavLink.tsx
+++ src/components/main/TopNav/DesktopNavLink.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import styled from 'styled-components';
 
-const StyledNavLink = styled(Link)<{ isActive: boolean }>`
-  border-bottom: ${(props) => (props.isActive ? '3px solid var(--primary-color)' : '')};
-  color: ${(props) => (props.isActive ? 'var(--primary-color)' : 'var(--text-color)')};
+const StyledNavLink = styled(Link)<{ $isActive: boolean }>`
+  border-bottom: ${(props) => (props.$isActive ? '3px solid var(--primary-color)' : '')};
+  color: ${(props) => (props.$isActive ? 'var(--primary-color)' : 'var(--text-color)')};
 `;
 
 export function DesktopNavLink({ label, href }: { label: string; isActive?: boolean; href: string }) {
@@ -13,7 +13,7 @@ export function DesktopNavLink({ label, href }: { label: string; isActive?: bool
   return (
     <StyledNavLink
       href={href}
-      isActive={isActive}
+      $isActive={isActive}
       className={
         isActive
           ? 'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium text-gray-900'
