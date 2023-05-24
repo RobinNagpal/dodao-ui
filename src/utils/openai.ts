1f43d7e4bc234190f0d0bda7d8808b7dacff7b66
robin <robinnagpal.tiet@gmail.com>
2023-05-24 16:01:40 -0400

    remove extra code

--- src/utils/openai.ts
+++
@@ -1,8 +0,0 @@
-const { Configuration, OpenAIApi } = require("openai");
-
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-export default openai
