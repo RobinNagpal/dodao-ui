28dcd8a99a719bd964ffeb8686f280d59bfb576a
robin <robinnagpal.tiet@gmail.com>
2023-05-24 19:20:57 -0400

    Add test academy

--- src/types/spaceFeatures.ts
+++ src/types/spaceFeatures.ts
@@ -51,6 +51,43 @@ export const compoundFeatures: FeatureItem[] = [
     },
   },
 ];
+export const testAcademyFeatures: FeatureItem[] = [
+  {
+    featureName: FeatureName.Guides,
+    enabled: true,
+    details: {
+      priority: 90,
+    },
+  },
+  {
+    featureName: FeatureName.Bytes,
+    enabled: true,
+    details: {
+      priority: 80,
+    },
+  },
+  {
+    featureName: FeatureName.Simulations,
+    enabled: true,
+    details: {
+      priority: 70,
+    },
+  },
+  {
+    featureName: FeatureName.Timelines,
+    enabled: true,
+    details: {
+      priority: 60,
+    },
+  },
+  {
+    featureName: FeatureName.Courses,
+    enabled: true,
+    details: {
+      priority: 50,
+    },
+  },
+];
 
 export const uniswapFeatures: FeatureItem[] = [
   {
--- src/utils/features.ts
+++ src/utils/features.ts
@@ -1,4 +1,4 @@
-import { compoundFeatures, dodaoFeatures, FeatureItem, FeatureName, uniswapFeatures } from '@/types/spaceFeatures';
+import { compoundFeatures, dodaoFeatures, FeatureItem, FeatureName, testAcademyFeatures, uniswapFeatures } from '@/types/spaceFeatures';
 
 export function getFeaturesArray(spaceId: string): FeatureItem[] {
   if (spaceId === 'dodao-academy-eth-1') {
@@ -7,6 +7,10 @@ export function getFeaturesArray(spaceId: string): FeatureItem[] {
 
   if (spaceId === 'compound-eth-1') {
     return compoundFeatures;
+  }
+
+  if (spaceId === 'test-academy-eth') {
+    return testAcademyFeatures;
   }
 
   if (spaceId === 'uniswap-eth-1') {
