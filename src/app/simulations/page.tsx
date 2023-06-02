8edea5e99eebb20f18e8346928bc8e61241e16e3
robin <robinnagpal.tiet@gmail.com>
2023-06-02 13:48:00 -0400

    Add code to show simulations

---
+++ src/app/simulations/page.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import withSpace, { SpaceProps } from '@/app/withSpace';
+import Block from '@/components/app/Block';
+import RowLoading from '@/components/app/RowLoading';
+import SimulationSummaryCard from '@/components/simulation/Simulations/SimulationSummaryCard';
+import NoSimulation from '@/components/simulation/Simulations/NoSimulations';
+import { Grid4Cols } from '@/components/core/grids/Grid4Colst';
+import PageWrapper from '@/components/core/page/PageWrapper';
+import { useSimulationsQuery } from '@/graphql/generated/generated-types';
+import React from 'react';
+
+function Simulation({ space }: SpaceProps) {
+  const { data, error, loading, refetch: fetchSimulations } = useSimulationsQuery({ variables: { spaceId: space.id } });
+
+  const loadingData = loading || !space;
+  return (
+    <PageWrapper>
+      {!data?.simulations.length && !loadingData && <NoSimulation />}
+      {!!data?.simulations?.length && (
+        <Grid4Cols>
+          {data?.simulations?.map((simulation, i) => (
+            <SimulationSummaryCard key={i} simulation={simulation} />
+          ))}
+        </Grid4Cols>
+      )}
+      <div style={{ height: '10px', width: '10px', position: 'absolute' }} />
+      {loadingData && (
+        <Block slim={true}>
+          <RowLoading className="my-2" />
+        </Block>
+      )}
+    </PageWrapper>
+  );
+}
+
+export default withSpace(Simulation);
