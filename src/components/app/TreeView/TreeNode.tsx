82ea7ba3c2efba10cea42a5ceef956ffaf3b82f7
TanayK07 <tkedia7@gmail.com>
2023-06-16 02:40:26 +0530

    Navigation Fix in progress

--- src/components/app/TreeView/TreeNode.tsx
+++ src/components/app/TreeView/TreeNode.tsx
@@ -1,5 +1,4 @@
-// TreeNode.tsx
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { ChevronDownIcon, ChevronRightIcon } from '@heroicons/react/20/solid';
 
 export interface TreeNodeType {
@@ -9,32 +8,41 @@ export interface TreeNodeType {
 
 interface TreeNodeProps {
   node: TreeNodeType;
-  openNodes: { [key: string]: string };
-  setOpenNodes: React.Dispatch<React.SetStateAction<{ [key: string]: string }>>;
+  openNodes: { [key: string]: boolean };
+  setOpenNodes: React.Dispatch<React.SetStateAction<{ [key: string]: boolean }>>;
   level: number;
+  isOpen: boolean; // Add isOpen prop
 }
 
-export const TreeNode: FC<TreeNodeProps> = ({ node, openNodes, setOpenNodes, level }) => {
+export const TreeNode: FC<TreeNodeProps> = ({ node, openNodes, setOpenNodes, level, isOpen }) => {
   const handleClick = () => {
-    if (openNodes[level] === (node.component.key as string)) {
-      setOpenNodes({ ...openNodes, [level]: '' });
-    } else {
-      setOpenNodes({ ...openNodes, [level]: node.component.key as string });
-    }
+    setOpenNodes((prevOpenNodes) => ({
+      ...prevOpenNodes,
+      [node.component.key as string]: !prevOpenNodes[node.component.key as string],
+    }));
   };
-
-  const isOpen = openNodes[level] === (node.component.key as string);
 
   return (
     <div className="px-4 py-2">
       <div className="flex items-center cursor-pointer" onClick={handleClick}>
-        {node.children ? isOpen ? <ChevronDownIcon className="h-5 w-5 mr-2" /> : <ChevronRightIcon className="h-5 w-5 mr-2" /> : null}
+        {node.children ? isOpen ? (
+          <ChevronDownIcon className="h-5 w-5 mr-2" />
+        ) : (
+          <ChevronRightIcon className="h-5 w-5 mr-2" />
+        ) : null}
         {node.component}
       </div>
       {isOpen && node.children && (
         <div className="ml-4">
           {node.children.map((childNode) => (
-            <TreeNode key={childNode.component.key as string} node={childNode} openNodes={openNodes} setOpenNodes={setOpenNodes} level={level + 1} />
+            <TreeNode
+              key={childNode.component.key as string}
+              node={childNode}
+              openNodes={openNodes}
+              setOpenNodes={setOpenNodes}
+              level={level + 1}
+              isOpen={openNodes[childNode.component.key as string] || false} // Pass isOpen prop to sub-nodes
+            />
           ))}
         </div>
       )}
