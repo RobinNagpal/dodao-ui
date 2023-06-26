83fb116f465d9d829f55e00ad082bd0148037455
robin <robinnagpal.tiet@gmail.com>
2023-06-26 10:32:57 -0400

    add badge on enter

--- src/components/core/badge/UpsertBadgeInput.tsx
+++ src/components/core/badge/UpsertBadgeInput.tsx
@@ -1,7 +1,7 @@
 import BadgeWithRemove from '@/components/core/badge/BadgeWithRemove';
 import Button from '@/components/core/buttons/Button';
 import Input from '@/components/core/input/Input';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 export interface Badges {
   id: string;
@@ -13,23 +13,24 @@ export interface UpsertBadgeItemsProps {
   badges: Badges[];
   onAdd: (label: string) => void;
   onRemove: (id: string) => void;
+  className?: string;
 }
 
 export default function UpsertBadgeInput(props: UpsertBadgeItemsProps) {
-  const [domain, setDomain] = useState<string>('');
+  const [newBadge, setNewBadge] = useState<string>('');
+
+  function addBadge(e?: FormEvent<HTMLFormElement>) {
+    props.onAdd(newBadge);
+    setNewBadge('');
+    e?.preventDefault();
+    return false;
+  }
+
   return (
-    <div>
+    <form className={`${props.className ? props.className : ' '}`} onSubmit={addBadge} onSubmitCapture={addBadge}>
       <div className="flex w-full items-end mb-2">
-        <Input label={props.label} onUpdate={(v) => setDomain(v?.toString() || '')} modelValue={domain} className="grow" />
-        <Button
-          onClick={() => {
-            props.onAdd(domain);
-            setDomain('');
-          }}
-          className="ml-2 grow-0 w-16"
-          variant="contained"
-          primary
-        >
+        <Input label={props.label} onUpdate={(v) => setNewBadge(v?.toString() || '')} modelValue={newBadge} className="grow" />
+        <Button onClick={addBadge} className="ml-2 grow-0 w-16" variant="contained" primary>
           Add
         </Button>
       </div>
@@ -37,6 +38,6 @@ export default function UpsertBadgeInput(props: UpsertBadgeItemsProps) {
       {props.badges.map((badge) => (
         <BadgeWithRemove key={badge.id} id={badge.id} label={badge.label} onRemove={props.onRemove} />
       ))}
-    </div>
+    </form>
   );
 }
