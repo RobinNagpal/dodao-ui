a3779a1b4b6a486e48f64c9173bca96c4a82bc3b
Tanayk07 <tkedia7@gmail.com>
2023-07-08 01:35:08 +0530

    minor changes to be reviewed again

--- src/components/timelines/View/TimelineDetails.tsx
+++ src/components/timelines/View/TimelineDetails.tsx
@@ -1,90 +1,37 @@
-import CalendarIcon from '@/components/core/icons/CalendarIcon';
-import Button from '@/components/core/buttons/Button';
 import { Space, TimelineDetailsFragment } from '@/graphql/generated/generated-types';
-import styled from 'styled-components';
-import { Fragment, useState } from 'react';
-import { CheckCircleIcon } from '@heroicons/react/24/solid';
-import { FaceFrownIcon, FaceSmileIcon, FireIcon, HandThumbUpIcon, HeartIcon, PaperClipIcon, XMarkIcon } from '@heroicons/react/20/solid';
-import { Listbox, Transition } from '@headlessui/react';
 import moment from 'moment';
-
 interface TimelineProps {
   space: Space;
   timeline: TimelineDetailsFragment;
   inProgress?: boolean;
 }
-const TimelineList = styled.ol`
-  border-left: 2px solid var(--primary-color);
-`;
-const TimelineItem = styled.li`
-  margin-left: 10px;
-  margin-bottom: 10px;
-  display: flex;
-  align-items: flex-start;
-`;
-const TimelineDateIcon = styled.div`
-  background-color: var(--primary-color);
-  width: 15px;
-  height: 15px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 50%;
-  margin-left: -18px;
-  min-width: 15px;
-  box-shadow: 0 0 0 4px var(--bg-color);
-`;
-const TimelineContent = styled.div`
-  padding: 20px 20px 20px 20px;
-  margin-left: 20px;
-  width: max-content;
-  margin-bottom: 10px;
-  border-radius: 8px;
-  border: 1px solid var(--border-color);
-  background-color: var(--bg-color);
-  h1 {
-    margin-bottom: 1px;
-  }
-`;
-const CircleCSS = styled.div`
-  .circle {
-    background-color: #f3f4f6;
-    border: 4px solid #d6d7db;
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-`;
 const Timeline = ({ timeline }: TimelineProps) => {
   return (
     <>
       <ul role="list" className="space-y-6">
         {timeline.events.map((event, i) => {
           const timeAgo = moment(event.date).local().startOf('seconds').fromNow();
-
           return (
-            <li key={i} className="relative flex gap-x-4">
-              <div className="absolute left-0 top-0 flex w-6 justify-center -bottom-6">
+            <li key={i} className="relative flex gap-x-4 items-center">
+              <div className="absolute left-0 top-0 flex w-5 justify-center -bottom-6 mr-4 z-10">
                 <div className="w-px bg-gray-200"></div>
               </div>
-              <li className="relative flex gap-x-4">
-                <div className="absolute left-0 top-0 flex w-6 justify-center -bottom-6">
-                  <div className="w-px bg-gray-200"></div>
+              <div className="flex items-center">
+                <div className="rounded-full bg-white h-8 w-5 flex items-center justify-center rounded-full relative z-20">
+                  <div className="rounded-full bg-gray-200 ring-1 ring-gray-400 h-3 w-3"></div>
                 </div>
-                <div className="rounded-full bg-gray-120 ring-1 ring-gray-300 mt-3 h-6 w-6 flex-none rounded-full"></div>
-                <div className="flex-auto rounded-md p-3 ring-1 ring-inset ring-gray-200">
-                  <div className="flex justify-between gap-x-4">
-                    <div className="py-0.5 text-xs leading-5 text-gray-500">
-                      <span className="font-medium text-gray-900">{event.name}</span>
-                    </div>
-                    <time dateTime={event.date} className="flex-none py-0.5 text-xs leading-5 text-gray-500">
-                      {timeAgo}
-                    </time>
+              </div>
+              <div className="flex-auto rounded-md ring-1 ring-inset ring-gray-200">
+                <div className="flex justify-between gap-x-4">
+                  <div className="text-xs leading-5 text-gray-500 p-4 pb-0">
+                    <span className="font-medium text-lg text-gray-900">{event.name}</span>
                   </div>
-                  <p className="text-sm leading-6 text-gray-500">{event.content}</p>
+                  <time dateTime={event.date} className="flex-none  p-4  pb-0 text-xs leading-5 text-gray-500">
+                    {timeAgo}
+                  </time>
                 </div>
-              </li>
+                <p className=" p-4 pt-2 text-sm leading-6 text-gray-500">{event.content}</p>
+              </div>
             </li>
           );
         })}
@@ -92,5 +39,4 @@ const Timeline = ({ timeline }: TimelineProps) => {
     </>
   );
 };
-
 export default Timeline;
