64fe28a2a8f59942ab6b9848d97942f25239f8a5
Tanayk07 <tkedia7@gmail.com>
2023-07-08 01:51:46 +0530

    added bg-color var

--- src/components/timelines/View/TimelineDetails.tsx
+++ src/components/timelines/View/TimelineDetails.tsx
@@ -13,18 +13,18 @@ const Timeline = ({ timeline }: TimelineProps) => {
           const timeAgo = moment(event.date).local().startOf('seconds').fromNow();
           return (
             <li key={i} className="relative flex gap-x-4 items-center">
-              <div className="absolute left-0 top-0 flex w-5 justify-center -bottom-6 mr-4 z-10">
+              <div className="absolute left-0 top-0 flex w-5 justify-center -bottom-6 mr-4 ">
                 <div className="w-px bg-gray-200"></div>
               </div>
               <div className="flex items-center">
-                <div className="rounded-full bg-white h-8 w-5 flex items-center justify-center rounded-full relative z-20">
+                <div className="rounded-full bg-var(--bg--color) h-8 w-5 flex items-center justify-center rounded-full relative z-10">
                   <div className="rounded-full bg-gray-200 ring-1 ring-gray-400 h-3 w-3"></div>
                 </div>
               </div>
               <div className="flex-auto rounded-md ring-1 ring-inset ring-gray-200">
                 <div className="flex justify-between gap-x-4">
                   <div className="text-xs leading-5 text-gray-500 p-4 pb-0">
-                    <span className="font-medium text-lg text-gray-900">{event.name}</span>
+                    <span className="font-medium text-lg ">{event.name}</span>
                   </div>
                   <time dateTime={event.date} className="flex-none  p-4  pb-0 text-xs leading-5 text-gray-500">
                     {timeAgo}
