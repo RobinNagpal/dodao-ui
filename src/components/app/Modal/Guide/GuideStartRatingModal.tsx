abc5f968322935233a65cfad04235dcf21f82181
robin <robinnagpal.tiet@gmail.com>
2023-07-10 03:53:54 -0400

    remove extra icons

--- src/components/app/Modal/Guide/GuideStartRatingModal.tsx
+++ src/components/app/Modal/Guide/GuideStartRatingModal.tsx
@@ -1,8 +1,7 @@
+import EmojiRatings from '@/components/app/Rating/EmojiRatings';
+import CustomButton from '@/components/core/buttons/Button';
 import FullScreenModal from '@/components/core/modals/FullScreenModal';
 import styled from 'styled-components';
-import { useState } from 'react';
-import CustomButton from '@/components/core/buttons/Button';
-import Image from 'next/image';
 
 export interface GuideStartRatingModalProps {
   open: boolean;
@@ -11,81 +10,27 @@ export interface GuideStartRatingModalProps {
   setStartRating: (rating: number) => void;
 }
 
-const RatingButton = styled.button`
-  :hover {
-    border: 1px solid #00AD79; 
-    background-color: #222;
-  }
-  
-    display: inline-block;
-    padding: 10px 20px;
-    background-color: #000;
-    color: #fff;
-    border-radius: 5px;
-    border: none;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3), 0 1px 2px rgba(0, 0, 0, 0.2);
-    background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.1));
-  
-  :active {
-    background-color: #444;
-    box-shadow: inset 0 2px 5px rgba(0, 0, 0, 0.3), inset 0 1px 2px rgba(0, 0, 0, 0.2);
-  }
-  
-  &.selected {
-    background-color: var(--primary-color);
-
-`;
 const ModalContent = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  border: 2px solid white;
-  padding: 10px;
-  background-color: #0d131a;
 `;
 
 export default function GuideStartRatingModal({ open, onClose, skipStartRating, setStartRating }: GuideStartRatingModalProps) {
-  const ratings = [
-    { number: 1, label: 'sad' },
-    { number: 2, label: 'neutral' },
-    { number: 3, label: 'smile' },
-    { number: 4, label: 'smiling' },
-    { number: 5, label: 'superstar' },
-  ];
-
-  const [selectedRating, setSelectedRating] = useState<number | null>(null);
-
   return (
     <FullScreenModal open={open} onClose={onClose} title="">
       <ModalContent>
         <div className=" flex flex-col justify-center items-center  text-center  h-[300px]">
-          <h1 className="text-2xl font-semibold leading-6 text-gray-200 mb-8">How well do you know this topic ? </h1>
-          <p>Tell us about the level of knowledge you have about the guide</p>
+          <h1 className="text-2xl font-semibold leading-6 text-gray-200 mb-8">How well do you know this topic currently? </h1>
           <div className={`mt-4 flex flex-col items-center`}>
-            <div className={`flex justify-center`}>
-              {ratings.map(({ number, label }) => (
-                <RatingButton
-                  key={number}
-                  type="button"
-                  className={`inline-flex items-center justify-center mr-3 text-3xl  hover:scale-110 transition-transform duration-200  ${
-                    selectedRating === number ? 'selected' : ''
-                  }`}
-                  onClick={() => {
-                    setStartRating(number);
-                    setSelectedRating(number);
-                  }}
-                >
-                  <Image src={`/${label}.png`} width={50} height={60} alt={label} />
-                </RatingButton>
-              ))}
-            </div>
+            <EmojiRatings selectRating={(rating) => setStartRating(rating)} />
             <CustomButton
               className="mt-5 w-[30%] flex justify-center items-center "
               onClick={() => {
-                skipStartRating;
+                skipStartRating();
               }}
             >
-              Skip{' '}
+              Skip
             </CustomButton>
           </div>
         </div>
---
+++ src/components/app/Rating/EmojiRatings.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+const RatingButton = styled.button`
+  :hover {
+    background-color: var(--primary-color);
+  }
+
+  
+  &.selected {
+    background-color: var(--primary-color);
+
+`;
+
+export interface EmojiRatingsProps {
+  selectRating: (rating: number) => void;
+}
+export default function EmojiRatings(props: EmojiRatingsProps) {
+  const ratings = [
+    { number: 1, label: '🙁' },
+    { number: 2, label: '😕' },
+    { number: 3, label: '😐' },
+    { number: 4, label: '😄' },
+    { number: 5, label: '😊' },
+  ];
+
+  return (
+    <div className={`flex justify-center`}>
+      {ratings.map(({ number, label }) => (
+        <RatingButton
+          key={number}
+          type="button"
+          className={`inline-flex items-center justify-center w-20 h-16 text-3xl rounded-full hover:scale-110 transition-transform duration-200`}
+          onClick={() => props.selectRating(number)}
+        >
+          {label}
+        </RatingButton>
+      ))}
+    </div>
+  );
+}
