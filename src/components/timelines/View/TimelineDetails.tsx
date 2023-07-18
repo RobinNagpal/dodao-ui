5af59a00bd4c7833e840ad7149bfa292f222b27f
robin <robinnagpal.tiet@gmail.com>
2023-07-18 11:55:26 -0400

    Fix showing of event summary

--- src/components/timelines/View/TimelineDetails.tsx
+++ src/components/timelines/View/TimelineDetails.tsx
@@ -3,6 +3,8 @@
 import CalendarIcon from '@/components/core/icons/CalendarIcon';
 import Button from '@/components/core/buttons/Button';
 import { Space, TimelineDetailsFragment } from '@/graphql/generated/generated-types';
+import { getMarkedRenderer } from '@/utils/ui/getMarkedRenderer';
+import { marked } from 'marked';
 import styled from 'styled-components';
 
 interface TimelineProps {
@@ -45,27 +47,32 @@ const TimelineContent = styled.div`
   }
 `;
 const Timeline = ({ timeline }: TimelineProps) => {
+  const renderer = getMarkedRenderer();
+
   return (
     <TimelineList>
-      {timeline.events.map((event, i) => (
-        <TimelineItem key={i}>
-          <TimelineDateIcon>
-            <CalendarIcon />
-          </TimelineDateIcon>
-          <TimelineContent>
-            <div className={event.moreLink ? 'mb-4' : ''}>
-              <h1 className="mb-1 text-lg font-semibold">{event.title}</h1>
-              <h3 className="block mb-2 text-sm font-normal leading-none">{new Date(event.date).toISOString().split('T')[0]}</h3>
-              <p className="text-base font-normal">{event.summary}</p>
-            </div>
-            {event.moreLink && (
-              <a href={event.moreLink} target="_blank" rel="noopener noreferrer">
-                <Button primary>Learn More</Button>
-              </a>
-            )}
-          </TimelineContent>
-        </TimelineItem>
-      ))}
+      {timeline.events.map((event, i) => {
+        const eventSummary = marked.parse(event.summary, { renderer });
+        return (
+          <TimelineItem key={i}>
+            <TimelineDateIcon>
+              <CalendarIcon />
+            </TimelineDateIcon>
+            <TimelineContent>
+              <div className={event.moreLink ? 'mb-4' : ''}>
+                <h1 className="mb-1 text-lg font-semibold">{event.title}</h1>
+                <h3 className="block mb-2 text-sm font-normal leading-none">{new Date(event.date).toISOString().split('T')[0]}</h3>
+                <p className="text-base font-normal markdown-body" dangerouslySetInnerHTML={{ __html: eventSummary }} />
+              </div>
+              {event.moreLink && (
+                <a href={event.moreLink} target="_blank" rel="noopener noreferrer">
+                  <Button primary>Learn More</Button>
+                </a>
+              )}
+            </TimelineContent>
+          </TimelineItem>
+        );
+      })}
     </TimelineList>
   );
 };
