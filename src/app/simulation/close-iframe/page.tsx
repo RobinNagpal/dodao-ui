244c5adfe879f74b3ae5ec7d97907dbd07127b27
robin <robinnagpal.tiet@gmail.com>
2023-07-19 18:50:52 -0400

    Add close modal option

--- src/app/simulation/close-iframe/page.tsx
+++ src/app/simulation/close-iframe/page.tsx
@@ -3,19 +3,11 @@
 import { useEffect } from 'react';
 
 export default function CloseTimeline() {
-  function inIframe() {
-    try {
-      return window.self !== window.top;
-    } catch (e) {
-      return true;
-    }
-  }
   useEffect(() => {
-    if (inIframe()) {
+    window.close();
+    setTimeout(() => {
       window.close();
-    } else {
-      window.location.href = '/simulations';
-    }
+    }, 2000);
   }, []);
   return null;
 }
