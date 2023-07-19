92229e00e3134ec2b18f0eabf741d9876e4f0f1f
robin <robinnagpal.tiet@gmail.com>
2023-07-19 18:51:18 -0400

    code nit

--- src/app/simulation/close-iframe/page.tsx
+++ src/app/simulation/close-iframe/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react';
 
-export default function CloseTimeline() {
+export default function CloseSimulation() {
   useEffect(() => {
     window.close();
     setTimeout(() => {
