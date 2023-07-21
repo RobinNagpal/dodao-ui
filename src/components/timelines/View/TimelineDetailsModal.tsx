bd0c299ba9d53204e12775eb27dbb10740f51ae8
robin <robinnagpal.tiet@gmail.com>
2023-07-21 07:06:35 -0400

    Cleanups

--- src/components/timelines/View/TimelineDetailsModal.tsx
+++ src/components/timelines/View/TimelineDetailsModal.tsx
@@ -20,15 +20,10 @@ export default function TimelineDetailsModal({ event, open, onClose }: TimelineD
 
   const renderer = getMarkedRenderer();
   const eventDetails = marked.parse(fullDetails, { renderer });
-  const eventSummary = marked.parse(event.summary, { renderer });
 
   return (
     <FullScreenModal open={open} onClose={onClose} title={<LargeTitle>{event.title} Details</LargeTitle>}>
-      <p className="markdown-body mt-4 font-semibold  text-2xl text-left">Audit Details </p>
-
-      <div className="markdown-body  text-sm text-left" dangerouslySetInnerHTML={{ __html: eventSummary }} />
-
-      <div className="markdown-body mt-4  text-sm text-left" dangerouslySetInnerHTML={{ __html: eventDetails }} />
+      <div className="markdown-body p-4 text-sm text-left" dangerouslySetInnerHTML={{ __html: eventDetails }} />
     </FullScreenModal>
   );
 }
