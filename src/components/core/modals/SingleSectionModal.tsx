59fa66cba2eced5708d843a7a70bff08237a080f
robin <robinnagpal.tiet@gmail.com>
2023-07-31 20:17:31 -0400

    Add borders

--- src/components/core/modals/SingleSectionModal.tsx
+++ src/components/core/modals/SingleSectionModal.tsx
@@ -43,7 +43,7 @@ export default function SingleSectionModal({ open, title, children, onClose, sho
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+              <Dialog.Panel className="border border-gray-200 relative transform overflow-hidden rounded-lg px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
                 <div>
                   {showCloseButton && (
                     <div className="flex justify-end">
