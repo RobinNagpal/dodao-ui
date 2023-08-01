f883152370b774083ef68746b9649dec708880a8
robin <robinnagpal.tiet@gmail.com>
2023-07-31 20:25:13 -0400

    Fix prettier

--- src/types/features/compoundFeatures.ts
+++ src/types/features/compoundFeatures.ts
@@ -1,39 +1,39 @@
-import { FeatureItem, FeatureName } from "@/types/features/spaceFeatures";
+import { FeatureItem, FeatureName } from '@/types/features/spaceFeatures';
 
 export const compoundFeatures: FeatureItem[] = [
   {
     featureName: FeatureName.Guides,
     enabled: true,
     details: {
-      priority: 90
-    }
+      priority: 90,
+    },
   },
   {
     featureName: FeatureName.Bytes,
     enabled: true,
     details: {
-      priority: 80
-    }
+      priority: 80,
+    },
   },
   {
     featureName: FeatureName.Simulations,
     enabled: true,
     details: {
-      priority: 70
-    }
+      priority: 70,
+    },
   },
   {
     featureName: FeatureName.Timelines,
     enabled: true,
     details: {
-      priority: 60
-    }
+      priority: 60,
+    },
   },
   {
     featureName: FeatureName.Courses,
     enabled: true,
     details: {
-      priority: 50
-    }
-  }
+      priority: 50,
+    },
+  },
 ];
