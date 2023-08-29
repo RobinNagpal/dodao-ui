b47d061f4e460920b637fcebd432483339b57560
robin <robinnagpal.tiet@gmail.com>
2023-08-29 17:32:43 -0400

    Add chatbot files

---
+++ src/chatbot/home/home.state.tsx
@@ -0,0 +1,54 @@
+import { Conversation, Message } from '@/chatbot/types/chat';
+import { ErrorMessage } from '@/chatbot/types/error';
+import { FolderInterface } from '@/chatbot/types/folder';
+import { OpenAIModel, OpenAIModelID } from '@/chatbot/types/openai';
+import { PluginKey } from '@/chatbot/types/plugin';
+import { Prompt } from '@/chatbot/types/prompt';
+
+export interface HomeInitialState {
+  apiKey: string;
+  pluginKeys: PluginKey[];
+  loading: boolean;
+  lightMode: 'light' | 'dark';
+  messageIsStreaming: boolean;
+  modelError: ErrorMessage | null;
+  models: OpenAIModel[];
+  folders: FolderInterface[];
+  conversations: Conversation[];
+  selectedConversation: Conversation | undefined;
+  currentMessage: Message | undefined;
+  prompts: Prompt[];
+  temperature: number;
+  showChatbar: boolean;
+  showPromptbar: boolean;
+  currentFolder: FolderInterface | undefined;
+  messageError: boolean;
+  searchTerm: string;
+  defaultModelId: OpenAIModelID | undefined;
+  serverSideApiKeyIsSet: boolean;
+  serverSidePluginKeysSet: boolean;
+}
+
+export const initialState: HomeInitialState = {
+  apiKey: '',
+  loading: false,
+  pluginKeys: [],
+  lightMode: 'dark',
+  messageIsStreaming: false,
+  modelError: null,
+  models: [],
+  folders: [],
+  conversations: [],
+  selectedConversation: undefined,
+  currentMessage: undefined,
+  prompts: [],
+  temperature: 1,
+  showPromptbar: true,
+  showChatbar: true,
+  currentFolder: undefined,
+  messageError: false,
+  searchTerm: '',
+  defaultModelId: undefined,
+  serverSideApiKeyIsSet: true,
+  serverSidePluginKeysSet: false,
+};
---
+++ src/chatbot/types/prompt.ts
@@ -0,0 +1,10 @@
+import { OpenAIModel } from '@/chatbot/types/openai';
+
+export interface Prompt {
+  id: string;
+  name: string;
+  description: string;
+  content: string;
+  model: OpenAIModel;
+  folderId: string | null;
+}
