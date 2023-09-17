856e34c4fddbe022b45e60340f120ba3accfa788
robin <robinnagpal.tiet@gmail.com>
2023-09-17 13:02:55 -0400

    Update analytics ids

--- src/utils/analytics/getGTagId.ts
+++ src/utils/analytics/getGTagId.ts
@@ -2,8 +2,8 @@ import { SpaceWithIntegrationsFragment } from '@/graphql/generated/generated-typ
 
 export function getGTagId(space?: SpaceWithIntegrationsFragment) {
   if (space?.id === 'uniswap-eth-1') {
-    return 'G-BJX2V8FE7L';
-  } else {
     return 'G-GHYGJ5DYRF';
+  } else {
+    return 'G-BJX2V8FE7L';
   }
 }
