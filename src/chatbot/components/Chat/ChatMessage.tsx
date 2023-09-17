5b082803de8b4c45bb8cced4e16246e3242e8787
robin <robinnagpal.tiet@gmail.com>
2023-09-17 07:52:12 -0400

    Some cleanups

--- src/chatbot/components/Chat/ChatMessage.tsx
+++ src/chatbot/components/Chat/ChatMessage.tsx
@@ -112,11 +112,13 @@ export const ChatMessage: FC<Props> = memo(({ message, messageIndex, onEdit }) =
 
   return (
     <div
-      className={`group md:px-4 ${message.role === 'assistant' ? 'border-b border-black/10 dark:border-gray-900/50 ' : 'border-b border-black/10'}`}
+      className={`group px-4 ${message.role === 'assistant' ? 'border-b text-sm markdown-body' : 'border-b text-base'}`}
       style={{ overflowWrap: 'anywhere' }}
     >
-      <div className="relative m-auto flex p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
-        <div className="min-w-[40px] text-right font-bold">{message.role === 'assistant' ? <IconRobot size={30} /> : <IconUser size={30} />}</div>
+      <div className="relative m-auto flex p-4 md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
+        <div className="min-w-[40px] text-right font-bold">
+          {message.role === 'assistant' ? <IconRobot size={30} className="mr-4" /> : <IconUser className="mr-4" size={30} />}
+        </div>
 
         <div className="prose mt-[-2px] w-full dark:prose-invert">
           {message.role === 'user' ? (
