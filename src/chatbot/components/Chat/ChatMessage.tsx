42a277019c1b195e5e22a99af3c0c6c24bb2c6ac
robin <robinnagpal.tiet@gmail.com>
2023-11-29 10:40:31 -0500

    Some cleanups

--- src/chatbot/components/Chat/ChatMessage.tsx
+++ src/chatbot/components/Chat/ChatMessage.tsx
@@ -58,55 +58,57 @@ export const ChatMessage: FC<Props> = memo(({ message, messageIndex, onEdit, onS
           </div>
         </div>
       )}
-      <div className={`group overflow-scroll px-4 text-sm markdown-body`}>
-        <div className="relative m-auto flex p-4 md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
-          <div className={'min-w-[40px] text-right font-bold ' + styles.chatMessageIcons}>
-            <CommandLineIcon width={30} height={30} className="mr-4" />
-          </div>
+      {message.assistantResponse && (
+        <div className={`group overflow-scroll px-4 text-sm markdown-body`}>
+          <div className="relative m-auto flex p-4 md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
+            <div className={'min-w-[40px] text-right font-bold ' + styles.chatMessageIcons}>
+              <CommandLineIcon width={30} height={30} className="mr-4" />
+            </div>
 
-          <div className="prose mt-[-2px] w-full dark:prose-invert">
-            <div className="flex flex-row">
-              <MemoizedReactMarkdown
-                linkTarget="_blank"
-                className="prose dark:prose-invert flex-1"
-                remarkPlugins={[remarkGfm, remarkMath]}
-                components={{
-                  code({ node, inline, className, children, ...props }) {
-                    if (children.length) {
-                      if (children[0] == '▍') {
-                        return <span className="animate-pulse cursor-default mt-1">▍</span>;
+            <div className="prose mt-[-2px] w-full dark:prose-invert">
+              <div className="flex flex-row">
+                <MemoizedReactMarkdown
+                  linkTarget="_blank"
+                  className="prose dark:prose-invert flex-1"
+                  remarkPlugins={[remarkGfm, remarkMath]}
+                  components={{
+                    code({ node, inline, className, children, ...props }) {
+                      if (children.length) {
+                        if (children[0] == '▍') {
+                          return <span className="animate-pulse cursor-default mt-1">▍</span>;
+                        }
+
+                        children[0] = (children[0] as string).replace('`▍`', '▍');
                       }
 
-                      children[0] = (children[0] as string).replace('`▍`', '▍');
-                    }
+                      const match = /language-(\w+)/.exec(className || '');
 
-                    const match = /language-(\w+)/.exec(className || '');
-
-                    return !inline ? (
-                      <CodeBlock key={Math.random()} language={(match && match[1]) || ''} value={String(children).replace(/\n$/, '')} {...props} />
-                    ) : (
-                      <code className={className} {...props}>
-                        {children}
-                      </code>
-                    );
-                  },
-                  table({ children }) {
-                    return <table className="border-collapse border border-black px-3 py-1 dark:border-white">{children}</table>;
-                  },
-                  th({ children }) {
-                    return <th className="break-words border border-black bg-gray-500 px-3 py-1">{children}</th>;
-                  },
-                  td({ children }) {
-                    return <td className="break-words border border-black px-3 py-1 dark:border-white">{children}</td>;
-                  },
-                }}
-              >
-                {`${message.assistantResponse}${messageIsStreaming && messageIndex == (selectedConversation?.messages.length ?? 0) - 1 ? '`▍`' : ''}`}
-              </MemoizedReactMarkdown>{' '}
+                      return !inline ? (
+                        <CodeBlock key={Math.random()} language={(match && match[1]) || ''} value={String(children).replace(/\n$/, '')} {...props} />
+                      ) : (
+                        <code className={className} {...props}>
+                          {children}
+                        </code>
+                      );
+                    },
+                    table({ children }) {
+                      return <table className="border-collapse border border-black px-3 py-1 dark:border-white">{children}</table>;
+                    },
+                    th({ children }) {
+                      return <th className="break-words border border-black bg-gray-500 px-3 py-1">{children}</th>;
+                    },
+                    td({ children }) {
+                      return <td className="break-words border border-black px-3 py-1 dark:border-white">{children}</td>;
+                    },
+                  }}
+                >
+                  {`${message.assistantResponse}${messageIsStreaming && messageIndex == (selectedConversation?.messages.length ?? 0) - 1 ? '`▍`' : ''}`}
+                </MemoizedReactMarkdown>{' '}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 });
