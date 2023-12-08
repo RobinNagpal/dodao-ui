42dd34fad3d9a4249b563bff35233847df32cbf1
ALIRAZA17 <ialidar2001@gmail.com>
2023-12-08 16:17:27 +0500

    removed linting issue

--- src/components/timelines/View/TimelineDetails.tsx
+++ src/components/timelines/View/TimelineDetails.tsx
@@ -46,30 +46,23 @@ const Timeline = ({ timeline }: TimelineProps) => {
                 <div className="relative flex justify-between">
                   <div className="flex flex-col items-center w-10 mr-4 md:w-24">
                     <div>
-                      <div
-                        className="flex items-center justify-center w-8 h-8 bg-blue-200 rounded-full dark:bg-gray-600">
+                      <div className="flex items-center justify-center w-8 h-8 bg-blue-200 rounded-full dark:bg-gray-600">
                         <div className="w-4 h-4 bg-blue-600 rounded-full dark:bg-blue-400"></div>
                       </div>
                     </div>
                     <div className="w-px h-full bg-blue-300 dark:bg-gray-600"></div>
                   </div>
                   <div>
-                    <h2
-                      className="inline-block px-4 py-2 mb-4 text-xs font-medium text-gray-100 bg-gradient-to-r from-blue-500 to-blue-900 dark:from-blue-400 dark:to-blue-500 rounded-3xl dark:text-gray-100">
-                      {timeAgo}</h2>
-                    <div
-                      className="relative flex-1 mb-10 border-b-4 border-blue-200 shadow rounded-3xl"
-                      style={{ width: "800px" }}
-                    >
+                    <h2 className="inline-block px-4 py-2 mb-4 text-xs font-medium text-gray-100 bg-gradient-to-r from-blue-500 to-blue-900 dark:from-blue-400 dark:to-blue-500 rounded-3xl dark:text-gray-100">
+                      {timeAgo}
+                    </h2>
+                    <div className="relative flex-1 mb-10 border-b-4 border-blue-200 shadow rounded-3xl" style={{ width: '800px' }}>
                       <div className="relative z-20 p-6">
                         <p className="mb-2 text-xl font-bold">{event.title}</p>
                         <p dangerouslySetInnerHTML={{ __html: eventSummary }} />
                         <div className="flex">
                           {event.fullDetails && (
-                            <a
-                              className={`p-4 flex ${styles.styledLink}`}
-                              onClick={() => handleShowFullDetailsModal(event)}
-                            >
+                            <a className={`p-4 flex ${styles.styledLink}`} onClick={() => handleShowFullDetailsModal(event)}>
                               Full Details <ArrowTopRightOnSquareIcon width={16} height={16} className="ml-1 mt-1 mr-1" />
                             </a>
                           )}
@@ -86,20 +79,17 @@ const Timeline = ({ timeline }: TimelineProps) => {
                         </div>
                       </div>
                     </div>
-
                   </div>
                 </div>
               </div>
             );
           })}
         </div>
-      </section >
+      </section>
 
-      {selectedEvent && <TimelineDetailsModal open={showFullDetailsModal} onClose={handleCloseFullDetailsModal} event={selectedEvent} />
-      }
+      {selectedEvent && <TimelineDetailsModal open={showFullDetailsModal} onClose={handleCloseFullDetailsModal} event={selectedEvent} />}
     </>
   );
 };
 
 export default Timeline;
-
