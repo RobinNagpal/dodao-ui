89f669d884dde4315690d2a94f7e657390556a29
robin <robinnagpal.tiet@gmail.com>
2023-12-08 07:49:40 -0500

    Add new components for different timeline styles

--- src/app/timelines/view/[timelineId]/page.tsx
+++ src/app/timelines/view/[timelineId]/page.tsx
@@ -3,12 +3,11 @@
 import withSpace from '@/app/withSpace';
 import Block from '@/components/app/Block';
 import PrivateEllipsisDropdown from '@/components/core/dropdowns/PrivateEllipsisDropdown';
+import FullPageLoader from '@/components/core/loaders/FullPageLoading';
 import RowLoading from '@/components/core/loaders/RowLoading';
 import PageWrapper from '@/components/core/page/PageWrapper';
-import TimelineDetails from '@/components/timelines/View/TimelineDetails';
+import TimelineDetails from '@/components/timelines/View/TimelineDetailView';
 import { SpaceWithIntegrationsFragment, useTimelineDetailsQuery } from '@/graphql/generated/generated-types';
-import { getMarkedRenderer } from '@/utils/ui/getMarkedRenderer';
-import { marked } from 'marked';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
@@ -16,8 +15,7 @@ const TimelinePage = ({ params, space }: { params: { timelineId: string }; space
   const threeDotItems = [{ label: 'Edit', key: 'edit' }];
   const router = useRouter();
   const { data, loading } = useTimelineDetailsQuery({ variables: { timelineId: params.timelineId, spaceId: space.id } });
-  const renderer = getMarkedRenderer();
-  const contents = data?.timeline?.content && marked.parse(data?.timeline?.content, { renderer });
+
   return (
     <PageWrapper>
       <div className="pt-12">
@@ -39,21 +37,7 @@ const TimelinePage = ({ params, space }: { params: { timelineId: string }; space
                 </div>
               </div>
 
-              <div className="max-w-xl mx-auto">
-                <div className="text-center ">
-                  <div className="relative flex flex-col items-center">
-                    <h1 className="text-6xl font-bold leading-tight"> {data?.timeline?.name}</h1>
-                    <div className="flex w-24 mt-1 mb-10 overflow-hidden rounded">
-                      <div className="flex-1 h-2 bg-blue-200"></div>
-                      <div className="flex-1 h-2 bg-blue-400"></div>
-                      <div className="flex-1 h-2 bg-blue-600"></div>
-                    </div>
-                  </div>
-                  <p className="text-base text-center">{data?.timeline?.excerpt}</p>
-                  <p className="text-sm text-center" dangerouslySetInnerHTML={{ __html: contents || '' }}></p>
-                </div>
-              </div>
-              <div className="px-2">{!loading && <TimelineDetails timeline={data?.timeline} space={space} />}</div>
+              <div className="px-2">{loading ? <FullPageLoader /> : <TimelineDetails timeline={data?.timeline} space={space} />}</div>
             </>
           ) : (
             <Block slim>
