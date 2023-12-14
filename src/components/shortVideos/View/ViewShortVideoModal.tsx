3a0f166084cb2e09eaa2892da7d851b349159239
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2023-12-14 21:47:37 +0500

    Add video playback functionality to ViewShortVideoModal component

--- src/components/shortVideos/View/ViewShortVideoModal.tsx
+++ src/components/shortVideos/View/ViewShortVideoModal.tsx
@@ -4,7 +4,7 @@ import PrivateEllipsisDropdown from '@/components/core/dropdowns/PrivateEllipsis
 import FullScreenModal from '@/components/core/modals/FullScreenModal';
 import { ShortVideoFragment } from '@/graphql/generated/generated-types';
 // import { videos } from '../sampleVideos';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import SwiperCore from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -21,10 +21,28 @@ interface ViewShortVideoModalProps {
 }
 export default function ViewShortVideoModal({ initialSlide, videos, onClose, onShowEditModal }: ViewShortVideoModalProps) {
   const swiperRef = useRef<SwiperCore>(null);
-  const initialVideo = videos?.[initialSlide];
+  const videoRefs = useRef(videos.map(() => React.createRef<HTMLVideoElement>()));
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(initialSlide);
+
+  const handleSlideChange = (swiper: SwiperCore) => {
+    setCurrentSlideIndex(swiper.realIndex);
+
+    // Pause the video in the previously active slide
+    const previousVideoRef = videoRefs.current[currentSlideIndex];
+    if (previousVideoRef && previousVideoRef.current) {
+      previousVideoRef.current.pause();
+    }
+
+    // Play the video in the new active slide
+    const currentVideoRef = videoRefs.current[swiper.realIndex];
+    if (currentVideoRef && currentVideoRef.current) {
+      currentVideoRef.current.play();
+    }
+  };
+  const currentVideoTitle = videos[currentSlideIndex]?.title || '';
   const threeDotItems = [{ label: 'Edit', key: 'edit' }];
   return (
-    <FullScreenModal title={initialVideo?.title} open={true} onClose={onClose} fullWidth={false}>
+    <FullScreenModal title={currentVideoTitle} open={true} onClose={onClose} fullWidth={false}>
       <div className="flex justify-end">
         <PrivateEllipsisDropdown
           items={threeDotItems}
@@ -51,11 +69,13 @@ export default function ViewShortVideoModal({ initialSlide, videos, onClose, onS
           modules={[Keyboard, Mousewheel, History, Navigation]}
           initialSlide={initialSlide}
           style={{ display: 'flex' }}
+          onSlideChange={handleSlideChange}
         >
           {videos.map((vid, index) => (
             <SwiperSlide key={index} data-history={vid.videoUrl} className="flex justify-center items-center w-full h-full" style={{ display: 'flex' }}>
-              <div className="relative w-[300px] h-[533px] 2xl:w-[600px] 2xl:h-[1066px]">
+              <div className="relative w-[288px] h-[512px] 2xl:w-[585px] 2xl:h-[1040px]">
                 <video
+                  ref={videoRefs.current[index]}
                   id={vid.id}
                   controls //enables video control options
                   controlsList="nofullscreen" //disable full-screen option - only for Chrome
