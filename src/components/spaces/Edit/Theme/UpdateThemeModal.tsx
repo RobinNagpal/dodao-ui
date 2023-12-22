37d2052811c7540fe60100244a54a4090b22703b
ALIRAZA17 <ialidar2001@gmail.com>
2023-12-23 01:25:35 +0500

    Added theme edit functionality

--- src/components/spaces/Edit/Theme/UpdateThemeModal.tsx
+++ src/components/spaces/Edit/Theme/UpdateThemeModal.tsx
@@ -3,46 +3,65 @@ import FullScreenModal from '@/components/core/modals/FullScreenModal';
 import { SpaceWithIntegrationsFragment } from '@/graphql/generated/generated-types';
 import { CSSProperties, useState } from 'react';
 import styles from './UpdateThemeModal.module.scss';
+
 export interface UpdateThemeModalProps {
   space: SpaceWithIntegrationsFragment & { themeColors?: ThemeValue };
   open: boolean;
   onClose: () => void;
+  colorLabels: string[];
 }
 
-export default function UpdateThemeModal({ space, open, onClose }: UpdateThemeModalProps) {
-  const [themeColors, setThemeColors] = useState<ThemeValue>(space.themeColors || themes.ArbitrumTheme);
+export default function UpdateThemeModal({ space, open, onClose, colorLabels }: UpdateThemeModalProps) {
+  const [themeColors, setThemeColors] = useState<ThemeValue>(space.themeColors || themes.GlobalTheme);
+  const themeColorKeys = Object.keys(themeColors) as (keyof ThemeValue)[];
 
-  const style = {
-    '--primary-color': themeColors.primaryColor,
-    '--bg-color': themeColors.bgColor,
-    '--text-color': themeColors.textColor,
-    '--link-color': themeColors.linkColor,
-    '--heading-color': themeColors.headingColor,
-    '--border-color': themeColors.borderColor,
-    '--block-bg': themeColors.blockBg,
-  } as CSSProperties;
+  const handleColorChange = (colorKey: keyof ThemeValue, colorValue: string) => {
+    setThemeColors({ ...themeColors, [colorKey]: colorValue });
+  };
 
-  // Here you add logic to show color selector and the Tidbit Grid. This is the screenshot which you send.
-  // This is EDIT MODE
   return (
-    <FullScreenModal open={open} onClose={onClose} title="Basic Space Settings">
-      <div style={style} className={styles.bodyText}>
-        <div>Here you add logic to show color selector and the Tidbit Grid. This is the screenshot which you send.</div>
-        <div>
-          <label htmlFor="hs-color-input" className="block text-sm font-medium mb-2 dark:text-white">
-            Color picker
-          </label>
-          <input
-            type="color"
-            className="p-1 h-10 block bg-white border border-gray-200 cursor-pointer w-10 rounded-lg disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700"
-            id="hs-color-input"
-            value={themeColors.textColor}
-            title="Choose your color"
-            onChange={(e) => {
-              setThemeColors({ ...themeColors, primaryColor: e.target.value });
+    <FullScreenModal open={open} onClose={onClose} title="Theme Settings">
+      <div className={'mt-4 ' + styles.bodyText}>
+        <div className="flex flex-wrap">
+          <div className="w-1/2">
+            <h1 className="font-bold text-2xl mb-4">Theme Details</h1>
+            {colorLabels.map((label, index) => {
+              const colorKey = themeColorKeys[index];
+              const colorValue = themeColors[colorKey] || '';
+              return (
+                <div key={index} className="flex justify-between items-center mb-2">
+                  <label className="ml-7">{label}</label>
+                  <input type="color" className="w-12 h-8 mr-8" value={colorValue} onChange={(e) => handleColorChange(colorKey, e.target.value)} />
+                </div>
+              );
+            })}
+          </div>
+          <div
+            className="w-1/2 p-4"
+            style={{
+              color: themeColors.textColor,
+              backgroundColor: themeColors.bgColor,
+              border: '3px solid ' + themeColors.borderColor,
             }}
-          />
-          - {themeColors.textColor}
+          >
+            <h1
+              className="text-2xl text-center"
+              style={{
+                color: themeColors.headingColor,
+              }}
+            >
+              Test Heading
+            </h1>
+
+            <p>
+              This is a sample text Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum voluptas eaque illo nesciunt perspiciatis accusantium
+              tenetur unde quo exercitationem, molestias error est neque recusandae. Similique sequi, fuga commodi ipsum repellat quisquam cupiditate dolor cum
+              possimus odio accusantium nam facere corrupti aliquam inventore est soluta id asperiores dicta temporibus optio. Earum, porro reiciendis quidem
+              nam laudantium dolores, ullam quis enim tempore consequatur, doloremque sed! Suscipit reiciendis officia mollitia adipisci vel velit fugiat,
+              explicabo corrupti voluptatum necessitatibus, debitis commodi laborum numquam, exercitationem ipsum sed. Obcaecati quasi quo error placeat
+              repellat assumenda, autem voluptas porro expedita illum excepturi aspernatur ex earum corrupti perspiciatis.
+            </p>
+          </div>
         </div>
       </div>
     </FullScreenModal>
--- src/components/spaces/View/SpaceThemeDetails.tsx
+++ src/components/spaces/View/SpaceThemeDetails.tsx
@@ -1,22 +1,25 @@
-import { ThemeValue } from '@/app/themes';
+import { ThemeValue, themes } from '@/app/themes';
 import PrivateEllipsisDropdown from '@/components/core/dropdowns/PrivateEllipsisDropdown';
 import UpdateThemeModal from '@/components/spaces/Edit/Theme/UpdateThemeModal';
 import { SpaceWithIntegrationsFragment } from '@/graphql/generated/generated-types';
-import React from 'react';
+import React, { useState } from 'react';
 
 export interface SpaceDetailsProps {
   space: SpaceWithIntegrationsFragment & { themeColors?: ThemeValue };
 }
 
 export default function SpaceThemeDetails({ space }: SpaceDetailsProps) {
-  // Here you add logic to show color selected and the Tidbit Grid. This is the screenshot which you send.
-  // This is VIEW ONLY MODE
-
   const [showThemeUpdateModal, setShowThemeUpdateModal] = React.useState(false);
+
+  const [themeColors, setThemeColors] = useState<ThemeValue>(space.themeColors || themes.GlobalTheme);
+  const themeColorKeys = Object.keys(themeColors) as (keyof ThemeValue)[];
+
   const threeDotItems = [
     { label: 'Reload Repo', key: 'reloadRepo' },
     { label: 'Edit', key: 'edit' },
   ];
+
+  const colorLabels = ['Primary Color', 'Background Color', 'Text Color', 'Link Color', 'Heading Color', 'Border Color', 'Block Color'];
 
   const selectFromThreedotDropdown = async (e: string) => {
     if (e === 'edit') {
@@ -32,9 +35,37 @@ export default function SpaceThemeDetails({ space }: SpaceDetailsProps) {
         </div>
         <PrivateEllipsisDropdown items={threeDotItems} onSelect={selectFromThreedotDropdown} className="ml-4 pt-4 grow-0 w-16" />
       </div>
-      <div>Here you add logic to show color selected and the Tidbit Grid. This is the screenshot which you send.</div>
-      <div>This is view only mode. It show boxes instead of color input</div>
-      <UpdateThemeModal space={space} open={showThemeUpdateModal} onClose={() => setShowThemeUpdateModal(false)} />
+
+      <div className={'mt-4 '}>
+        <div className="flex flex-wrap">
+          <div className="w-1/2 mt-4">
+            {colorLabels.map((label, index) => {
+              const colorKey = themeColorKeys[index];
+              const colorValue = themeColors[colorKey] || '';
+              console.log('i am ali ' + colorValue);
+              return (
+                <div key={index} className="flex justify-between items-center mb-2">
+                  <label className="ml-7">{label}</label>
+                  <input type="color" className="w-12 h-8 mr-8" value={colorValue} disabled />
+                </div>
+              );
+            })}
+          </div>
+          <div className="w-1/2 p-4 border">
+            <h1 className="text-2xl text-center">Test Heading</h1>
+
+            <p>
+              This is a sample text Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum voluptas eaque illo nesciunt perspiciatis accusantium
+              tenetur unde quo exercitationem, molestias error est neque recusandae. Similique sequi, fuga commodi ipsum repellat quisquam cupiditate dolor cum
+              possimus odio accusantium nam facere corrupti aliquam inventore est soluta id asperiores dicta temporibus optio. Earum, porro reiciendis quidem
+              nam laudantium dolores, ullam quis enim tempore consequatur, doloremque sed! Suscipit reiciendis officia mollitia adipisci vel velit fugiat,
+              explicabo corrupti voluptatum necessitatibus, debitis commodi laborum numquam, exercitationem ipsum sed. Obcaecati quasi quo error placeat
+              repellat assumenda, autem voluptas porro expedita illum excepturi aspernatur ex earum corrupti perspiciatis.
+            </p>
+          </div>
+        </div>
+      </div>
+      <UpdateThemeModal colorLabels={colorLabels} space={space} open={showThemeUpdateModal} onClose={() => setShowThemeUpdateModal(false)} />
     </div>
   );
 }
