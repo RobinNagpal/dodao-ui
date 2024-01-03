247a206c7a3b8dc74de63123b56fc09e92e869a5
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-01-03 23:42:40 +0500

    Padding to the header and subheader

--- src/components/core/details/DetailsHeader.tsx
+++ src/components/core/details/DetailsHeader.tsx
@@ -12,8 +12,8 @@ export default function DetailsHeader(props: DetailsHeaderProps) {
   return (
     <div className={`sm:flex sm:items-center ${props.className || ''}`}>
       <div className="sm:flex-auto">
-        <h1 className="font-semibold leading-6 text-2xl">{props.header}</h1>
-        {props.subheader && <p className="mt-2 text-sm">{props.subheader}</p>}
+        <h1 className="font-semibold leading-6 text-2xl px-2">{props.header}</h1>
+        {props.subheader && <p className="mt-2 text-sm p-2">{props.subheader}</p>}
       </div>
       <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
         {props.editLink && (
