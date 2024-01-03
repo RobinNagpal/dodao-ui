9138ea10cba33cb2287a37a9d9fd991a158e1caa
robin <robinnagpal.tiet@gmail.com>
2024-01-03 06:03:12 -0500

    read super admins from env file

--- src/utils/auth/superAdmins.ts
+++ src/utils/auth/superAdmins.ts
@@ -1,6 +1,7 @@
 import { Session } from '@/types/auth/Session';
 
-export const superAdmins = ['0x470579d16401a36bf63b1428eaa7189fbde5fee9', 'robinnagpal.tiet@gmail.com'].map((username) => username.toLowerCase());
+const superAdminsFromEnv = process.env.DODAO_SUPERADMINS ? process.env.DODAO_SUPERADMINS.split(',') : [];
+const superAdmins = [...superAdminsFromEnv, '0x470579d16401a36BF63b1428eaA7189FBdE5Fee9', 'robinnagpal.tiet@gmail.com'].map((u) => u.toLowerCase().trim());
 
 export const isSuperAdmin = (session: Session) => {
   return session.username && superAdmins.includes(session.username.toLowerCase());
