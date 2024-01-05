69305ecafb6d04cc4072531b90ca3b5ebf815349
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-01-06 04:04:17 +0500

    Update margin in ToggleWithIcon component

--- src/components/core/toggles/ToggleWithIcon.tsx
+++ src/components/core/toggles/ToggleWithIcon.tsx
@@ -25,7 +25,7 @@ export default function ToggleWithIcon(props: ToggleWithIconProps) {
 
   return (
     <div className="flex align-center w-full mt-4">
-      <div className="mr-8">
+      <div className="mr-1 s:mr-4">
         <StyledSwitch
           checked={enabled}
           onChange={setEnabled}
