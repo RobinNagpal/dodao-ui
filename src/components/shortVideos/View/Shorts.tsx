8c7ccec3b7bfd985b441e7eecc5c27a3fc9bc8a6
ALIRAZA17 <ialidar2001@gmail.com>
2024-01-06 18:24:02 +0500

    added image component from next

--- src/components/shortVideos/View/Shorts.tsx
+++ src/components/shortVideos/View/Shorts.tsx
@@ -4,6 +4,7 @@ import Grid5Cols from '@/components/core/grids/Grid5Cols';
 import PageWrapper from '@/components/core/page/PageWrapper';
 import { ProjectShortVideo, ShortVideo } from '@/graphql/generated/generated-types';
 import styled from 'styled-components';
+import Image from 'next/image';
 
 const ImageWrapper = styled.div`
   width: 250px;
@@ -15,7 +16,7 @@ function ShortsThumbnail({ shortVideo, onClick }: { shortVideo: ShortVideo | Pro
     <button onClick={onClick} className="p-2 min-w-0 flex">
       <ImageWrapper>
         <div>
-          <img src={thumbnail} alt={title} className="rounded-lg" />
+          <Image alt={title} src={thumbnail} width={300} height={200} className="rounded-lg" placeholder="blur" blurDataURL={thumbnail} />
         </div>
         <div className="title-wrapper">
           <h1>{title}</h1>
