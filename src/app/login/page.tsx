9197c77809ea833d445b276f978e54fb0d5aa5db
ALIRAZA17 <ialidar2001@gmail.com>
2024-01-12 18:13:06 +0500

    Added states for name and email

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,32 +5,33 @@ import Input from '@/components/core/input/Input';
 import PageWrapper from '@/components/core/page/PageWrapper';
 import { useAuth } from '@/hooks/useAuth';
 import { useSession } from 'next-auth/react';
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 function LoginPage() {
   const { data: session } = useSession();
   const { logout, active } = useAuth();
 
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleNameChange = (value: string | number | undefined) => {
+    setName(value as string);
+  };
+
+  const handleEmailChange = (value: string | number | undefined) => {
+    setEmail(value as string);
+  };
   return (
     <PageWrapper>
       <div className="space-y-12 text-left p-6">
         <div>
           <h1 className="text-base font-semibold leading-7">Basic Login Credentials</h1>
           <p className="mt-1 text-sm leading-6">Please provide login data to proceed!</p>
-          <Input className="mt-4" label="Name" />
-          <Input label="Email" />
+          <Input className="mt-4" label="Name" modelValue={name} onUpdate={handleNameChange} />
+          <Input label="Email" modelValue={email} onUpdate={handleEmailChange} />
         </div>
         <div className="flex items-center justify-start gap-x-6">
-          <Button
-            variant="contained"
-            primary
-            // loading={}
-            // disabled={uploadThumbnailLoading || upserting}
-            // onClick={async () => {
-            //   await upsertSpace();
-            //   props.onClose();
-            // }}
-          >
+          <Button variant="contained" primary>
             Save
           </Button>
         </div>
