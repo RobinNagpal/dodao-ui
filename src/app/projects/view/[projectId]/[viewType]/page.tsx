430508a914cf9bec79fdaab881fe1eaf463a18e8
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-01-15 17:28:21 +0500

    Refactor project home page to show archived tidbits and byte collections

--- src/app/projects/view/[projectId]/[viewType]/page.tsx
+++ src/app/projects/view/[projectId]/[viewType]/page.tsx
@@ -1,29 +1,29 @@
 import ByteCollectionsGrid from '@/components/byteCollection/View/ByteCollectionsGrid';
 import BytesGrid from '@/components/bytes/List/BytesGrid';
+import ListTidbitsTopBar from '@/components/bytes/List/ListTidbitsTopbar';
 import ProjectShortVideosGrid from '@/components/projects/projectShortVideo/List/ProjectShortVideosGrid';
 import { ProjectByteCollectionFragment, ProjectByteFragment, ProjectFragment } from '@/graphql/generated/generated-types';
 import getApiResponse from '@/utils/api/getApiResponse';
 import { getSpaceServerSide } from '@/utils/api/getSpaceServerSide';
 import React from 'react';
 
-async function ProjectHomePage(props: { params: { projectId: string; viewType: string } }) {
+async function ProjectHomePage(props: { params: { projectId: string; viewType: string }; searchParams: { [key: string]: string | string[] | undefined } }) {
   const space = (await getSpaceServerSide())!;
-
+  const showArchived = props.searchParams?.['showArchived'] === 'true';
   const project = await getApiResponse<ProjectFragment>(space, `projects/${props.params.projectId}`);
 
   const byteCollections = await getApiResponse<ProjectByteCollectionFragment[]>(space, `projects/${props.params.projectId}/byte-collections`);
 
   const bytes = await getApiResponse<ProjectByteFragment[]>(space, `projects/${props.params.projectId}/bytes`);
 
+  const tidbitsToShow = bytes.filter((byte) => byte.archived === showArchived);
+  const tidbitsCollectionsToShow = byteCollections.filter((bytecollection) => bytecollection.archived === showArchived);
   if (props.params.viewType === 'tidbits') {
     return (
-      <BytesGrid
-        bytes={bytes?.filter((byte) => !byte?.archived)}
-        baseByteViewUrl={`/projects/view/${project.id}/tidbits`}
-        byteType={'projectByte'}
-        project={project}
-        space={space}
-      />
+      <>
+        <ListTidbitsTopBar space={space} showArchived={showArchived} />
+        <BytesGrid bytes={tidbitsToShow} baseByteViewUrl={`/projects/view/${project.id}/tidbits`} byteType={'projectByte'} project={project} space={space} />
+      </>
     );
   }
 
@@ -32,13 +32,16 @@ async function ProjectHomePage(props: { params: { projectId: string; viewType: s
   }
 
   return (
-    <ByteCollectionsGrid
-      loadingData={false}
-      space={space}
-      project={project}
-      byteCollections={byteCollections?.filter((byteCollection) => !byteCollection?.archived)}
-      byteCollectionType={'projectByteCollection'}
-    />
+    <>
+      <ListTidbitsTopBar space={space} showArchived={showArchived} />
+      <ByteCollectionsGrid
+        loadingData={false}
+        space={space}
+        project={project}
+        byteCollections={tidbitsCollectionsToShow}
+        byteCollectionType={'projectByteCollection'}
+      />
+    </>
   );
 }
 
