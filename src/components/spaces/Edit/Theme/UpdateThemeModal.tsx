e5af29ab2e26b3d773ba96212b40c33f106e3709
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-01-15 19:28:59 +0500

    Resolved tsc errors

--- src/components/spaces/Edit/Theme/UpdateThemeModal.tsx
+++ src/components/spaces/Edit/Theme/UpdateThemeModal.tsx
@@ -105,12 +105,7 @@ export default function UpdateThemeModal({ space, open, onClose, byteCollection 
               })}
             </div>
             <div className="flex justify-center items-center w-full md:mt-0 md:w-1/2 p-2 md:p-4">
-              <ByteCollectionsCard
-                isEditingAllowed={false}
-                byteCollection={byteCollection}
-                onSelectByte={() => {}}
-                byteCollectionType={'projectByteCollection'}
-              />
+              <ByteCollectionsCard space={space} isEditingAllowed={false} byteCollection={byteCollection} byteCollectionType={'projectByteCollection'} />
             </div>
           </div>
         </div>
