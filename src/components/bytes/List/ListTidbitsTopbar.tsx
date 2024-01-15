134f9d67c8d3f5cf50bc724f357319cd05a0a6e4
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-01-15 17:27:45 +0500

    Add ListTidbitsTopbar component

---
+++ src/components/bytes/List/ListTidbitsTopbar.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import ToggleWithIcon from '@/components/core/toggles/ToggleWithIcon';
+import { SpaceWithIntegrationsFragment } from '@/graphql/generated/generated-types';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import { usePathname } from 'next/navigation';
+
+export default function ListTidbitsTopBar(props: { space: SpaceWithIntegrationsFragment; showArchived: boolean }) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  return (
+    <div className="flex justify-end mb-4">
+      <ToggleWithIcon label={'Show Archived'} enabled={props.showArchived} setEnabled={(value) => router.push(`${pathname}?showArchived=${value}`)} />
+    </div>
+  );
+}
