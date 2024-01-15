95ebe27a1868217e0410613eaad65ca26c8da407
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-01-15 15:45:55 +0500

    Add show more button to display more projects

--- src/components/projects/List/ListProjectsHelper.tsx
+++ src/components/projects/List/ListProjectsHelper.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import ProjectSummaryCard from '@/components/projects/List/ProjectSummaryCard';
+import Button from '@/components/core/buttons/Button';
 import { ProjectFragment, SpaceWithIntegrationsFragment } from '@/graphql/generated/generated-types';
 import groupBy from 'lodash/groupBy';
-import React from 'react';
+import React, { useState } from 'react';
 import 'react-loading-skeleton/dist/skeleton.css';
 
 type ListProjectsHelperProps = {
@@ -10,7 +13,19 @@ type ListProjectsHelperProps = {
 };
 
 const ListProjectsHelper: React.FC<ListProjectsHelperProps> = ({ projects, space }) => {
+  const [visibleProjectsCount, setVisibleProjectsCount] = useState<{ [key: string]: number }>({});
   const groupedProjects = groupBy(projects, 'type');
+  Object.keys(groupedProjects).forEach((type) => {
+    if (!visibleProjectsCount[type]) {
+      visibleProjectsCount[type] = 5;
+    }
+  });
+  const handleShowMoreClick = (type: string) => {
+    setVisibleProjectsCount((prevCount) => ({
+      ...prevCount,
+      [type]: (prevCount[type] || 0) + 5,
+    }));
+  };
 
   return (
     <div className="my-5">
@@ -18,10 +33,17 @@ const ListProjectsHelper: React.FC<ListProjectsHelperProps> = ({ projects, space
         <div key={type} className="mb-16">
           <h2 className="text-lg font-semibold my-2">{type} Projects</h2>
           <div className="grid grid-cols-1 xs:grid-cols-1 s:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-            {groupedProjects[type].map((proj) => (
+            {groupedProjects[type].slice(0, visibleProjectsCount[type] || 5).map((proj) => (
               <ProjectSummaryCard key={proj.id} space={space} project={proj} />
             ))}
           </div>
+          {groupedProjects[type].length > (visibleProjectsCount[type] || 5) && (
+            <div className="text-center mt-4">
+              <Button variant="contained" primary onClick={() => handleShowMoreClick(type)}>
+                Show More
+              </Button>
+            </div>
+          )}
         </div>
       ))}
     </div>
