2ccfc0a8eecacaac90bdd3dd78ecccf73e7f4102
ALIRAZA17 <ialidar2001@gmail.com>
2024-01-16 14:12:38 +0500

    api for updating user

---
+++ src/app/api/auth/update-user/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { prisma } from '@/prisma';
+
+export async function PUT(request: Request) {
+  const { name, email, phone_number, username, spaceId } = await request.json();
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { username_spaceId: {username, spaceId}},
+      data: { name, email, phone_number }
+    });
+
+    return NextResponse.json(updatedUser);
+  } catch (error) {
+    console.error('Error updating user:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
