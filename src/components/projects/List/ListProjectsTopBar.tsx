3d7a8aca95315223f3ec03290ba6d61b05609922
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-01-16 21:34:00 +0500

    Changed simple toggle with private toggle in topbar of list projects

--- src/components/projects/List/ListProjectsTopBar.tsx
+++ src/components/projects/List/ListProjectsTopBar.tsx
@@ -1,22 +1,18 @@
 'use client';
 
 import Button from '@/components/core/buttons/Button';
-import ToggleWithIcon from '@/components/core/toggles/ToggleWithIcon';
 import UpsertProjectModal from '@/components/projects/Edit/UpsertProjectModal';
 import { ProjectFragment, SpaceWithIntegrationsFragment } from '@/graphql/generated/generated-types';
-import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
-import { usePathname } from 'next/navigation';
+import PrivateArchivedToggle from '@/components/projects/List/PrivateArchivedToggle';
 
 export default function ListProjectsTopBar(props: { space: SpaceWithIntegrationsFragment; showArchived: boolean }) {
   const [showProjectAddModal, setShowProjectAddModal] = useState(false);
   const [project, setProject] = useState<ProjectFragment | null>(null);
-  const router = useRouter();
-  const pathname = usePathname();
 
   return (
     <div className="flex gap-2 sm:gap-7 justify-between items-center">
-      <ToggleWithIcon label={'Show Archived'} enabled={props.showArchived} setEnabled={(value) => router.push(`${pathname}?showArchived=${value}`)} />
+      <PrivateArchivedToggle space={props.space} showArchived={props.showArchived} />
       <div className="mt-4 flex-none">
         <Button variant="contained" primary onClick={() => setShowProjectAddModal(true)}>
           Add Project
