d49b328f0516077f15dc0a01bf750ff2e2dc1e26
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-01-16 21:33:13 +0500

    Added dynamic path in PrivateArchivedToggle component

--- src/components/projects/List/PrivateArchivedToggle.tsx
+++ src/components/projects/List/PrivateArchivedToggle.tsx
@@ -6,20 +6,18 @@ import { Session } from '@/types/auth/Session';
 import { isAdmin } from '@/utils/auth/isAdmin';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
-export interface ShowArchivedToggleProps {
-  space: SpaceWithIntegrationsFragment;
-  showArchived?: boolean;
-}
-export default function ShowArchivedToggle({ showArchived, space }: ShowArchivedToggleProps) {
+export default function PrivateArchivedToggle(props: { space: SpaceWithIntegrationsFragment; showArchived: boolean }) {
   const router = useRouter();
+  const pathname = usePathname();
 
   const { data: session } = useSession();
 
-  const isUserAdmin = session && isAdmin(session as Session, space);
+  const isUserAdmin = session && isAdmin(session as Session, props.space);
 
   return isUserAdmin ? (
-    <ToggleWithIcon label={'Show Archived'} enabled={showArchived || false} setEnabled={(value) => router.push(`/projects/type/all?showArchived=${value}`)} />
+    <ToggleWithIcon label={'Show Archived'} enabled={props.showArchived} setEnabled={(value) => router.push(`${pathname}?showArchived=${value}`)} />
   ) : null;
 }
