6110675ebf3eb4ac3931fde0522df02bf5b0af43
ALIRAZA17 <ialidar2001@gmail.com>
2024-01-17 16:32:29 +0500

    used present private component

--- src/components/projects/List/ListProjectsTopBar.tsx
+++ src/components/projects/List/ListProjectsTopBar.tsx
@@ -5,28 +5,22 @@ import UpsertProjectModal from '@/components/projects/Edit/UpsertProjectModal';
 import { ProjectFragment, SpaceWithIntegrationsFragment } from '@/graphql/generated/generated-types';
 import React, { useState } from 'react';
 import PrivateArchivedToggle from '@/components/projects/List/PrivateArchivedToggle';
-import { isAdmin } from '@/utils/auth/isAdmin';
-import { Session } from '@/types/auth/Session';
-import { useSession } from 'next-auth/react';
+import PrivateComponent from '@/components/core/PrivateComponent';
 
 export default function ListProjectsTopBar(props: { space: SpaceWithIntegrationsFragment; showArchived: boolean }) {
   const [showProjectAddModal, setShowProjectAddModal] = useState(false);
   const [project, setProject] = useState<ProjectFragment | null>(null);
-  const { data: session } = useSession();
-  const isAdminUser = isAdmin(session as Session, props.space);
 
   return (
     <div className="flex gap-2 sm:gap-7 justify-between items-center">
       <PrivateArchivedToggle space={props.space} showArchived={props.showArchived} />
-
-      {isAdminUser && (
+      <PrivateComponent>
         <div className="mt-4 flex-none">
           <Button variant="contained" primary onClick={() => setShowProjectAddModal(true)}>
             Add Project
           </Button>
         </div>
-      )}
-
+      </PrivateComponent>
       {showProjectAddModal && (
         <UpsertProjectModal
           spaceId={props.space.id}
