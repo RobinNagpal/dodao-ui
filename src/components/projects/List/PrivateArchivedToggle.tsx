478345eadcbb38f664c4dc2ba1879e7bc84d86f8
ALIRAZA17 <ialidar2001@gmail.com>
2024-01-17 16:41:23 +0500

    Used already defined component for checks

--- src/components/projects/List/PrivateArchivedToggle.tsx
+++ src/components/projects/List/PrivateArchivedToggle.tsx
@@ -1,10 +1,8 @@
 'use client';
 
+import PrivateComponent from '@/components/core/PrivateComponent';
 import ToggleWithIcon from '@/components/core/toggles/ToggleWithIcon';
 import { SpaceWithIntegrationsFragment } from '@/graphql/generated/generated-types';
-import { Session } from '@/types/auth/Session';
-import { isAdmin } from '@/utils/auth/isAdmin';
-import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { usePathname } from 'next/navigation';
 import React from 'react';
@@ -13,11 +11,9 @@ export default function PrivateArchivedToggle(props: { space: SpaceWithIntegrati
   const router = useRouter();
   const pathname = usePathname();
 
-  const { data: session } = useSession();
-
-  const isUserAdmin = session && isAdmin(session as Session, props.space);
-
-  return isUserAdmin ? (
-    <ToggleWithIcon label={'Show Archived'} enabled={props.showArchived} setEnabled={(value) => router.push(`${pathname}?showArchived=${value}`)} />
-  ) : null;
+  return (
+    <PrivateComponent>
+      <ToggleWithIcon label={'Show Archived'} enabled={props.showArchived} setEnabled={(value) => router.push(`${pathname}?showArchived=${value}`)} />
+    </PrivateComponent>
+  );
 }
