1a5892e3625cd75d44c5bfaa414ea694e157f4ae
robin <robinnagpal.tiet@gmail.com>
2024-01-17 15:38:32 -0500

    Remove the extra space

--- src/components/projects/List/ListProjects.tsx
+++ src/components/projects/List/ListProjects.tsx
@@ -8,12 +8,10 @@ export default function ListProjects(props: { space: SpaceWithIntegrationsFragme
   const projectsToShow = props.projects.filter((project) => project.archived === props.showArchived);
   return (
     <PageWrapper>
-      <div className="px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-col sm:flex-row justify-end">
-          <ListProjectsTopBar space={props.space} showArchived={props.showArchived} />
-        </div>
-        <ListProjectsHelper space={props.space} projects={projectsToShow} />
+      <div className="flex flex-col sm:flex-row justify-end">
+        <ListProjectsTopBar space={props.space} showArchived={props.showArchived} />
       </div>
+      <ListProjectsHelper space={props.space} projects={projectsToShow} />
     </PageWrapper>
   );
 }
