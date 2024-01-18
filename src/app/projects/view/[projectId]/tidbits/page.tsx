ce61445e4930dd4716fb43f84de2e390736bff71
ALIRAZA17 <ialidar2001@gmail.com>
2024-01-18 16:48:45 +0500

    removed unnecessary imports

--- src/app/projects/view/[projectId]/tidbits/page.tsx
+++ src/app/projects/view/[projectId]/tidbits/page.tsx
@@ -1,4 +1,3 @@
-import ByteCollectionsGrid from '@/components/byteCollection/View/ByteCollectionsGrid';
 import BytesGrid from '@/components/bytes/List/BytesGrid';
 import PrivateArchivedToggle from '@/components/projects/List/PrivateArchivedToggle';
 import { ProjectByteFragment, ProjectFragment } from '@/graphql/generated/generated-types';
