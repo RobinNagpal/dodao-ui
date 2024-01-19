f14afb295c7e9246bb4d791024b18a47744363da
robin <robinnagpal.tiet@gmail.com>
2024-01-19 10:04:28 -0500

    Small improvements

--- src/components/projects/View/ViewProjectHeader.tsx
+++ src/components/projects/View/ViewProjectHeader.tsx
@@ -4,9 +4,11 @@ import Button from '@/components/core/buttons/Button';
 import FullScreenModal from '@/components/core/modals/FullScreenModal';
 import PrivateComponent from '@/components/core/PrivateComponent';
 import CreateProjectContentModalContents from '@/components/projects/Nav/CreateProjectContentModalContents';
+import styles from '@/components/projects/View/TabLink.module.scss';
 import { ProjectFragment, Space } from '@/graphql/generated/generated-types';
+import classNames from '@/utils/classNames';
+import Link from 'next/link';
 import React from 'react';
-import TabLink from './TabLink';
 import { isAdmin } from '@/utils/auth/isAdmin';
 import { useSession } from 'next-auth/react';
 import { Session } from '@/types/auth/Session';
@@ -16,15 +18,13 @@ export function ViewProjectHeader({ project, selectedViewType, space }: { projec
   const [showCreateContentsModal, setShowCreateContentsModal] = React.useState(false);
   const { data: session } = useSession();
   const isUserAdmin = session && (isAdmin(session as Session, space) || isSuperAdmin(session as Session));
-  var tabs = [
+  const tabs = [
     { name: 'Tidbits', href: `/projects/view/${project.id}/tidbits`, current: selectedViewType === 'tidbits' },
     { name: 'Tidbits Collections', href: `/projects/view/${project.id}/tidbit-collections`, current: selectedViewType === 'tidbit-collections' },
     { name: 'Shorts', href: `/projects/view/${project.id}/shorts`, current: selectedViewType === 'shorts' },
   ];
 
-  if (!isUserAdmin) {
-    tabs = tabs.filter((tab) => tab.name !== 'Tidbits');
-  }
+  const visibleTabs = tabs.filter((tab) => isUserAdmin || tab.name !== 'Tidbits');
 
   return (
     <div>
@@ -42,17 +42,27 @@ export function ViewProjectHeader({ project, selectedViewType, space }: { projec
                 id="current-tab"
                 name="current-tab"
                 className="block w-full rounded-md border-0 py-1.5 pl-3 pr-10 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-                defaultValue={tabs.find((tab) => tab.current)?.name}
+                defaultValue={visibleTabs.find((tab) => tab.current)?.name}
               >
-                {tabs.map((tab) => (
+                {visibleTabs.map((tab) => (
                   <option key={tab.name}>{tab.name}</option>
                 ))}
               </select>
             </div>
             <div className="hidden sm:block">
               <nav className="-mb-px flex space-x-8">
-                {tabs.map((tab) => (
-                  <TabLink key={tab.name} tab={tab} />
+                {visibleTabs.map((tab) => (
+                  <Link
+                    key={tab.name}
+                    href={tab.href}
+                    className={classNames(
+                      tab.current ? styles.selectedHeaderTab : 'border-transparent text hover:border-gray-300 ',
+                      'whitespace-nowrap border-b-2 px-1 pb-4 text-sm font-medium'
+                    )}
+                    aria-current={tab.current ? 'page' : undefined}
+                  >
+                    {tab.name}
+                  </Link>
                 ))}
               </nav>
             </div>
