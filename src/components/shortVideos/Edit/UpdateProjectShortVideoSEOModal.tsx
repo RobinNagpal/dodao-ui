1a4ef390205775be701bf080479e498a3b0a061d
ALIRAZA17 <ialidar2001@gmail.com>
2024-01-24 21:32:57 +0500

    added seo meta for short video

--- src/components/shortVideos/Edit/UpdateProjectShortVideoSEOModal.tsx
+++ src/components/shortVideos/Edit/UpdateProjectShortVideoSEOModal.tsx
@@ -1,11 +1,30 @@
 import UpdateSEOModal from '@/components/app/Common/UpdateSEOModal';
 import { useNotificationContext } from '@/contexts/NotificationContext';
-import { ProjectShortVideo, ShortVideo, useUpdateSeoOfProjectShortVideoMutation } from '@/graphql/generated/generated-types';
+import {
+  ProjectShortVideo,
+  ShortVideo,
+  useProjectShortVideoQuery,
+  useProjectShortVideosQuery,
+  useUpdateSeoOfProjectShortVideoMutation,
+} from '@/graphql/generated/generated-types';
 import React from 'react';
 
-export default function UpdateProjectShortVideoSEOModal(props: { projectShortVideo: ShortVideo | ProjectShortVideo; open: boolean; onClose: () => void }) {
+export default function UpdateProjectShortVideoSEOModal(props: {
+  projectShortVideo: ShortVideo | ProjectShortVideo;
+  open: boolean;
+  onClose: () => void;
+  projectId: string | undefined;
+}) {
   const [updateSeoOfProjectShortVideo] = useUpdateSeoOfProjectShortVideoMutation();
   const { showNotification } = useNotificationContext();
+
+  const result = useProjectShortVideoQuery({
+    variables: {
+      id: props.projectShortVideo.id,
+      projectId: props.projectId!,
+    },
+    fetchPolicy: 'no-cache',
+  });
 
   const updateSEOOfProjectShortVideo = async (title: string, description: string, keywords: string[]) => {
     try {
@@ -35,6 +54,7 @@ export default function UpdateProjectShortVideoSEOModal(props: { projectShortVid
       onClose={() => {
         props.onClose();
       }}
+      seoMeta={result.data?.projectShortVideo.seoMeta}
       onSeoMetaUpdate={async (seoMeta) => {
         await updateSEOOfProjectShortVideo(seoMeta.title, seoMeta.description, seoMeta.keywords);
       }}
