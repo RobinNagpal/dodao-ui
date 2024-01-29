46fcda9a983668cfb6b6a096bb9a1fcb38482479
ALIRAZA17 <ialidar2001@gmail.com>
2024-01-29 18:49:36 +0500

    added new prop

--- src/components/bytes/List/ByteCardAdminDropdown.tsx
+++ src/components/bytes/List/ByteCardAdminDropdown.tsx
@@ -67,6 +67,7 @@ export default function ByteCardAdminDropdown({ byte, byteType, project }: ByteC
   return (
     <>
       <PrivateEllipsisDropdown
+        blockMenuColor={true}
         items={getThreeDotItems(byte)}
         onSelect={async (key, e: React.MouseEvent<HTMLAnchorElement>) => {
           if (key === 'edit') {
