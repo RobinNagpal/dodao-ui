8a117a8698905c7f0d26bdbc198a300f835333a4
ALIRAZA17 <ialidar2001@gmail.com>
2024-02-01 12:18:49 +0500

    added dynamic meta data for each guide

--- src/app/guides/view/[...guideIdAndStep]/page.tsx
+++ src/app/guides/view/[...guideIdAndStep]/page.tsx
@@ -12,10 +12,14 @@ export async function generateMetadata({ params }: GuideViewProps): Promise<Meta
   const guideIdAndStep = params.guideIdAndStep;
   const guideId = Array.isArray(guideIdAndStep) ? guideIdAndStep[0] : (guideIdAndStep as string);
   const space = (await getSpaceServerSide())!;
-  const project = await getApiResponse<GuideFragment>(space, `guides/${guideId}`);
+  const guide = await getApiResponse<GuideFragment>(space, `guides/${guideId}`);
+  // let stepOrder = 0;
+  // if (Array.isArray(guideIdAndStep)) {
+  //   stepOrder = parseInt(guideIdAndStep[1]);
+  // }
   return {
-    title: project.name,
-    description: project.content,
+    title: guide.name,
+    description: guide.content,
     keywords: [],
   };
 }
