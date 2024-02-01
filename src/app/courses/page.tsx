f3d38193f42a5c05f7ef6582bdec2b3a3c3a02b9
ALIRAZA17 <ialidar2001@gmail.com>
2024-02-01 12:16:24 +0500

    added static metadata to courses main page

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,40 +1,22 @@
-'use client';
-
-import withSpace, { SpaceProps } from '@/app/withSpace';
-import Block from '@/components/app/Block';
-import { Grid3Cols } from '@/components/core/grids/Grid3Cols';
-import RowLoading from '@/components/core/loaders/RowLoading';
 import PageWrapper from '@/components/core/page/PageWrapper';
-import CourseSummaryCard from '@/components/courses/Summary/CourseSummaryCard';
-import NoCourses from '@/components/courses/Summary/NoCourses';
 import MainContainer from '@/components/main/Container/MainContainer';
-import { CourseFragment, useCoursesQueryQuery } from '@/graphql/generated/generated-types';
 import React from 'react';
+import CoursesInformation from './component/coursesInfo';
+import { Metadata } from 'next';
 
-function Courses({ space }: SpaceProps) {
-  const { data, loading } = useCoursesQueryQuery({ variables: { spaceId: space.id } });
+export const metadata: Metadata = {
+  title: 'Courses',
+  description: 'Different courses related to Blockchain',
+};
 
-  const loadingData = loading || !space;
+function Courses() {
   return (
     <MainContainer>
       <PageWrapper>
-        {!data?.courses?.length && !loadingData && <NoCourses />}
-        {!!data?.courses?.length && (
-          <Grid3Cols>
-            {data?.courses?.map((c: CourseFragment, i) => (
-              <CourseSummaryCard key={i} course={c} inProgress={false} />
-            ))}
-          </Grid3Cols>
-        )}
-        <div style={{ height: '10px', width: '10px', position: 'absolute' }} />
-        {loadingData && (
-          <Block slim={true}>
-            <RowLoading className="my-2" />
-          </Block>
-        )}
+        <CoursesInformation />
       </PageWrapper>
     </MainContainer>
   );
 }
 
-export default withSpace(Courses);
+export default Courses;
