e7ee8113427e41593636f8c739b958c493a86e6d
ALIRAZA17 <ialidar2001@gmail.com>
2024-02-01 20:33:52 +0500

    removed page container and main container

--- src/app/courses/component/coursesInfo.tsx
+++ src/app/courses/component/coursesInfo.tsx
@@ -4,10 +4,8 @@ import withSpace, { SpaceProps } from '@/app/withSpace';
 import Block from '@/components/app/Block';
 import { Grid3Cols } from '@/components/core/grids/Grid3Cols';
 import RowLoading from '@/components/core/loaders/RowLoading';
-import PageWrapper from '@/components/core/page/PageWrapper';
 import CourseSummaryCard from '@/components/courses/Summary/CourseSummaryCard';
 import NoCourses from '@/components/courses/Summary/NoCourses';
-import MainContainer from '@/components/main/Container/MainContainer';
 import { CourseFragment, useCoursesQueryQuery } from '@/graphql/generated/generated-types';
 import React from 'react';
 
@@ -16,24 +14,22 @@ function CoursesInformation({ space }: SpaceProps) {
 
   const loadingData = loading || !space;
   return (
-    <MainContainer>
-      <PageWrapper>
-        {!data?.courses?.length && !loadingData && <NoCourses />}
-        {!!data?.courses?.length && (
-          <Grid3Cols>
-            {data?.courses?.map((c: CourseFragment, i) => (
-              <CourseSummaryCard key={i} course={c} inProgress={false} />
-            ))}
-          </Grid3Cols>
-        )}
-        <div style={{ height: '10px', width: '10px', position: 'absolute' }} />
-        {loadingData && (
-          <Block slim={true}>
-            <RowLoading className="my-2" />
-          </Block>
-        )}
-      </PageWrapper>
-    </MainContainer>
+    <>
+      {!data?.courses?.length && !loadingData && <NoCourses />}
+      {!!data?.courses?.length && (
+        <Grid3Cols>
+          {data?.courses?.map((c: CourseFragment, i) => (
+            <CourseSummaryCard key={i} course={c} inProgress={false} />
+          ))}
+        </Grid3Cols>
+      )}
+      <div style={{ height: '10px', width: '10px', position: 'absolute' }} />
+      {loadingData && (
+        <Block slim={true}>
+          <RowLoading className="my-2" />
+        </Block>
+      )}
+    </>
   );
 }
 
