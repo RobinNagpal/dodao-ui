257d383a5f0234b1722e312654e06a5268b39c02
ALIRAZA17 <ialidar2001@gmail.com>
2024-02-05 19:12:58 +0500

    fragment changes

--- src/app/courses/view/[...courseInfo]/page.tsx
+++ src/app/courses/view/[...courseInfo]/page.tsx
@@ -3,7 +3,7 @@ import CourseInformation from './CourseInformation';
 import { Metadata } from 'next';
 import { getSpaceServerSide } from '@/utils/api/getSpaceServerSide';
 import getApiResponse from '@/utils/api/getApiResponse';
-import { CourseFragment } from '@/graphql/generated/generated-types';
+import { CourseDetailsFragment, CourseFragment } from '@/graphql/generated/generated-types';
 
 type CourseViewProps = {
   params: { courseInfo: string[] };
@@ -13,7 +13,7 @@ export async function generateMetadata({ params }: CourseViewProps): Promise<Met
   const courseInfo = params.courseInfo;
   const courseKey = Array.isArray(courseInfo) ? courseInfo[0] : (courseInfo as string);
   const space = (await getSpaceServerSide())!;
-  const course = await getApiResponse<CourseFragment>(space, `courses/${courseKey}`);
+  const course = await getApiResponse<CourseDetailsFragment>(space, `courses/${courseKey}`);
   return {
     title: course.title,
     description: course.summary,
