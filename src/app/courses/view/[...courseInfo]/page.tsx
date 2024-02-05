4ad146aec324af5a8a6d02c9575a78670dfaccae
ALIRAZA17 <ialidar2001@gmail.com>
2024-02-05 20:35:53 +0500

    added proper description for seo in courses

--- src/app/courses/view/[...courseInfo]/page.tsx
+++ src/app/courses/view/[...courseInfo]/page.tsx
@@ -3,7 +3,7 @@ import CourseInformation from './CourseInformation';
 import { Metadata } from 'next';
 import { getSpaceServerSide } from '@/utils/api/getSpaceServerSide';
 import getApiResponse from '@/utils/api/getApiResponse';
-import { CourseDetailsFragment, CourseFragment } from '@/graphql/generated/generated-types';
+import { CourseDetailsFragment } from '@/graphql/generated/generated-types';
 
 type CourseViewProps = {
   params: { courseInfo: string[] };
@@ -12,11 +12,40 @@ type CourseViewProps = {
 export async function generateMetadata({ params }: CourseViewProps): Promise<Metadata> {
   const courseInfo = params.courseInfo;
   const courseKey = Array.isArray(courseInfo) ? courseInfo[0] : (courseInfo as string);
+
+  const isCourseSubmissionScreen = Array.isArray(courseInfo) && courseInfo.length > 1 && courseInfo[1] === 'submission';
+  const topicKey = Array.isArray(courseInfo) && courseInfo.length > 1 && !isCourseSubmissionScreen ? courseInfo[1] : undefined;
+  const itemType = Array.isArray(courseInfo) && courseInfo.length > 2 ? courseInfo[2] : undefined;
+  const itemKey = Array.isArray(courseInfo) && courseInfo.length > 3 ? courseInfo[3] : undefined;
   const space = (await getSpaceServerSide())!;
   const course = await getApiResponse<CourseDetailsFragment>(space, `courses/${courseKey}`);
+  let description = `\n\n${course.title}\n\n${course.details}`;
+  if (topicKey !== undefined) {
+    const topic = course.topics.find((t) => t.key === topicKey);
+    if (topic) {
+      description += `\n\nTopic: ${topic.title}\nDetails: ${topic.details}`;
+    }
+  }
+  if (itemType !== undefined) {
+    const topic = course.topics.find((t) => t.key === topicKey);
+    if (topic) {
+      switch (itemType) {
+        case 'readings':
+          const currentReading = topic.readings.find((r) => r.uuid == itemKey);
+          description += `\n\n${currentReading?.details}`;
+          break;
+        case 'summaries':
+          const currentSummary = topic.summaries.find((s) => s.key == itemKey);
+          description += `\n\n${currentSummary?.details}`;
+          break;
+        default:
+          break;
+      }
+    }
+  }
   return {
     title: course.title,
-    description: course.summary,
+    description: description,
     keywords: [],
   };
 }
