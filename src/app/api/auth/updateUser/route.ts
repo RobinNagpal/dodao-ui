84ab72878ab66cc66e83121d84a695c206932a39
ALIRAZA17 <ialidar2001@gmail.com>
2024-02-07 19:28:26 +0500

    renamed file

---
+++ src/app/api/auth/updateUser/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+import { prisma } from '@/prisma';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '../[...nextauth]/authOptions';
+
+export async function PUT(request: Request) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+  const { name, email, phone_number, username, spaceId } = await request.json();
+
+  if (username !== session.username || spaceId !== session.spaceId) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { username_spaceId: { username: session.username, spaceId: session.spaceId } },
+      data: { name: name, email: email, phone_number: phone_number },
+    });
+    return NextResponse.json(updatedUser);
+  } catch (error) {
+    console.error('Error updating user:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
