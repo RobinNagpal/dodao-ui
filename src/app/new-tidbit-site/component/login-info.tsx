50600248b3997a83a4517f65582c4b87656d9ea1
ALIRAZA17 <ialidar2001@gmail.com>
2024-02-15 18:22:44 +0500

    changed file destination

---
+++ src/app/new-tidbit-site/component/login-info.tsx
@@ -0,0 +1,99 @@
+'use client';
+
+import Button from '@/components/core/buttons/Button';
+import Input from '@/components/core/input/Input';
+import PageWrapper from '@/components/core/page/PageWrapper';
+import React, { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import ReCAPTCHA from 'react-google-recaptcha';
+import { useNotificationContext } from '@/contexts/NotificationContext';
+
+function LoginPage() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [captcha, setCaptcha] = useState<string | null>('');
+  const [updatedUser, setUpdatedUser] = useState(null);
+  const { data: session } = useSession();
+  const { showNotification } = useNotificationContext();
+  const captchaRef = React.useRef(null);
+
+  const handleNameChange = (value: string | number | undefined) => {
+    setName(value as string);
+  };
+
+  const handleEmailChange = (value: string | number | undefined) => {
+    setEmail(value as string);
+  };
+
+  const handlePhoneChange = (value: string | number | undefined) => {
+    setPhone(value as string);
+  };
+
+  const handleSave = async () => {
+    const username = session?.username;
+    const spaceId = session?.spaceId;
+
+    if (!captcha) {
+      showNotification({ type: 'error', message: 'Please complete the reCAPTCHA to proceed.' });
+      return;
+    }
+
+    if (!name.trim() || !email.trim()) {
+      showNotification({ type: 'error', message: 'Name and email cannot be empty' });
+      return;
+    }
+
+    try {
+      const response = await fetch('/api/auth/updateUser', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          phone_number: phone,
+          username: username,
+          spaceId: spaceId,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to update user data');
+      }
+
+      const updatedUser = await response.json();
+      setUpdatedUser(updatedUser);
+
+      setName('');
+      setEmail('');
+      setPhone('');
+      setCaptcha(null);
+    } catch (error) {
+      console.error('Error updating user data:', error);
+    }
+  };
+
+  return (
+    <PageWrapper>
+      <div className="space-y-12 text-left p-6">
+        <div>
+          <h1 className="text-base font-semibold leading-7">Basic Login Credentials</h1>
+          <p className="mt-1 text-sm leading-6">Please provide login data to proceed!</p>
+          <Input className="mt-4" label="Name" modelValue={name} onUpdate={handleNameChange} />
+          <Input label="Email" modelValue={email} onUpdate={handleEmailChange} />
+          <Input label="Phone Number" modelValue={phone} onUpdate={handlePhoneChange} />
+          <ReCAPTCHA ref={captchaRef} sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!} onChange={setCaptcha} className="mt-4" />
+        </div>
+        <div className="flex items-center justify-start gap-x-6">
+          <Button variant="contained" primary onClick={handleSave}>
+            Save
+          </Button>
+        </div>
+      </div>
+    </PageWrapper>
+  );
+}
+
+export default LoginPage;
