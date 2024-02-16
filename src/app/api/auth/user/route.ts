908b0dd93a84a6ee3c9f6cb3bbbc088c6d19c273
ALIRAZA17 <ialidar2001@gmail.com>
2024-02-16 16:49:12 +0500

    changed route

---
+++ src/app/api/auth/user/route.ts
@@ -0,0 +1,51 @@
+import { prisma } from '@/prisma';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '../[...nextauth]/authOptions';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        username: session.username,
+        spaceId: session.spaceId,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+    return NextResponse.json(user);
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
+
+export async function PUT(request: Request) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+  const { name, email, phone_number, username, spaceId } = await request.json();
+
+  if (username !== session.username || spaceId !== session.spaceId) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { username_spaceId: { username: session.username, spaceId: session.spaceId } },
+      data: { name: name, email: email, phone_number: phone_number },
+    });
+
+    return NextResponse.json(updatedUser);
+  } catch (error) {
+    console.error('Error updating user:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
