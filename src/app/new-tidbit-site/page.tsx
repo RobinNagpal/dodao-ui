7970317fa7ec57e3cd114d00bc7b1c5e86f260af
ALIRAZA17 <ialidar2001@gmail.com>
2024-02-23 04:01:09 +0500

    added changes

--- src/app/new-tidbit-site/page.tsx
+++ src/app/new-tidbit-site/page.tsx
@@ -1,6 +1,5 @@
 import TidbitsSiteSetup from '@/app/new-tidbit-site/[stepId]/TidbitsSiteSetup';
 import React from 'react';
-import { StepId } from './steps';
 
 export default function NewTidbitSite() {
   return <TidbitsSiteSetup stepId={'user-details'} />;
