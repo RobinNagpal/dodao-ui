25e24b875d7abebeed28fd4db3ae215065ed2174
ALIRAZA17 <ialidar2001@gmail.com>
2024-02-23 04:01:27 +0500

    some changes

--- src/components/core/stepper/HorizontalStepperSimple.tsx
+++ src/components/core/stepper/HorizontalStepperSimple.tsx
@@ -15,7 +15,6 @@ interface StepperItemProps {
 const getStepStatus = (stepId: string, currentStepId: string): StepStatus => {
   const currentStepIndex = initialSteps.findIndex((step) => step.id === currentStepId);
   const stepIndex = initialSteps.findIndex((step) => step.id === stepId);
-
   if (stepIndex < currentStepIndex) {
     return 'complete';
   } else if (stepIndex === currentStepIndex) {
@@ -26,12 +25,15 @@ const getStepStatus = (stepId: string, currentStepId: string): StepStatus => {
 };
 
 const HorizontalStepperSimple: React.FC<StepperItemProps> = ({ currentStepId, steps }) => {
+  console.log('Steps: ', steps);
   return (
     <nav aria-label="Progress">
       <ol role="list" className="space-y-4 md:flex md:space-x-8 md:space-y-0">
         {steps.map((step) => {
           const index = steps.findIndex((s) => step.id === s.id) + 1;
-          const status = getStepStatus(steps[0].id, currentStepId);
+          const status = getStepStatus(step.id, currentStepId);
+
+          console.log('I am ');
           return (
             <li key={step.id} className="md:flex-1">
               {status === 'complete' ? (
@@ -42,6 +44,11 @@ const HorizontalStepperSimple: React.FC<StepperItemProps> = ({ currentStepId, st
               ) : status === 'current' ? (
                 <div className="flex flex-col border-l-4 border-indigo-600 py-2 pl-4 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4" aria-current="step">
                   <span className="text-sm font-medium text-indigo-600">{`Step ${index}`}</span>
+                  <span className="text-sm font-medium">{step.name}</span>
+                </div>
+              ) : status === 'upcoming' ? (
+                <div className="group flex flex-col border-l-4 border-gray-200 py-2 pl-4 hover:border-gray-300 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4">
+                  <span className="text-sm font-medium text-gray-500 group-hover:text-gray-700">{`Step ${index}`}</span>
                   <span className="text-sm font-medium">{step.name}</span>
                 </div>
               ) : (
