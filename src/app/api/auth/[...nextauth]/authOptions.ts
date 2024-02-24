71cbf3ac73bc5d83f4879fb9c11802ec1a2845f1
robin <robinnagpal.tiet@gmail.com>
2024-02-24 09:11:24 -0500

    Fix compilation

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,5 +1,6 @@
 // see: https://next-auth.js.org/configuration/options
 import { authorizeCrypto } from '@/app/api/auth/[...nextauth]/authorizeCrypto';
+import { headers } from 'next/headers';
 import { CustomPrismaAdapter } from './customPrismaAdapter';
 import { prisma } from '@/prisma';
 import { DoDaoJwtTokenPayload, Session } from '@/types/auth/Session';
@@ -120,6 +121,48 @@ export const authOptions: AuthOptions = {
         },
       },
       from: process.env.EMAIL_FROM,
+      sendVerificationRequest: ({ identifier: email, url, provider }) => {
+        // Capture the hostname from the request
+        const headersList = headers();
+        const host = headersList.get('x-forwarded-host') || headersList.get('host');
+        const httpsProto = headersList.get('x-forwarded-proto');
+        // Do whatever you want here, before the request is passed down to `NextAuth`
+
+        const { from } = provider;
+        const baseUrl = `${httpsProto}://${host}`;
+        const customUrl = url.replace(/^http?:\/\/[^/]+/, baseUrl);
+        const link = `${customUrl}&utm_source=${host}`;
+
+        // Email HTML body
+        const emailBody = `Your sign in link is <a href="${link}">here</a>.`;
+
+        console.log('Sending email to', email, 'from', from, 'with body', emailBody);
+        // Sending email via AWS SES
+        ses.sendEmail(
+          {
+            Source: from,
+            Destination: { ToAddresses: [email] },
+            Message: {
+              Subject: {
+                Data: 'Sign in to your account',
+              },
+              Body: {
+                Html: {
+                  Data: emailBody,
+                },
+              },
+            },
+          },
+          (err, info) => {
+            if (err) {
+              console.error(err);
+              throw new Error('Failed to send email');
+            } else {
+              console.log('Email sent: ', info);
+            }
+          }
+        );
+      },
     }),
   ],
   adapter: CustomPrismaAdapter(prisma) as Adapter,
