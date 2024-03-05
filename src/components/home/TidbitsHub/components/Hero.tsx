55248c1c6658ac9772dde83c8fbd1aebd3e5ad2a
Muhammad Sami Tariq <samitariq40@gmail.com>
2024-03-05 19:15:48 +0500

    Added GIF and removed 'as featured in' section

--- src/components/home/TidbitsHub/components/Hero.tsx
+++ src/components/home/TidbitsHub/components/Hero.tsx
@@ -83,25 +83,6 @@ export function Hero() {
               </PhoneFrame>
             </div>
           </div>
-          <div className="relative -mt-4 lg:col-span-7 lg:mt-0 xl:col-span-6">
-            <p className="text-center text-sm font-semibold lg:text-left">As featured in</p>
-            <ul role="list" className="mx-auto mt-8 flex max-w-xl flex-wrap justify-center gap-x-10 gap-y-8 lg:mx-0 lg:justify-start">
-              {[
-                ['Forbes', logoForbes],
-                ['TechCrunch', logoTechcrunch],
-                ['Wired', logoWired],
-                ['CNN', logoCnn, 'hidden xl:block'],
-                ['BBC', logoBbc],
-                ['CBS', logoCbs],
-                ['Fast Company', logoFastCompany],
-                ['HuffPost', logoHuffpost, 'hidden xl:block'],
-              ].map(([name, logo, className]) => (
-                <li key={name} className={clsx('flex', className)}>
-                  <Image src={logo} alt={name} className="h-8" unoptimized />
-                </li>
-              ))}
-            </ul>
-          </div>
         </div>
       </Container>
     </div>
