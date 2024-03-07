252aeca8af708672999ff24e4d849d14d3212e99
Muhammad Sami Tariq <samitariq40@gmail.com>
2024-03-07 23:11:44 +0500

    update grid

--- src/components/home/DoDAOHome/components/AcademySites.tsx
+++ src/components/home/DoDAOHome/components/AcademySites.tsx
@@ -89,7 +89,7 @@ export function AcademySites() {
         </div>
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">Serving Industry Leaders</h2>
-          <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+          <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-6">
             <Image
               className="col-span-2 col-start-2 max-h-28 w-full object-contain sm:col-start-auto lg:col-span-1"
               src={Near}
