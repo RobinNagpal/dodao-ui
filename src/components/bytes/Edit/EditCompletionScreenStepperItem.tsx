39a7d5560d43f6b021d8cae8abe333b5ef9d5d52
ALIRAZA17 <ialidar2001@gmail.com>
2024-03-12 20:36:15 +0500

    some changes

--- src/components/bytes/Edit/EditCompletionScreenStepperItem.tsx
+++ src/components/bytes/Edit/EditCompletionScreenStepperItem.tsx
@@ -1,44 +1,64 @@
-import { EditByteType } from '@/components/bytes/Edit/editByteHelper';
+import { EditByteType, KeyOfByteInput, UpdateByteFunctions } from '@/components/bytes/Edit/editByteHelper';
 import IconButton from '@/components/core/buttons/IconButton';
 import { IconTypes } from '@/components/core/icons/IconTypes';
 import MarkdownEditor from '@/components/app/Markdown/MarkdownEditor';
 import Input from '@/components/core/input/Input';
 import { InputWithButton } from '@/components/core/input/InputWithButton';
-import { ImageType, SpaceWithIntegrationsFragment } from '@/graphql/generated/generated-types';
-import React, { useState } from 'react';
+import { CompletionScreen, ImageType, SpaceWithIntegrationsFragment } from '@/graphql/generated/generated-types';
+import React from 'react';
 import styled from 'styled-components';
 
 interface EditCompletionScreenStepperItemProps {
   space: SpaceWithIntegrationsFragment;
   byte: EditByteType;
+  updateByteCompletionScreen: (field: keyof CompletionScreen, value: any) => void;
+  onRemoveCompletionScreen: () => void;
 }
 
 const StyledStepItemContainer = styled.div`
   width: 100%;
 `;
 
-const StepItemWrapper = styled.div<{ hasError: boolean }>`
-  border: ${(props) => (props.hasError ? '1px solid red' : '1px solid var(--border-color)')};
-  border-radius: 0.5rem;
-  padding: 1rem;
-`;
-export default function EditCompletionScreenStepperItem({ space, byte }: EditCompletionScreenStepperItemProps) {
+export default function EditCompletionScreenStepperItem({
+  space,
+  byte,
+  onRemoveCompletionScreen,
+  updateByteCompletionScreen,
+}: EditCompletionScreenStepperItemProps) {
+  const handleDelete = () => {
+    onRemoveCompletionScreen();
+  };
+
   return (
     <StyledStepItemContainer className="w-full">
       <div>
         <div style={{ minHeight: '20px' }}>
-          <IconButton className="float-right ml-2" iconName={IconTypes.Trash} removeBorder disabled={byte.steps.length === 1} onClick={() => {}} />
+          <IconButton className="float-right ml-2" iconName={IconTypes.Trash} removeBorder disabled={byte.steps.length === 1} onClick={handleDelete} />
         </div>
         <div className="w-full mb-4">
-          <Input>Name*</Input>
+          <Input
+            modelValue={byte.completionScreen?.name}
+            onUpdate={(e) => {
+              updateByteCompletionScreen('name', e?.toString());
+              console.log('I am byte completion screen: ', byte.completionScreen);
+            }}
+          >
+            Name*
+          </Input>
         </div>
         <div className="w-full mb-4">
-          <InputWithButton buttonLabel={'Set Image'} inputLabel={'Image Url'} onButtonClick={() => {}} onInputUpdate={() => {}} inputModelValue={''} />
+          <InputWithButton
+            buttonLabel={'Set Image'}
+            inputLabel={'Image Url'}
+            onButtonClick={() => {}}
+            onInputUpdate={() => {}}
+            inputModelValue={byte.completionScreen?.imageUrl ?? ''}
+          />
         </div>
 
         <MarkdownEditor
           id={''}
-          modelValue={''}
+          modelValue={byte.completionScreen?.content}
           placeholder={'Contents'}
           onUpdate={() => {}}
           spaceId={space.id}
