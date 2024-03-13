61bcb964af5ee0e5c89fb39c711dfb9655dfe52c
ALIRAZA17 <ialidar2001@gmail.com>
2024-03-13 20:16:57 +0500

    changes

--- src/components/bytes/Edit/EditCompletionScreenStepperItem.tsx
+++ src/components/bytes/Edit/EditCompletionScreenStepperItem.tsx
@@ -4,16 +4,18 @@ import { IconTypes } from '@/components/core/icons/IconTypes';
 import MarkdownEditor from '@/components/app/Markdown/MarkdownEditor';
 import Input from '@/components/core/input/Input';
 import { InputWithButton } from '@/components/core/input/InputWithButton';
-import { CompletionScreen, ImageType, SpaceWithIntegrationsFragment } from '@/graphql/generated/generated-types';
+import { CompletionScreen, CompletionScreenItemInput, ImageType, SpaceWithIntegrationsFragment } from '@/graphql/generated/generated-types';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import SelectImageInputModal from '@/components/app/Image/SelectImageInputModal';
+import { InputType } from '@/types/deprecated/models/enums';
+import CompletionScreenItemModal from '@/components/app/Modal/CompletionScreenItem/CompletionScreenItemModal';
 
 interface EditCompletionScreenStepperItemProps {
   space: SpaceWithIntegrationsFragment;
   byte: EditByteType;
   updateByteCompletionScreen: (field: keyof CompletionScreen, value: any) => void;
-  onRemoveCompletionScreen: () => void;
+  removeCompletionScreen: () => void;
 }
 
 const StyledStepItemContainer = styled.div`
@@ -23,27 +25,44 @@ const StyledStepItemContainer = styled.div`
 export default function EditCompletionScreenStepperItem({
   space,
   byte,
-  onRemoveCompletionScreen,
   updateByteCompletionScreen,
+  removeCompletionScreen,
 }: EditCompletionScreenStepperItemProps) {
   const [selectImageUploadModal, setSelectImageUploadModal] = useState(false);
-  const handleDelete = () => {
-    onRemoveCompletionScreen();
-  };
+  const [modalAddButtonInput, setModalAddButtonInput] = useState(false);
 
   const updateCompletionScreenImageUrl = (imageUrl: string | null) => {
     updateByteCompletionScreen('imageUrl', imageUrl);
   };
 
+  const handleDelete = () => {
+    removeCompletionScreen();
+  };
+
   const updateCompletionScreenContent = (content: string) => {
     updateByteCompletionScreen('content', content);
   };
+  function addButton() {
+    const input: CompletionScreenItemInput = {
+      uuid: 'myinput',
+      label: 'Button',
+      link: 'google',
+    };
+    updateByteCompletionScreen('items', [input]);
+  }
 
   return (
     <StyledStepItemContainer className="w-full">
       <div>
         <div style={{ minHeight: '20px' }}>
-          <IconButton className="float-right ml-2" iconName={IconTypes.Trash} removeBorder disabled={byte.steps.length === 1} onClick={handleDelete} />
+          <IconButton
+            className="float-right ml-2"
+            iconName={IconTypes.GuideAddIcon}
+            disabled={byte.completionScreen?.items.length != undefined ? byte.completionScreen?.items.length >= 1 : false}
+            removeBorder
+            onClick={() => setModalAddButtonInput(true)}
+          />
+          <IconButton className="float-right ml-2" iconName={IconTypes.Trash} removeBorder onClick={() => handleDelete()} />
         </div>
         <div className="w-full mb-4">
           <Input
@@ -93,6 +112,20 @@ export default function EditCompletionScreenStepperItem({
           }}
         />
       )}
+
+      {modalAddButtonInput && (
+        <CompletionScreenItemModal
+          open={modalAddButtonInput}
+          onClose={() => setModalAddButtonInput(false)}
+          onAddButton={() => {
+            addButton();
+            console.log('Button Added: ', byte.completionScreen);
+          }}
+        />
+      )}
     </StyledStepItemContainer>
   );
+}
+function uuidv4(): string {
+  throw new Error('Function not implemented.');
 }
