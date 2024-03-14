09b27984cf1928cb5d8e06aa1cb8c1460e5b3c82
ALIRAZA17 <ialidar2001@gmail.com>
2024-03-14 19:50:14 +0500

    some changes

--- src/types/errors/byteErrors.ts
+++ src/types/errors/byteErrors.ts
@@ -1,7 +1,20 @@
 import { StepError } from '@/types/errors/error';
 
+export interface CompletionScreenItemErrors {
+  label?: boolean;
+  link?: boolean;
+}
+
+export interface CompletionScreenErrors {
+  name?: boolean;
+  content?: boolean;
+  imageUrl?: boolean;
+  items?: Record<string, CompletionScreenItemErrors>;
+}
+
 export interface ByteErrors {
   name?: boolean;
   content?: boolean;
   steps?: Record<string, StepError>;
+  completionScreen?: CompletionScreenErrors;
 }
