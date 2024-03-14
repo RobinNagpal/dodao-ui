9286e938b6ec418ad5b8036e917c8d8500559813
robin <robinnagpal.tiet@gmail.com>
2024-03-14 10:43:39 -0400

    Add tab switching

--- src/components/home/TidbitsSite/TidbitsSiteHome.tsx
+++ src/components/home/TidbitsSite/TidbitsSiteHome.tsx
@@ -1,24 +1,34 @@
 import ByteCollectionsGrid from '@/components/byteCollection/View/ByteCollectionsGrid';
+import BytesGrid from '@/components/bytes/List/BytesGrid';
 import PageWrapper from '@/components/core/page/PageWrapper';
-import { ByteCollectionFragment, SpaceWithIntegrationsFragment } from '@/graphql/generated/generated-types';
+import TidbitsSiteTabs from '@/components/home/TidbitsSite/TidbitsSiteTabs';
+import { ByteCollectionFragment, ByteSummaryFragment, SpaceWithIntegrationsFragment } from '@/graphql/generated/generated-types';
 // import { useRouter } from 'next/navigation';
 import React from 'react';
 
 export interface TidbitsSiteHomeProps {
   space: SpaceWithIntegrationsFragment;
   byteCollections: ByteCollectionFragment[];
+  bytes: ByteSummaryFragment[];
+  selectedTabId?: string;
 }
 export default function TidbitsSiteHome(props: TidbitsSiteHomeProps) {
   const { space, byteCollections } = props;
+
   return (
     space && (
       <PageWrapper>
-        <ByteCollectionsGrid
-          byteCollections={byteCollections}
-          space={space}
-          byteCollectionType={'byteCollection'}
-          byteCollectionsPageUrl={`/tidbit-collections`}
-        />
+        <TidbitsSiteTabs selectedTabId={props.selectedTabId} />
+        {props.selectedTabId === 'Tidbits' ? (
+          <BytesGrid space={props.space} byteType={'byte'} bytes={props.bytes} baseByteViewUrl={`/tidbits/view`} />
+        ) : (
+          <ByteCollectionsGrid
+            byteCollections={byteCollections}
+            space={space}
+            byteCollectionType={'byteCollection'}
+            byteCollectionsPageUrl={`/tidbit-collections`}
+          />
+        )}
       </PageWrapper>
     )
   );
---
+++ src/components/home/TidbitsSite/TidbitsSiteTabs.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import PrivateComponent from '@/components/core/PrivateComponent';
+import TabsWithUnderline, { TabItem } from '@/components/core/tabs/TabsWithUnderline';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+export enum TabIds {
+  Tidbits = 'Tidbits',
+  TidbitCollections = 'TidbitCollections',
+}
+export default function TidbitsSiteTabs({ selectedTabId }: { selectedTabId?: string }) {
+  const router = useRouter();
+
+  const tabs: TabItem[] = [
+    {
+      id: TabIds.Tidbits,
+      label: 'Tidbits',
+    },
+
+    {
+      id: TabIds.TidbitCollections,
+      label: 'Tidbit Collections',
+    },
+  ];
+
+  return (
+    <PrivateComponent>
+      <div className="w-full flex justify-end mb-6">
+        <TabsWithUnderline
+          selectedTabId={selectedTabId || TabIds.TidbitCollections}
+          setSelectedTabId={(id) => router.push('/?selectedTabId=' + id)}
+          tabs={tabs}
+          className="w-96"
+        />
+      </div>
+    </PrivateComponent>
+  );
+}
