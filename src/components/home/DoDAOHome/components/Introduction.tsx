60a93b019137835fcf43a8abd2b4cff7bd3999f6
ALIRAZA17 <ialidar2001@gmail.com>
2024-03-16 03:21:04 +0500

    some changes in intro section

--- src/components/home/DoDAOHome/components/Introduction.tsx
+++ src/components/home/DoDAOHome/components/Introduction.tsx
@@ -14,7 +14,7 @@ export function Introduction() {
           <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
             <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
               <div className="relative pl-16">
-                <dt className="text-base font-semibold leading-7">
+                <dt className="text-base font-bold leading-7">
                   <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
                     <svg className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                       <path
@@ -24,14 +24,15 @@ export function Introduction() {
                       />
                     </svg>
                   </div>
-                  Push to deploy
+                  Tidbits Hub
                 </dt>
                 <dd className="mt-2 text-base leading-7">
-                  Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.
+                  This is our latest product where you can create bite sized information, which we call tidbits. These tidbits can then be published to social
+                  media or can be used to create printed materials like pamphlets.
                 </dd>
               </div>
               <div className="relative pl-16">
-                <dt className="text-base font-semibold leading-7">
+                <dt className="text-base font-bold leading-7">
                   <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
                     <svg className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                       <path
@@ -41,14 +42,15 @@ export function Introduction() {
                       />
                     </svg>
                   </div>
-                  SSL certificates
+                  Academy Sites
                 </dt>
                 <dd className="mt-2 text-base leading-7">
-                  Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.
+                  These are detailed sources of information. With a combination of various innovative formats like guides, tidbits, simulations etc. we make
+                  sure users are able to comprehend complex concepts easily.
                 </dd>
               </div>
               <div className="relative pl-16">
-                <dt className="text-base font-semibold leading-7">
+                <dt className="text-base font-bold leading-7">
                   <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
                     <svg className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                       <path
@@ -58,14 +60,15 @@ export function Introduction() {
                       />
                     </svg>
                   </div>
-                  Simple queues
+                  AI Chatbot
                 </dt>
                 <dd className="mt-2 text-base leading-7">
-                  Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.
+                  Unlike many AI chatbots that can be inaccurate and share irrelevant information, our Chatbot stands out. It is trained on specific data,
+                  ensuring accurate responses. It also gives credit to the original source of information.
                 </dd>
               </div>
               <div className="relative pl-16">
-                <dt className="text-base font-semibold leading-7">
+                <dt className="text-base font-bold leading-7">
                   <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
                     <svg className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                       <path
@@ -75,10 +78,29 @@ export function Introduction() {
                       />
                     </svg>
                   </div>
-                  Advanced security
+                  Blockchain Courses
                 </dt>
                 <dd className="mt-2 text-base leading-7">
-                  Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.
+                  We have created a curriculum covering NFTs, DeFi, Layer 2 solutions, and more, providing essential knowledge and practical skills that will
+                  set you apart in this rapidly evolving industry.
+                </dd>
+              </div>
+              <div className="relative pl-16">
+                <dt className="text-base font-bold leading-7">
+                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                    <svg className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33"
+                      />
+                    </svg>
+                  </div>
+                  Blockchain Development
+                </dt>
+                <dd className="mt-2 text-base leading-7 m-auto">
+                  If you are looking for experts who can help you accelerate your development, we provide specialized development services for strategic
+                  projects.
                 </dd>
               </div>
             </dl>
