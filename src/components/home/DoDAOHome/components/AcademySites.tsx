6f4d0bd1afaebd978a49e1303999cd37414f47fc
ALIRAZA17 <ialidar2001@gmail.com>
2024-03-18 19:01:12 +0500

    some changes

--- src/components/home/DoDAOHome/components/AcademySites.tsx
+++ src/components/home/DoDAOHome/components/AcademySites.tsx
@@ -59,57 +59,6 @@ export function AcademySites() {
           ))}
         </dl>
       </Container>
-      {/* </sdiv> */}
-      <div className="relative isolate mt-32 sm:mt-48">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">Serving Industry Leaders</h2>
-          <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-6">
-            <Image
-              className="col-span-2 col-start-2 max-h-28 w-full object-contain sm:col-start-auto lg:col-span-1"
-              src={Near}
-              alt="Near"
-              width={158}
-              height={150}
-            />
-            <Image
-              className="col-span-2 col-start-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1"
-              src={Balancer}
-              alt="Balancer"
-              width={158}
-              height={150}
-            />
-
-            <Image
-              className="col-span-2 col-start-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1"
-              src={Harmony}
-              alt="Harmony"
-              width={158}
-              height={150}
-            />
-            <Image
-              className="col-span-2 col-start-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1"
-              src={Compound}
-              alt="Compound"
-              width={158}
-              height={150}
-            />
-            <Image
-              className="col-span-2 col-start-2 max-h-28 w-full object-contain sm:col-start-auto lg:col-span-1"
-              src={AAVE}
-              alt="AAVE"
-              width={158}
-              height={150}
-            />
-            <Image
-              className="col-span-2 col-start-2 max-h-28 w-full object-contain sm:col-start-auto lg:col-span-1"
-              src={Uniswap}
-              alt="Uniswap"
-              width={158}
-              height={150}
-            />
-          </div>
-        </div>
-      </div>
     </section>
   );
 }
