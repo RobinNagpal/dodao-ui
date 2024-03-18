64547a9fa0bde9864efe752d6e1287235bc4952f
ALIRAZA17 <ialidar2001@gmail.com>
2024-03-18 16:35:10 +0500

    added gif and text changes

--- src/components/home/DoDAOHome/components/TidbitsHub.tsx
+++ src/components/home/DoDAOHome/components/TidbitsHub.tsx
@@ -1,76 +1,63 @@
-export function TidbitsHub() {
+import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid';
+import tidbits_demo from '@/images/TidbitsHub/GIFs/tidbits_demo.gif';
+import Image from 'next/image';
+
+const features = [
+  {
+    name: 'Short videos:',
+    description: 'Users can watch concise, one-minute videos embedded within tidbits as we are aware that everyone has their own unique learning style.',
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: 'Shareable Content:',
+    description: 'These tidbits can then be published to social media or can be used to create printed materials like pamphlets.',
+    icon: LockClosedIcon,
+  },
+  {
+    name: 'Call to Actions:',
+    description: 'We can embed CTAs within tidbits that lead customers directly to application forms etc.',
+    icon: ServerIcon,
+  },
+];
+
+export default function TidbitsHub() {
   return (
-    <section id="tidibits-hub" aria-labelledby="tidibits-hub-title" className="scroll-mt-14 py-16 sm:scroll-mt-32 sm:py-20 lg:py-32 text-left">
-      <div className="overflow-hidden">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-            <div>
-              <div className="lg:max-w-lg">
-                <p className="text-3xl font-bold tracking-tight sm:text-4xl">Tidbits Hub</p>
-                <p className="mt-6 text-lg leading-8">
-                  At Tidbits Hub, we&apos;ve tapped into understanding precisely what format and style of content resonate most with today&apos;s consumers.
-                  Using this platform you can craft short, straightforward bite sized pieces of information which we call &quot;tidbits&quot; with each piece
-                  containing 5-10 sentences. It provides quick understanding within 5 minutes, without the need for users to go through extensive material.
-                </p>
-                <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 lg:max-w-none">
-                  <div className="relative pl-9">
-                    <dt className="inline font-semibold">
-                      <svg className="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path
-                          fill-rule="evenodd"
-                          d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
-                          clip-rule="evenodd"
-                        />
-                      </svg>
-                      Short videos:
-                    </dt>
-                    <dd className="inline">
-                      &nbsp;Users can watch concise, one-minute videos embedded within tidbits as we are aware that everyone has their own unique learning
-                      style.
-                    </dd>
+    <div className="overflow-hidden py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div className="lg:ml-auto lg:pl-4">
+            <div className="lg:max-w-lg">
+              <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">Tidbits Hub</p>
+              <p className="mt-6 text-lg leading-8">
+                At Tidbits Hub, we&apos;ve tapped into understanding precisely what format and style of content resonate most with today&apos;s consumers. Using
+                this platform you can craft short, straightforward bite sized pieces of information which we call &quot;tidbits&quot; with each piece containing
+                5-10 sentences. It provides quick understanding within 5 minutes, without the need for users to go through extensive material.
+              </p>
+              <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 lg:max-w-none">
+                {features.map((feature) => (
+                  <div key={feature.name} className="relative pl-9">
+                    <dt className="inline font-bold">
+                      <feature.icon className="absolute left-1 top-1 h-5 w-5 text-indigo-600" aria-hidden="true" />
+                      {feature.name}
+                    </dt>{' '}
+                    <dd className="inline">{feature.description}</dd>
                   </div>
-                  <div className="relative pl-9">
-                    <dt className="inline font-semibold">
-                      <svg className="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path
-                          fill-rule="evenodd"
-                          d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-                          clip-rule="evenodd"
-                        />
-                      </svg>
-                      Shareable Content:
-                    </dt>
-                    <dd className="inline">
-                      &nbsp;These tidbits can then be published to social media or can be used to create printed materials like pamphlets.
-                    </dd>
-                  </div>
-                  <div className="relative pl-9">
-                    <dt className="inline font-semibold">
-                      <svg className="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z" />
-                        <path
-                          fill-rule="evenodd"
-                          d="M4 13a2 2 0 100 4h12a2 2 0 100-4H4zm11.24 2a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z"
-                          clip-rule="evenodd"
-                        />
-                      </svg>
-                      Call to Actions:
-                    </dt>
-                    <dd className="inline">&nbsp;We can embed CTAs within tidbits that lead customers directly to application forms etc.</dd>
-                  </div>
-                </dl>
-              </div>
+                ))}
+              </dl>
             </div>
-            <img
-              src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+          </div>
+          <div className="flex items-start justify-end lg:order-first">
+            <Image
+              src={tidbits_demo}
               alt="Product screenshot"
-              className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
-              width="2432"
-              height="1442"
+              className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
+              style={{
+                width: 'auto',
+              }}
             />
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
