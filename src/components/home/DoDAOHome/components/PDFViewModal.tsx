139191248bba9c4b37f0965bf30948489e7629b0
ALIRAZA17 <ialidar2001@gmail.com>
2024-03-19 20:09:54 +0500

    some changes

--- src/components/home/DoDAOHome/components/PDFViewModal.tsx
+++ src/components/home/DoDAOHome/components/PDFViewModal.tsx
@@ -1,13 +1,13 @@
+import FullPageModal from '@/components/core/modals/FullPageModal';
 import FullScreenModal from '@/components/core/modals/FullScreenModal';
 export interface PDFViewModalProps {
   onClose: () => void;
 }
 
 export const PDFViewModal = ({ onClose }: PDFViewModalProps) => {
-  const pdfPath = 'public/blockchain_pdf.pdf';
   return (
-    <FullScreenModal open={true} onClose={onClose} title={'Blockchain BootCamp'}>
-      <iframe src={pdfPath} className="w-full h-full"></iframe>
-    </FullScreenModal>
+    <FullPageModal open={true} onClose={onClose} title={'Blockchain BootCamp'}>
+      <iframe src={"https://www.clickdimensions.com/links/TestPDFfile.pdf"}></iframe>
+    </FullPageModal>
   );
 };
