5061ac755bfcf1445c8a23e6a9d8b76c9f51b009
ALIRAZA17 <ialidar2001@gmail.com>
2024-03-20 17:41:44 +0500

    some changes

--- src/components/home/DoDAOHome/components/BlockchainDevelopmentTooling.tsx
+++ src/components/home/DoDAOHome/components/BlockchainDevelopmentTooling.tsx
@@ -1,6 +1,6 @@
 import { Container } from './Container';
 import { SectionHeading } from './SectionHeading';
-import BlockchainDevelopmentTool from '@/images/DoDAOHomePage/blockchainDevelopmentTooling.jpg';
+import BlockchainDevelopmentTool from '@/images/DoDAOHomePage/blockchainDevelopmentTooling.png';
 import Image from 'next/image';
 
 export function BlockchainDevelopmentTooling() {
@@ -18,12 +18,16 @@ export function BlockchainDevelopmentTooling() {
           <div className="mx-auto max-w-7xl lg:flex lg:justify-between lg:px-8 xl:justify-end">
             <div className="lg:flex lg:w-1/2 lg:shrink lg:grow-0 xl:absolute xl:inset-y-0 xl:right-1/2 xl:w-1/2">
               <div className="relative h-80 lg:-ml-8 lg:h-auto lg:w-full lg:grow xl:ml-0">
-                <Image className="absolute inset-0 mt-6 h-full w-full rounded-2xl object-cover" src={BlockchainDevelopmentTool} alt="" />
+                <Image
+                  className="absolute inset-0 mt-6 h-full w-full rounded-2xl object-cover"
+                  src={BlockchainDevelopmentTool}
+                  alt="Blockchain Development Tooling"
+                />
               </div>
             </div>
             <div className="px-6 lg:contents">
-              <div className="mx-auto max-w-2xl pb-24 pt-16 sm:pb-32 sm:pt-20 lg:ml-8 lg:mr-0 lg:w-full lg:max-w-lg lg:flex-none lg:pt-32 xl:w-1/2">
-                <h1 className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">Blockchain Development Tooling</h1>
+              <div className="mx-auto max-w-2xl pb-24 pt-16 sm:pb-32 sm:pt-20 lg:ml-8 lg:mr-0 lg:w-full lg:max-w-lg lg:flex-none lg:pt-16 xl:w-1/2">
+                <h1 className="text-3xl font-bold sm:text-4xl">Blockchain Development Tooling</h1>
                 <p className="mt-6 text-lg leading-8">
                   With over five years of experience in blockchain development, we specialize in collaborating with strategic projects to speed up their growth.
                   Our focus is on crafting custom blockchain solutions designed specifically for your business requirements. By adopting a flexible approach, we
