d9b98992554da0a757b0ae39f94b5fb950efaecb
ALIRAZA17 <ialidar2001@gmail.com>
2024-03-20 17:41:28 +0500

    some changes in logos

--- src/components/home/DoDAOHome/components/Hero.tsx
+++ src/components/home/DoDAOHome/components/Hero.tsx
@@ -35,64 +35,27 @@ export function Hero() {
 
       <div className="relative isolate mt-32 sm:mt-48">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">Trusted by the top blockchain companies</h2>
+          <h2 className="text-center text-lg font-semibold leading-8">Trusted by the top blockchain companies</h2>
           <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-6">
+            <Image className="col-span-2 col-start-2 object-contain sm:col-start-auto lg:col-span-1" src={Near} alt="Near" width={150} height={150} />
+            <Image className="col-span-2 col-start-2 object-contain sm:col-start-auto lg:col-span-1" src={Balancer} alt="Balancer" width={150} height={150} />
+            <Image className="col-span-2 col-start-2 object-contain sm:col-start-auto lg:col-span-1" src={Harmony} alt="Harmony" width={150} height={150} />
+            <Image className="col-span-2 col-start-2 object-contain sm:col-start-auto lg:col-span-1" src={Compound} alt="Compound" width={150} height={150} />
+            <Image className="col-span-2 col-start-2 object-contain sm:col-start-auto lg:col-span-1" src={AAVE} alt="AAVE" width={150} height={150} />
+            <Image className="col-span-2 col-start-2 object-contain sm:col-start-auto lg:col-span-1" src={Uniswap} alt="Uniswap" width={150} height={150} />
             <Image
-              className="col-span-2 col-start-2 max-h-28 w-full object-contain sm:col-start-auto lg:col-span-1"
-              src={Near}
-              alt="Near"
-              width={158}
-              height={150}
-            />
-            <Image
-              className="col-span-2 col-start-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1"
-              src={Balancer}
-              alt="Balancer"
-              width={158}
-              height={150}
-            />
-
-            <Image
-              className="col-span-2 col-start-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1"
-              src={Harmony}
-              alt="Harmony"
-              width={158}
-              height={150}
-            />
-            <Image
-              className="col-span-2 col-start-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1"
-              src={Compound}
-              alt="Compound"
-              width={158}
-              height={150}
-            />
-            <Image
-              className="col-span-2 col-start-2 max-h-28 w-full object-contain sm:col-start-auto lg:col-span-1"
-              src={AAVE}
-              alt="AAVE"
-              width={158}
-              height={150}
-            />
-            <Image
-              className="col-span-2 col-start-2 max-h-28 w-full object-contain sm:col-start-auto lg:col-span-1"
-              src={Uniswap}
-              alt="Uniswap"
-              width={158}
-              height={150}
-            />
-            <Image
-              className="col-span-2 col-start-2 max-h-28 w-full object-contain sm:col-start-auto lg:col-span-1 lg:col-start-3"
+              className="col-span-2 col-start-2 object-contain sm:col-start-auto lg:col-span-1 lg:col-start-3"
               src={Arbitrum}
               alt="Arbitrum"
-              width={158}
-              height={150}
+              width={200}
+              height={200}
             />
             <Image
-              className="col-span-2 col-start-2 max-h-28 w-full object-contain sm:col-start-auto lg:col-span-1 lg:col-start-4"
+              className="col-span-2 col-start-2 object-contain sm:col-start-auto lg:col-span-1 lg:col-start-4"
               src={Optimism}
               alt="Optimism"
-              width={158}
-              height={150}
+              width={200}
+              height={200}
             />
           </div>
         </div>
