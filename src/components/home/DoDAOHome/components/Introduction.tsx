d3f1ccff0d7feba7e37bd8a03458dc7bbf892d86
ALIRAZA17 <ialidar2001@gmail.com>
2024-03-20 17:41:17 +0500

    some changes

--- src/components/home/DoDAOHome/components/Introduction.tsx
+++ src/components/home/DoDAOHome/components/Introduction.tsx
@@ -85,9 +85,33 @@ export function Introduction() {
                   set you apart in this rapidly evolving industry.
                 </dd>
               </div>
-              <div className="relative pl-16">
+              {/* <div className="lg:col-start-1 lg:col-span-2">
+                <div className="relative pl-16">
+                  <dt className="text-base font-bold leading-7">
+                    <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                      <svg className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33"
+                        />
+                      </svg>
+                    </div>
+                    Blockchain Development
+                  </dt>
+                  <dd className="mt-2 text-base leading-7">
+                    If you are looking for experts who can help you accelerate your development, we provide specialized development services for strategic
+                    projects.
+                  </dd>
+                </div>
+              </div> */}
+            </dl>
+
+            {/* <div className="flex justify-center mt-20 lg:mt-0">
+              <div className="flex flex-col items-center pl-16">
                 <dt className="text-base font-bold leading-7">
-                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                  <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                    {' '}
                     <svg className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
                       <path
                         strokeLinecap="round"
@@ -98,12 +122,12 @@ export function Introduction() {
                   </div>
                   Blockchain Development
                 </dt>
-                <dd className="mt-2 text-base leading-7 m-auto">
+                <dd className="mt-2 text-base leading-7 text-center">
                   If you are looking for experts who can help you accelerate your development, we provide specialized development services for strategic
                   projects.
                 </dd>
               </div>
-            </dl>
+            </div> */}
           </div>
         </div>
       </div>
