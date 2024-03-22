afedf35238fae1f84311fecc1d0d6a5a40956458
ALIRAZA17 <ialidar2001@gmail.com>
2024-03-22 18:51:43 +0500

    Update AcademySites component styles

--- src/components/home/DoDAOHome/components/AcademySites.tsx
+++ src/components/home/DoDAOHome/components/AcademySites.tsx
@@ -65,7 +65,7 @@ export function AcademySites() {
               <Image src={Academy_site} alt="Academy Site Gif" className="mb-[-12%] rounded-xl ring-1 ring-gray-900/10" width={2432} height={1442} />
             </div>
           </div>
-          <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
+          <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-28 md:mt-32 lg:mt-44 lg:px-8">
             <dl className="mt-24 mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
               {features.map((feature) => (
                 <div key={feature.name} className="relative pl-9">
