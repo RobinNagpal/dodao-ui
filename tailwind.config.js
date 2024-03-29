f7e6751e190d9026a943709b17a081a7a54a2a25
robin <robinnagpal.tiet@gmail.com>
2024-03-29 19:15:19 -0400

    Add inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const { fontSize } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
@@ -7,6 +9,16 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      fontSize: Object.fromEntries(
+        Object.entries(fontSize).map(([key, value]) => {
+          const [size, lineHeight] = Array.isArray(value) ? value : [value];
+          // Inter font is a bit smaller than the default, so we increase the size a bit
+          return [key, [parseFloat(size) * 1.1 + 'rem', lineHeight]];
+        })
+      ),
     },
     screens: {
       xs: '320px',
