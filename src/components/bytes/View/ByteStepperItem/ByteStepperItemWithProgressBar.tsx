73fa140322d8b8ddc43058d12fcfcaa9da815ce5
robin <robinnagpal.tiet@gmail.com>
2024-04-05 14:30:51 -0400

    Fix styling issues

--- src/components/bytes/View/ByteStepperItem/ByteStepperItemWithProgressBar.tsx
+++ src/components/bytes/View/ByteStepperItem/ByteStepperItemWithProgressBar.tsx
@@ -175,7 +175,7 @@ function ByteStepperItemWithProgressBar({ viewByteHelper, step, byte, space, set
 
   return (
     <div className={`w-full flex flex-col justify-between py-12 px-4 md:px-8  ${styles.stepContainer}`}>
-      <div className={`w-full overflow-scroll flex flex-col ${transitionClasses[transitionState]} ${styles.stepContents}`}>
+      <div className={`w-full overflow-y-auto flex flex-col ${transitionClasses[transitionState]} ${styles.stepContents}`}>
         <div className="flex flex-col flex-grow justify-center align-center">
           {!stepItems.some(isQuestion) && !isShortScreen && step.imageUrl && (
             <div className="flex justify-center align-center ">
--- src/components/spaces/Edit/Theme/UpdateThemeModal.tsx
+++ src/components/spaces/Edit/Theme/UpdateThemeModal.tsx
@@ -2,6 +2,7 @@ import { CssTheme, ThemeKey, themes } from '@/app/themes';
 import ByteCollectionsCard from '@/components/byteCollection/ByteCollections/ByteCollectionsCard/ByteCollectionsCard';
 import Button from '@/components/core/buttons/Button';
 import FullScreenModal from '@/components/core/modals/FullScreenModal';
+import PageWrapper from '@/components/core/page/PageWrapper';
 import { useNotificationContext } from '@/contexts/NotificationContext';
 import { ProjectByteCollectionFragment, SpaceWithIntegrationsFragment, ThemeColors, useUpdateThemeColorsMutation } from '@/graphql/generated/generated-types';
 import { useI18 } from '@/hooks/useI18';
@@ -86,41 +87,43 @@ export default function UpdateThemeModal({ space, open, onClose, byteCollection 
   console.log('themeStyles', themeStyles);
   return (
     <FullScreenModal open={open} onClose={onClose} title="Theme Settings">
-      <div style={{ ...themeStyles }}>
-        <div className="mt-4">
-          <div className="flex flex-col md:flex-row flex-wrap">
-            <div className="w-full md:w-1/2 mt-4">
-              <h1 className="font-bold text-2xl mb-4">Theme Details</h1>
-              {Object.entries(ColorLabels).map((e) => {
-                const [colorKey, label] = e as [ThemeColorsKeys, string];
-                const colorValue = themeColors[colorKey];
-                return (
-                  <div key={colorKey} className="flex justify-between mb-2">
-                    <label className="ml-7">{label}</label>
-                    <div className="grid grid-cols-2	">
-                      <input type="color" className="w-12 h-8 mr-8" value={colorValue} onChange={(e) => handleColorChange(colorKey, e.target.value)} />
-                      <div>{colorValue}</div>
+      <PageWrapper>
+        <div style={{ ...themeStyles }}>
+          <div className="mt-4">
+            <div className="flex flex-col md:flex-row flex-wrap">
+              <div className="w-full md:w-1/2 mt-4">
+                <h1 className="font-bold text-2xl mb-4">Theme Details</h1>
+                {Object.entries(ColorLabels).map((e) => {
+                  const [colorKey, label] = e as [ThemeColorsKeys, string];
+                  const colorValue = themeColors[colorKey];
+                  return (
+                    <div key={colorKey} className="flex justify-between mb-2">
+                      <label className="ml-7">{label}</label>
+                      <div className="grid grid-cols-2	">
+                        <input type="color" className="w-12 h-8 mr-8" value={colorValue} onChange={(e) => handleColorChange(colorKey, e.target.value)} />
+                        <div>{colorValue}</div>
+                      </div>
                     </div>
-                  </div>
-                );
-              })}
-            </div>
-            <div className="flex justify-center items-center w-full md:mt-0 md:w-1/2 p-2 md:p-4">
-              <ByteCollectionsCard
-                isEditingAllowed={false}
-                byteCollection={byteCollection}
-                byteCollectionType={'projectByteCollection'}
-                viewByteBaseUrl={'/'}
-              />
+                  );
+                })}
+              </div>
+              <div className="flex justify-center items-center w-full md:mt-0 md:w-1/2 p-2 md:p-4">
+                <ByteCollectionsCard
+                  isEditingAllowed={false}
+                  byteCollection={byteCollection}
+                  byteCollectionType={'projectByteCollection'}
+                  viewByteBaseUrl={'/'}
+                />
+              </div>
             </div>
           </div>
+          <div className="p-6 mt-4 flex items-center justify-center gap-x-6">
+            <Button variant="contained" primary onClick={upsertThemeColors}>
+              Save
+            </Button>
+          </div>
         </div>
-        <div className="p-6 mt-4 flex items-center justify-center gap-x-6">
-          <Button variant="contained" primary onClick={upsertThemeColors}>
-            Save
-          </Button>
-        </div>
-      </div>
+      </PageWrapper>
     </FullScreenModal>
   );
 }
