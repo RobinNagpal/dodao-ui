152535c762502b314e9ce0da11ef612a7534a1fe
robin <robinnagpal.tiet@gmail.com>
2024-05-28 08:55:51 -0400

    Disable sentry

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,14 +2,14 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
-
-Sentry.init({
-  dsn: "https://de227901c92404d53a02b8000150b277@o4504373980954624.ingest.sentry.io/4506820989485056",
-
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+// import * as Sentry from "@sentry/nextjs";
+//
+// Sentry.init({
+//   dsn: "https://de227901c92404d53a02b8000150b277@o4504373980954624.ingest.sentry.io/4506820989485056",
+//
+//   // Adjust this value in production, or use tracesSampler for greater control
+//   tracesSampleRate: 1,
+//
+//   // Setting this option to true will print useful information to the console while you're setting up Sentry.
+//   debug: false,
+// });
