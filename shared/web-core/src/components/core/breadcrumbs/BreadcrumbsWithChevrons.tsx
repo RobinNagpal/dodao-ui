cc8f29b82d1953797327d1df7f690ac652211bf4
Usman <u2003252@gmail.com>
2024-07-09 15:14:34 +0500

    Breadcrumb updated for SpaceType based navigation

--- shared/web-core/src/components/core/breadcrumbs/BreadcrumbsWithChevrons.tsx
+++ shared/web-core/src/components/core/breadcrumbs/BreadcrumbsWithChevrons.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { ChevronRightIcon, HomeIcon } from '@heroicons/react/20/solid';
 import { useRouter } from 'next/navigation';
+import { SpaceWithIntegrationsFragment } from '../../../../../../academy-ui/src/graphql/generated/generated-types';
+import { SpaceTypes } from '../../../../../../academy-ui/src/graphql/generated/generated-types';
 import PageWrapper from '../page/PageWrapper';
 import Link from 'next/link';
 
@@ -11,17 +13,29 @@ export interface BreadcrumbsOjbect {
 }
 
 interface BreadcrumbsWithChevronsProps {
+  space: SpaceWithIntegrationsFragment;
   breadcrumbs: BreadcrumbsOjbect[];
 }
 
-export default function BreadcrumbsWithChevrons({ breadcrumbs }: BreadcrumbsWithChevronsProps) {
+export default function BreadcrumbsWithChevrons({ space, breadcrumbs }: BreadcrumbsWithChevronsProps) {
   const router = useRouter();
+
+  console.log('Categorys Space is : ', space);
 
   return breadcrumbs.length === 0 ? null : (
     <nav className="flex" aria-label="Breadcrumb">
       <ol role="list" className="flex items-center space-x-4">
         <li>
-          <div className="cursor-pointer" onClick={() => router.push('/')}>
+          <div
+            className="cursor-pointer"
+            onClick={() => {
+              if (space.type === SpaceTypes.AcademySite) {
+                router.push('/tidbit-collection-categories');
+              } else {
+                router.push('/');
+              }
+            }}
+          >
             <HomeIcon className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
             <span className="sr-only">Home</span>
           </div>
