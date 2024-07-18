dddd789b97a35874afc587cf56ab59e8057af1bb
Moez <moizahsan5@gmail.com>
2024-07-18 21:55:08 +0500

    Changed programs and rubrics components to server side

--- rubrics-ui/src/app/programs/page.tsx
+++ rubrics-ui/src/app/programs/page.tsx
@@ -1,56 +1,36 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import React from 'react';
 import Card from '@dodao/web-core/components/core/card/Card';
+import Link from 'next/link';
+import { ProgramListProps } from '@/types/rubricsTypes/types';
 
-const ProgramsList: React.FC = () => {
-  const [programs, setPrograms] = useState<{ id: string; name: string; details: string }[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
-  const router = useRouter();
-
-  const fetchPrograms = async () => {
-    try {
-      const response = await fetch('http://localhost:3004/api/programs');
-      const data = await response.json();
-
-      if (response.ok) {
-        setPrograms(data.body);
-      } else {
-        setError(data.body || 'Failed to fetch programs');
-      }
-    } catch (error) {
-      setError('Failed to fetch programs');
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchPrograms();
-  }, []);
-
-  const handleProgramClick = (id: string) => {
-    router.push(`/program-details/`);
-  };
-
-  if (loading) {
-    return <p className="text-center mt-4">Loading...</p>;
+const fetchPrograms = async (): Promise<ProgramListProps[]> => {
+  try {
+    const response = await fetch('/api/programs');
+    const data = await response.json();
+    return data.body;
+  } catch (error) {
+    throw new Error('Failed to fetch programs');
   }
+};
+
+const ProgramsList = async () => {
+  const programs = await fetchPrograms();
 
   return (
     <div className="container mx-auto py-8 px-4">
       <h1 className="text-4xl text-center font-extrabold mb-10">Programs</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {programs.map((program) => (
-          <Card key={program.id} className="cursor-pointer" onClick={() => handleProgramClick(program.id)}>
-            <div className="p-6">
-              <h2 className="text-2xl font-semibold mb-3 " style={{ color: 'var(--primary-color)' }}>
-                {program.name}
-              </h2>
-              <p>{program.details}</p>
-            </div>
-          </Card>
+        {programs?.map((program: ProgramListProps) => (
+          <Link key={program.id} href={`/program-details/${program.id}`} passHref>
+            <Card className="cursor-pointer">
+              <div className="p-6">
+                <h2 className="text-2xl font-semibold mb-3" style={{ color: 'var(--primary-color)' }}>
+                  {program.name}
+                </h2>
+                <p>{program.details}</p>
+              </div>
+            </Card>
+          </Link>
         ))}
       </div>
     </div>
--- rubrics-ui/src/app/rubrics/page.tsx
+++ rubrics-ui/src/app/rubrics/page.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
-import RubricsList from '@/components/RubricsList/RubricsList';
-const RubricLists = () => {
+import Card from '@dodao/web-core/components/core/card/Card';
+import { RubricListProps } from '@/types/rubricsTypes/types';
+
+const fetchRubrics = async (): Promise<RubricListProps[]> => {
+  try {
+    const response = await fetch('/api/rubrics');
+    const data = await response.json();
+    return data.body;
+  } catch (error) {
+    throw new Error('Failed to fetch rubrics');
+  }
+};
+
+const RubricsList = async () => {
+  const rubrics = await fetchRubrics();
+
   return (
-    <div>
-      <RubricsList />
+    <div className="container mx-auto py-8 px-4">
+      <h1 className="text-4xl text-center font-extrabold mb-10">Rubrics</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {rubrics.map((rubric: RubricListProps) => (
+          <Card key={rubric.id} className="cursor-pointer">
+            <div className="p-4">
+              <h2 className="text-xl font-semibold mb-2" style={{ color: 'var(--primary-color)' }}>
+                {rubric.name}
+              </h2>
+              <p>{rubric.summary}</p>
+            </div>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
 
-export default RubricLists;
+export default RubricsList;
