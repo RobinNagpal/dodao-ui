1fa24e84986d9c1daddad32777529592a813933f
Usman <u2003252@gmail.com>
2024-07-22 17:43:49 +0500

    UngroupedCollections's Referencing Updated According to User/Admin

--- academy-ui/src/app/tidbit-collections/page.tsx
+++ academy-ui/src/app/tidbit-collections/page.tsx
@@ -7,20 +7,35 @@ import { getSpaceServerSide } from '@/utils/space/getSpaceServerSide';
 import React from 'react';
 import axios from 'axios';
 import getBaseUrl from '@/utils/api/getBaseURL';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
+import { Session } from '@dodao/web-core/types/auth/Session';
 
 async function TidbitCollections() {
   const space = (await getSpaceServerSide())!;
+  const session = (await getServerSession(authOptions)) as Session | null;
+
   const response = await axios.get(`${getBaseUrl()}/api/byte-collection/byte-collections`, {
     params: {
       spaceId: space.id,
     },
   });
   const byteCollections: ByteCollectionFragment[] = response.data.byteCollections;
+  let filteredCollections;
+
+  if (session?.isAdminOfSpace) {
+    filteredCollections = byteCollections.filter(
+      (collection) => !(collection.id === 'UNGROUPED_TIDBITS' && Array.isArray(collection.bytes) && collection.bytes.length === 0)
+    );
+  } else {
+    filteredCollections = byteCollections.filter((collection) => collection.id !== 'UNGROUPED_TIDBITS');
+  }
+
   return (
     <PageWrapper>
       <TidbitsSiteTabs selectedTabId={TidbitSiteTabIds.TidbitCollections} />
       <ByteCollectionsGrid
-        byteCollections={byteCollections}
+        byteCollections={filteredCollections}
         space={space}
         byteCollectionType={'byteCollection'}
         byteCollectionsBaseUrl={`/tidbit-collections`}
