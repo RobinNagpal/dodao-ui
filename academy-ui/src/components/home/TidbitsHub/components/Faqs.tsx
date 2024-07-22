499475d706743131e88fccb55eb2dfaa1daea0dc
HussainKhanSherwani <ghulamhussainsherwani@gmail.com>
2024-07-22 23:17:23 +0500

    improved view for the FAQs part

--- academy-ui/src/components/home/TidbitsHub/components/Faqs.tsx
+++ academy-ui/src/components/home/TidbitsHub/components/Faqs.tsx
@@ -43,8 +43,8 @@ export function Faqs() {
   return (
     <section id="faqs" aria-labelledby="faqs-title" className="border-t border-gray-200 py-20 sm:py-32">
       <Container>
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 id="faqs-title" className="text-3xl font-medium tracking-tight">
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 id="faqs-title" className="text-3xl font-medium tracking-tight mb-6">
             Frequently asked questions
           </h2>
           <p className="mt-2 text-lg">
@@ -55,17 +55,15 @@ export function Faqs() {
             .
           </p>
         </div>
-        <ul role="list" className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 sm:mt-20 lg:max-w-none lg:grid-cols-3">
+        <ul role="list" className="mx-auto mt-16 grid max-w-2xl gap-12 sm:mt-20 lg:max-w-none lg:grid-cols-3">
           {faqs.map((column, columnIndex) => (
-            <li key={columnIndex}>
-              <ul role="list" className="space-y-10">
-                {column.map((faq, faqIndex) => (
-                  <li key={faqIndex}>
-                    <h3 className="text-lg font-semibold leading-6">{faq.question}</h3>
-                    <p className="mt-4 text-sm">{faq.answer}</p>
-                  </li>
-                ))}
-              </ul>
+            <li key={columnIndex} className="grid grid-cols-1 gap-8">
+              {column.map((faq, faqIndex) => (
+                <div key={faqIndex} className="space-y-4">
+                  <h3 className="text-xl font-semibold leading-tight">{faq.question}</h3>
+                  <p className="text-base">{faq.answer}</p>
+                </div>
+              ))}
             </li>
           ))}
         </ul>
