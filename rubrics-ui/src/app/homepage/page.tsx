7253d19b385fff4af9613be0a682dcf329aa8240
Moez <moizahsan5@gmail.com>
2024-07-31 16:20:36 +0500

    Added Hero section and fixed many calls to api problem

--- rubrics-ui/src/app/homepage/page.tsx
+++ rubrics-ui/src/app/homepage/page.tsx
@@ -2,12 +2,12 @@ import { getServerSession } from 'next-auth';
 import React from 'react';
 import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
 import { Session } from '@dodao/web-core/types/auth/Session';
-
+import { Hero } from '@/components/Hero/Hero';
 async function Home() {
   return (
     <div>
       <div className="mt-10 flex items-center justify-center gap-x-6">
-        <h1 className="text-center text-3xl">HomePage</h1>
+        <Hero />
       </div>
     </div>
   );
