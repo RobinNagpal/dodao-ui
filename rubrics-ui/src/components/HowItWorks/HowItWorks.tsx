6b9bfed6325c2290d8aa18fe9e6538c9c18d5142
Moez <moizahsan5@gmail.com>
2024-08-01 16:34:20 +0500

    Added gif video

--- rubrics-ui/src/components/HowItWorks/HowItWorks.tsx
+++ rubrics-ui/src/components/HowItWorks/HowItWorks.tsx
@@ -2,60 +2,29 @@ import Image from 'next/image';
 
 import { Container } from '@/components/Container/Container';
 import createCustomRubricImage from '@/images/create-custom-rubric.png';
+import gatherFeedbackImage from '@/images/gather-feedback.png';
+import analyticsImage from '@/images/analytics.png';
+
 const resources = [
   {
     title: 'Create Custom Rubrics',
     description: 'Design detailed evaluation rubrics tailored to your specific needs.',
-    image: function FigmaImage() {
-      return (
-        <img
-          src="https://erezlife.com/wp-content/uploads/2021/09/Rubric-1.png"
-          style={{
-            width: '100%',
-            height: '100%',
-            objectFit: 'cover',
-          }}
-          alt="createCustomRubricImage"
-        />
-      );
+    image: function CreateCustomRubricImage() {
+      return <Image src={createCustomRubricImage} layout="fill" objectFit="cover" alt="Create Custom Rubric" />;
     },
   },
   {
     title: 'Gather Feedback',
     description: 'Collect precise and actionable feedback effortlessly through our user-friendly interface.',
-    image: function VideoPlayerImage() {
-      return (
-        <div className="absolute inset-0 flex items-center justify-center">
-          <img
-            src="https://rapidr.io/blog/content/images/2022/06/Group-146.png"
-            style={{
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover',
-            }}
-            alt="createCustomRubricImage"
-          />
-        </div>
-      );
+    image: function GatherFeedbackImage() {
+      return <Image src={gatherFeedbackImage} layout="fill" objectFit="cover" alt="Gather Feedback" />;
     },
   },
   {
     title: 'Review and Improve',
     description: 'Gain deep insights with comprehensive reports and analytics to make informed decisions.',
-    image: function DiscordImage() {
-      return (
-        <div className="absolute inset-0 flex items-center justify-center">
-          <img
-            src="https://www.travelpayouts.com/blog/wp-content/uploads/2020/07/how-to-use-google-analytics-to-improve-it-projects.png"
-            style={{
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover',
-            }}
-            alt="createCustomRubricImage"
-          />
-        </div>
-      );
+    image: function AnalyticsImage() {
+      return <Image src={analyticsImage} layout="fill" objectFit="cover" alt="Analytics" />;
     },
   },
 ];
--- rubrics-ui/src/components/HowItWorksVideo/HowItWorksVideo.tsx
+++ rubrics-ui/src/components/HowItWorksVideo/HowItWorksVideo.tsx
@@ -1,46 +1,9 @@
 import { Container } from '@/components/Container/Container';
 import { ArrowPathIcon, CloudArrowUpIcon, Cog6ToothIcon, LockClosedIcon, FolderOpenIcon, BellAlertIcon } from '@heroicons/react/20/solid';
-// import Academy_site from '@/images/Academy_site.gif';
+import RubricVideo from '@/images/rubric-video.gif';
 import Image from 'next/image';
 
 export default function HowItWorksVideo() {
-  const features = [
-    {
-      name: 'Guides',
-      description:
-        'Our nano-courses offer focused learning on specific topics. In just 5-10 minutes, users can absorb information, test their knowledge with multiple-choice questions.',
-      icon: CloudArrowUpIcon,
-    },
-    {
-      name: 'Simulations/Clickable Demos',
-      description:
-        "We've built simulations to confidently acquaint users with protocols without the risk. Interactive and secure, these demos teach without the fear of clicking wrong links.",
-      icon: FolderOpenIcon,
-    },
-    {
-      name: 'TidBits',
-      description:
-        'Designed for efficiency, TidBits deliver crucial information in compact, easy-to-digest steps. These brief 5-10 sentence pieces allow for rapid understanding without the overwhelm of longer reads.',
-      icon: ArrowPathIcon,
-    },
-    {
-      name: 'Advanced security.',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
-      icon: LockClosedIcon,
-    },
-    {
-      name: 'Courses',
-      description:
-        'For those seeking depth, our courses combine texts, diagrams, videos, and interactive questions to provide a rich, 30-60 minute learning journey.',
-      icon: Cog6ToothIcon,
-    },
-    {
-      name: 'Timelines',
-      description:
-        'Stay up-to-date with our timelines that track and inform about the latest product developments and releases, keeping customers connected and informed.',
-      icon: BellAlertIcon,
-    },
-  ];
   return (
     <section id="rubric-site" aria-labelledby="rubric-site-title" className="scroll-mt-14  sm:scroll-mt-32  pb-16">
       <Container>
@@ -48,8 +11,8 @@ export default function HowItWorksVideo() {
         <div className="py-16 sm:py-12">
           <div className="mx-auto max-w-7xl px-6 lg:px-8"></div>
           <div className="relative pt-16">
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
-              <Image src="" alt="Rubric Site Gif" className="mb-[-12%] rounded-xl ring-1 ring-gray-900/10" width={2432} height={1442} />
+            <div className="mx-auto max-w-7xl px-6 lg:px-8 py-16">
+              <Image src={RubricVideo} alt="Rubric Site Gif" className="mb-[-12%]  rounded-xl ring-1 ring-gray-900/10" width={2432} height={1442} />
             </div>
           </div>
           <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-28 md:mt-32 lg:mt-44 lg:px-8"></div>
