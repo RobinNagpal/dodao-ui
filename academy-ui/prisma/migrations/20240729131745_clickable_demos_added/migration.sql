340f214cd82db5afe2bd67f272498293f8a04317
Usman <u2003252@gmail.com>
2024-08-01 15:50:17 +0500

    Migration Issues Resolved

--- academy-ui/prisma/migrations/20240729131745_clickable_demos_added/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropForeignKey
-ALTER TABLE "byte_collections_items_mappings" DROP CONSTRAINT "FK_ClickableDemos";
--- academy-ui/prisma/migrations/20240729154218_demos_relation_with_mapping_table_created/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "byte_collections_items_mappings" ADD CONSTRAINT "FK_ClickableDemos" FOREIGN KEY ("itemId") REFERENCES "clickable_demos"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
