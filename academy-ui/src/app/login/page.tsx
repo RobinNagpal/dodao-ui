16d52ed93894f28a6dad34547a830d64191b889a
robin <robinnagpal.tiet@gmail.com>
2024-08-06 06:07:46 -0400

    Fix server side component

--- academy-ui/src/app/login/page.tsx
+++ academy-ui/src/app/login/page.tsx
@@ -1,4 +1,5 @@
 import { LoginLogoutButtons } from '@/app/login/loginLogoutButtons';
+import { getSpaceServerSide } from '@/utils/space/getSpaceServerSide';
 import PageWrapper from '@dodao/web-core/components/core/page/PageWrapper';
 import { Session } from '@dodao/web-core/types/auth/Session';
 import { useSession } from 'next-auth/react';
@@ -7,11 +8,12 @@ import React from 'react';
 async function LoginPage() {
   const { data: sessionData } = useSession();
   const session: Session | null = sessionData as Session | null;
+  const space = await getSpaceServerSide();
 
   return (
     <PageWrapper>
       <div className="w-full flex justify-center break-all">
-        <LoginLogoutButtons session={session} space={} />
+        <LoginLogoutButtons session={session} space={space!} />
       </div>
     </PageWrapper>
   );
