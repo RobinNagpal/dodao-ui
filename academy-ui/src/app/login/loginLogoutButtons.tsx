46c44e8a77b00ffc6d32df81506f0bbc66d51819
robin <robinnagpal.tiet@gmail.com>
2024-08-06 06:00:14 -0400

    Fix server side component

---
+++ academy-ui/src/app/login/loginLogoutButtons.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { SpaceWithIntegrationsFragment } from '@/graphql/generated/generated-types';
+import ButtonLarge from '@dodao/web-core/components/core/buttons/Button';
+import { Session } from '@dodao/web-core/types/auth/Session';
+import { useAuth } from '@dodao/web-core/ui/auth/useAuth';
+import { useLoginModalContext } from '@dodao/web-core/ui/contexts/LoginModalContext';
+import React from 'react';
+
+export function LoginLogoutButtons(props: { session: Session | null; space: SpaceWithIntegrationsFragment }) {
+  const { logout, active } = useAuth(props.space!.id);
+  const { setShowLoginModal } = useLoginModalContext();
+
+  return (
+    <div>
+      <div className="flex-col">
+        {props.session?.username && <p>Welcome - {props.session?.username}</p>}
+        {props.session?.username && (
+          <ButtonLarge variant={'contained'} primary onClick={logout}>
+            Logout
+          </ButtonLarge>
+        )}
+      </div>
+      {!props.session?.username && (
+        <div className="mt-4">
+          <ButtonLarge onClick={() => setShowLoginModal(true)} primary>
+            Show Login Modal
+          </ButtonLarge>
+        </div>
+      )}
+    </div>
+  );
+}
--- academy-ui/src/app/login/page.tsx
+++ academy-ui/src/app/login/page.tsx
@@ -1,41 +1,17 @@
-'use client';
-
-import { getSpaceServerSide } from '@/utils/space/getSpaceServerSide';
-import ButtonLarge from '@dodao/web-core/components/core/buttons/Button';
+import { LoginLogoutButtons } from '@/app/login/loginLogoutButtons';
 import PageWrapper from '@dodao/web-core/components/core/page/PageWrapper';
-import { useLoginModalContext } from '@dodao/web-core/ui/contexts/LoginModalContext';
-import { useAuth } from '@dodao/web-core/ui/auth/useAuth';
 import { Session } from '@dodao/web-core/types/auth/Session';
 import { useSession } from 'next-auth/react';
 import React from 'react';
 
 async function LoginPage() {
-  const space = await getSpaceServerSide();
   const { data: sessionData } = useSession();
   const session: Session | null = sessionData as Session | null;
-  const { logout, active } = useAuth(space!.id);
-  const { setShowLoginModal } = useLoginModalContext();
 
   return (
     <PageWrapper>
       <div className="w-full flex justify-center break-all">
-        <div>
-          <div className="flex-col">
-            {session?.username && <p>Welcome - {session?.username}</p>}
-            {session?.username && (
-              <ButtonLarge variant={'contained'} primary onClick={logout}>
-                Logout
-              </ButtonLarge>
-            )}
-          </div>
-          {!session?.username && (
-            <div className="mt-4">
-              <ButtonLarge onClick={() => setShowLoginModal(true)} primary>
-                Show Login Modal
-              </ButtonLarge>
-            </div>
-          )}
-        </div>
+        <LoginLogoutButtons session={session} space={} />
       </div>
     </PageWrapper>
   );
