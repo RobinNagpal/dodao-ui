4f1aef1442858b80c95b2518c2dbb267cf34ce7f
Moez <moizahsan5@gmail.com>
2024-08-16 23:01:02 +0500

    Fix issues

--- rubrics-ui/src/app/rubrics/page.tsx
+++ rubrics-ui/src/app/rubrics/page.tsx
@@ -1,8 +1,8 @@
 import { fetchRubrics, RubricsGrid } from '@/app/rubrics/RubricsGrid';
 import React from 'react';
-import { SpaceWithIntegrationsFragment } from '@/types/rubricsTypes/types';
-const RubricsList = async (props: { space: SpaceWithIntegrationsFragment }) => {
-  const { space } = props;
+import { getSpaceServerSide } from '@/utils/space/getSpaceServerSide';
+const RubricsList = async () => {
+  const space = (await getSpaceServerSide())!;
   const rubrics = await fetchRubrics();
 
   return <RubricsGrid rubrics={rubrics} space={space} />;
