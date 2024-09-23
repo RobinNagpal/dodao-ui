a59e6fb267d22b3d3706f93a3f899837cd102775
HussainKhanSherwani <ghulamhussainsherwani@gmail.com>
2024-09-23 16:23:15 +0500

    set consistent table name and name of fields

--- academy-ui/prisma/migrations/20240923112212_added_a_new_entity_clcikable_demo_html_captures/migration.sql
+++ academy-ui/prisma/migrations/20240923112212_added_a_new_entity_clcikable_demo_html_captures/migration.sql
@@ -1,11 +1,11 @@
 -- CreateTable
-CREATE TABLE "ClickableDemoHtmlCaptures" (
+CREATE TABLE "clickable_demo_html_cpatures" (
     "id" VARCHAR(255) NOT NULL,
     "clickable_demo_id" VARCHAR(255) NOT NULL,
     "file_name" VARCHAR(1024) NOT NULL,
     "file_url" VARCHAR(1024) NOT NULL,
-    "image_url" VARCHAR(1024) NOT NULL,
+    "file_image_url" VARCHAR(1024) NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "ClickableDemoHtmlCaptures_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "clickable_demo_html_cpatures_pkey" PRIMARY KEY ("id")
 );
