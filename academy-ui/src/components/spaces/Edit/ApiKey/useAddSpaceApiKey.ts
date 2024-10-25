d36ae5db50f4f6de11274293ac3269c33c030dc4
robin <robinnagpal.tiet@gmail.com>
2024-10-25 13:47:22 -0400

    Fix code to add api key

--- academy-ui/src/components/spaces/Edit/ApiKey/useAddSpaceApiKey.ts
+++ academy-ui/src/components/spaces/Edit/ApiKey/useAddSpaceApiKey.ts
@@ -1,39 +1,47 @@
 import { SpaceWithIntegrationsFragment } from '@/graphql/generated/generated-types';
 import { Session } from '@dodao/web-core/types/auth/Session';
 import { useNotificationContext } from '@dodao/web-core/ui/contexts/NotificationContext';
+import { useFetchUtils } from '@dodao/web-core/ui/hooks/useFetchUtils';
 import getBaseUrl from '@dodao/web-core/utils/api/getBaseURL';
 import { useSession } from 'next-auth/react';
 
+interface AddApiKeyRequest {
+  spaceId: string;
+  creator: string;
+  apiKey: string;
+}
+
+interface AddApiKeyResponse {
+  space: SpaceWithIntegrationsFragment;
+}
+
 export function useAddSpaceApiKey(space: SpaceWithIntegrationsFragment, onUpdate: (space: any) => void) {
   const { showNotification } = useNotificationContext();
+  const { postData } = useFetchUtils();
   const { data: clientSession } = useSession() as { data: Session | null };
 
-  const addApiKey = async (apiKey: string) => {
-    try {
-      // Simulate generating a new API key
-      const response = await fetch(`${getBaseUrl()}/api/${space.id}/actions/spaces/generate-api-key`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          spaceId: space.id,
-          creator: clientSession?.username,
-          apiKey: apiKey,
-        }),
-      });
+  const username = clientSession?.username;
+  if (!username) {
+    showNotification({ type: 'error', message: 'No session present for user' });
+    return;
+  }
 
-      if (response.ok) {
-        const data = await response.json();
-        onUpdate(data.space);
-        showNotification({ type: 'success', message: 'API key added successfully' });
-      } else {
-        throw new Error('Failed to add new API key');
+  const addApiKey = async (apiKey: string) => {
+    // Simulate generating a new API key
+    const response = await postData<AddApiKeyResponse, AddApiKeyRequest>(
+      `${getBaseUrl()}/api/${space.id}/actions/spaces/generate-api-key`,
+      {
+        spaceId: space.id,
+        creator: username,
+        apiKey: apiKey,
+      },
+      {
+        errorMessage: 'Failed to add new API key',
+        successMessage: 'API key added successfully',
       }
-    } catch (error) {
-      console.error('Error adding new API key:', error);
-      showNotification({ type: 'error', message: 'Failed to add new API key' });
-    }
+    );
+
+    onUpdate(response!.space);
   };
   return {
     addApiKey,
