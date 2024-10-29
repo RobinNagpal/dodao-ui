c8bdd6c93acda955ebc6ad6457a7b0cef77e73bc
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2024-10-29 18:58:57 +0500

    added tidbits hub detailed page in dodao.io

--- academy-ui/src/app/home-section/dodao-io/products/decen-reviews/page.tsx
+++ academy-ui/src/app/home-section/dodao-io/products/decen-reviews/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div>Empty Page</div>;
+import PageWrapper from '@dodao/web-core/components/core/page/PageWrapper';
+import DecenReviews from './decen-reviews.mdx';
+
+export default async function RiskAnalysisPage() {
+  return (
+    <PageWrapper>
+      <div className="markdown-body w-full">
+        <DecenReviews />
+      </div>
+    </PageWrapper>
+  );
 }
--- academy-ui/src/app/home-section/dodao-io/products/tidbitshub/page.tsx
+++ academy-ui/src/app/home-section/dodao-io/products/tidbitshub/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div>Empty Page</div>;
+import PageWrapper from '@dodao/web-core/components/core/page/PageWrapper';
+import TidBitsHubMdx from './tidbitshub.mdx';
+
+export default async function RiskAnalysisPage() {
+  return (
+    <PageWrapper>
+      <div className="markdown-body w-full">
+        <TidBitsHubMdx />
+      </div>
+    </PageWrapper>
+  );
 }
