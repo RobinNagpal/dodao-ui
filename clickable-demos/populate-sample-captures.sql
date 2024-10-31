1c97ce52c3e07fe6ecd66ce5ba60374a11a3acac
robin <robinnagpal.tiet@gmail.com>
2024-10-31 11:48:28 -0400

    Add html captures

---
+++ clickable-demos/populate-sample-captures.sql
@@ -0,0 +1,40 @@
+insert into public.clickable_demo_html_cpatures
+values (md5(random()::text),
+        'deposit-into-a-vault-4123',
+        'file-10-30-2024-8-55-39-am',
+        'http://localhost:9090/sample-captures/safe-captures/file-10-30-2024-8-55-39-am/index.html',
+        'http://localhost:9090/sample-captures/safe-screenshots/1730292942806_file-10-30-2024-8-55-39-AMscreenshot.png',
+        '2024-10-30 08:55:39');
+
+insert into public.clickable_demo_html_cpatures
+values (md5(random()::text),
+        'deposit-into-a-vault-4123',
+        'file-10-30-2024-8-53-47-am',
+        'http://localhost:9090/sample-captures/safe-captures/file-10-30-2024-8-53-47-am/index.html',
+        'http://localhost:9090/sample-captures/safe-screenshots/1730292831760_file-10-30-2024-8-53-47-AMscreenshot.png',
+        '2024-10-30 08:53:47');
+
+insert into public.clickable_demo_html_cpatures
+values (md5(random()::text),
+        'deposit-into-a-vault-4123',
+        'file-10-30-2024-8-55-02-am',
+        'http://localhost:9090/sample-captures/safe-captures/file-10-30-2024-8-55-02-am/index.html',
+        'http://localhost:9090/sample-captures/safe-screenshots/1730292906607_file-10-30-2024-8-55-02-AMscreenshot.png',
+        '2024-10-30 08:55:02');
+
+insert into public.clickable_demo_html_cpatures
+values (md5(random()::text),
+        'deposit-into-a-vault-4123',
+        'file-10-30-2024-8-55-22-am',
+        'http://localhost:9090/sample-captures/safe-captures/file-10-30-2024-8-55-22-am/index.html',
+        'http://localhost:9090/sample-captures/safe-screenshots/1730292928022_file-10-30-2024-8-55-22-AMscreenshot.png',
+        '2024-10-30 08:55:22');
+
+insert into public.clickable_demo_html_cpatures
+values (md5(random()::text),
+        'deposit-into-a-vault-4123',
+        'file-10-30-2024-8-54-49-am',
+        'http://localhost:9090/sample-captures/safe-captures/file-10-30-2024-8-54-49-am/index.html',
+        'http://localhost:9090/sample-captures/safe-screenshots/1730292893566_file-10-30-2024-8-54-49-AMscreenshot.png',
+        '2024-10-30 08:54:49');
+
