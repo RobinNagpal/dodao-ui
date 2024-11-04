7395be9ba3c1eef971f62709e349310b080bd668
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-11-04 23:21:24 +0500

    route for creating token

---
+++ academy-ui/src/app/api/[spaceId]/actions/spaces/new-tidbit-subspace/route.ts
@@ -0,0 +1,28 @@
+import { createHash } from '@dodao/web-core/api/auth/createHash';
+import { withErrorHandlingV1 } from '@/app/api/helpers/middlewares/withErrorHandling';
+import { prisma } from '@/prisma';
+import { NextRequest, NextResponse } from 'next/server';
+import { defaultNormalizer, randomString } from '@dodao/web-core/api/auth/custom-email/send-verification';
+import { getDecodedJwtFromContext } from '@dodao/web-core/api/auth/getJwtFromContext';
+
+async function postHandler(req: NextRequest): Promise<NextResponse<string>> {
+  const session = await getDecodedJwtFromContext(req);
+  if (!session) throw new Error('User not present in session');
+
+  const userEmail = defaultNormalizer(session.username);
+
+  const token = randomString(32);
+  const ONE_DAY_IN_SECONDS = 86400;
+  const expires = new Date(Date.now() + ONE_DAY_IN_SECONDS * 1000 * 1); // 1 day
+
+  const data = {
+    identifier: userEmail,
+    token: await createHash(`${token}${process.env.EMAIL_TOKEN_SECRET!}`),
+    expires,
+  };
+  await prisma.verificationToken.create({ data });
+
+  return NextResponse.json(token);
+}
+
+export const POST = withErrorHandlingV1<string>(postHandler);
