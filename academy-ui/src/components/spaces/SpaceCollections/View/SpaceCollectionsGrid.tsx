6dcecf217d0ebbf9cd028de3abc7f20f464837aa
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-11-05 20:12:08 +0500

    fixed colors

--- academy-ui/src/components/spaces/SpaceCollections/View/SpaceCollectionsGrid.tsx
+++ academy-ui/src/components/spaces/SpaceCollections/View/SpaceCollectionsGrid.tsx
@@ -10,15 +10,15 @@ interface SpaceCollectionGridProps {
 export default function SpaceCollectionsGrid({ spaceCollections }: SpaceCollectionGridProps) {
   return (
     <div className="mx-auto max-w-md">
-      <h2 className="text-base font-semibold leading-6 text-gray-900">Select Your Space</h2>
-      <p className="mt-1 text-sm text-gray-500">Get started by selecting a space or create a new space.</p>
-      <ul role="list" className="mt-6 max-w-md divide-y divide-gray-200 border-b border-t border-gray-200">
+      <h2 className="text-base font-semibold leading-6">Select Your Space</h2>
+      <p className="mt-1 text-sm">Get started by selecting a space or create a new space.</p>
+      <ul role="list" className="mt-6 max-w-md border-b border-color">
         {spaceCollections.map((byteCollection, i) => (
           <SpaceCollectionsCard key={i} spaceCollection={byteCollection} />
         ))}
       </ul>
       <div className="mt-6 flex">
-        <Link href="/spaces/create" className="text-sm font-medium text-indigo-600 hover:text-indigo-500">
+        <Link href="/spaces/create" className="text-sm font-medium link-color">
           Or create a new space
           <span aria-hidden="true"> &rarr;</span>
         </Link>
--- shared/web-core/src/components/app/Form/FormFooter.tsx
+++ shared/web-core/src/components/app/Form/FormFooter.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Button from '@dodao/web-core/components/core/buttons/Button';
-import styles from './FormFooter.module.scss';
 
 interface FormFooterProps {
   saveButtonText: string;
@@ -13,13 +12,9 @@ interface FormFooterProps {
 export const FormFooter = ({ saveButtonText, onSave, onSaveLoading, cancelButtonText, onCancel }: FormFooterProps) => {
   return (
     <div className="pt-12 pb-6">
-      <div className="border-t border-gray-900/10">
+      <div className="border-t border-color">
         <div className="mt-6 flex items-center justify-end gap-x-2">
-          {cancelButtonText && onCancel && (
-            <Button removeBorder className={`text-sm font-semibold ${styles.textColor}`} onClick={onCancel}>
-              {cancelButtonText}
-            </Button>
-          )}
+          {cancelButtonText && onCancel && <Button onClick={onCancel}>{cancelButtonText}</Button>}
           <Button variant="contained" primary loading={onSaveLoading} onClick={onSave}>
             {saveButtonText}
           </Button>
