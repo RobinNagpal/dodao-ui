73dcf9b2bfb36bc2dd08b76a8de53c4ad23f07e5
robin <robinnagpal.tiet@gmail.com>
2024-11-06 07:40:39 -0500

    Fix jobs

---
+++ academy-ui/src/utils/api/fetchDataServerSide.ts
@@ -0,0 +1,6 @@
+import fetchDataServerSideCore from '@dodao/web-core/ui/hooks/fetchDataServerSideCore';
+import { authOptions } from 'academy-ui/src/app/api/auth/[...nextauth]/authOptions';
+
+export default async function fetchDataServerSide<T>(url: string, options: RequestInit = {}): Promise<T> {
+  return fetchDataServerSideCore(url, authOptions, options);
+}
