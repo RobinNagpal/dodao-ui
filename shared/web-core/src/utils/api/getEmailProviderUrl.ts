97585b72cac637db5a67eb192855bc181a2ed3eb
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-11-12 19:20:32 +0500

    ran prettier

--- shared/web-core/src/utils/api/getEmailProviderUrl.ts
+++ shared/web-core/src/utils/api/getEmailProviderUrl.ts
@@ -1,19 +1,18 @@
 export const getEmailProviderUrl = (email: string): string | null => {
-    const domain = email.split('@')[1];
-  
-    switch (domain) {
-      case 'gmail.com':
-        return 'https://mail.google.com/';
-      case 'yahoo.com':
-        return 'https://mail.yahoo.com/';
-      case 'outlook.com':
-        return 'https://outlook.live.com/';
-      case 'hotmail.com':
-        return 'https://outlook.live.com/';
-      case 'icloud.com':
-        return 'https://www.icloud.com/mail';
-      default:
-        return null;
-    }
-  };
-  
+  const domain = email.split('@')[1];
+
+  switch (domain) {
+    case 'gmail.com':
+      return 'https://mail.google.com/';
+    case 'yahoo.com':
+      return 'https://mail.yahoo.com/';
+    case 'outlook.com':
+      return 'https://outlook.live.com/';
+    case 'hotmail.com':
+      return 'https://outlook.live.com/';
+    case 'icloud.com':
+      return 'https://www.icloud.com/mail';
+    default:
+      return null;
+  }
+};
