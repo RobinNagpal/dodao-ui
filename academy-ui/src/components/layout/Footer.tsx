bd7c9a917c2c5695f741f3c288e103156f4ea678
robin <robinnagpal.tiet@gmail.com>
2024-11-22 16:21:07 -0500

    Add login button at bottom

--- academy-ui/src/components/layout/Footer.tsx
+++ academy-ui/src/components/layout/Footer.tsx
@@ -1,24 +1,33 @@
+import FooterLoginButton from '@/components/layout/FooterLoginButton';
 import { SpaceProps } from '@/contexts/withSpace';
 import { SpaceTypes } from '@/types/space/SpaceDto';
 import { PredefinedSpaces } from '@dodao/web-core/utils/constants/constants';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 export default function Footer({ space }: SpaceProps) {
   const pathname = usePathname();
 
-  if ((space.type !== SpaceTypes.TidbitsSite && space.type !== SpaceTypes.AcademySite) || pathname !== '/' || space.id === PredefinedSpaces.DODAO_HOME) {
+  if (
+    (space.type !== SpaceTypes.TidbitsSite && space.type !== SpaceTypes.AcademySite) ||
+    pathname !== '/' ||
+    space.id === PredefinedSpaces.DODAO_HOME ||
+    space.id === PredefinedSpaces.TIDBITS_HUB
+  ) {
     return null; // Do not render the footer if the conditions are not met
   }
 
   return (
     <footer>
-      <div className="flex justify-center text-sm pb-8 text-slate-500">
-        <p>
+      <div className="flex justify-center text-sm my-4 pb-4 text-gray-400">
+        <p className="mr-4">
           Crafted with Passion by{' '}
-          <a href={space.type === SpaceTypes.TidbitsSite ? 'https://tidbitshub.org' : 'https://dodao.io/'} className="underline">
+          <Link href={space.type === SpaceTypes.TidbitsSite ? 'https://tidbitshub.org' : 'https://dodao.io/'} className="underline">
             DoDAO
-          </a>
+          </Link>
+          .
         </p>
+        <FooterLoginButton space={space} />
       </div>
     </footer>
   );
---
+++ academy-ui/src/components/layout/FooterLoginButton.tsx
@@ -0,0 +1,16 @@
+import { SpaceProps } from '@/contexts/withSpace';
+import EmailLoginModal from '@dodao/web-core/ui/auth/login/components/EmailLoginModal';
+import { useState } from 'react';
+
+export default function FooterLoginButton({ space }: SpaceProps) {
+  const [showLoginModal, setShowLoginModal] = useState(false);
+
+  return (
+    <>
+      <div className="text-sm text-gray-400 hover:text-gray-900 cursor-pointer underline" onClick={() => setShowLoginModal(true)}>
+        Login
+      </div>
+      ?{showLoginModal && <EmailLoginModal space={space} open={showLoginModal} onClose={() => setShowLoginModal(false)} />}
+    </>
+  );
+}
