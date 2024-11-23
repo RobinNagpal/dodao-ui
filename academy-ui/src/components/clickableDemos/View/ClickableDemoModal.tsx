8828e6f0f2b27c60ed519b2b75a5620eb75b4439
robin <robinnagpal.tiet@gmail.com>
2024-11-22 22:46:55 -0500

    Optimize loading of steps

--- academy-ui/src/components/clickableDemos/View/ClickableDemoModal.tsx
+++ academy-ui/src/components/clickableDemos/View/ClickableDemoModal.tsx
@@ -16,6 +16,7 @@ function ClickableDemoModal({ clickableDemoWithSteps, space, onClose }: Clickabl
   const { showNotification } = useNotificationContext();
 
   const [selectedStepNumber, setSelectedStepNumber] = useState(0);
+  const [loadedSteps, setLoadedSteps] = useState<number[]>([]);
 
   function sendMessageToIframe(stepIndex: number) {
     const iframe: HTMLIFrameElement | null = document.getElementById(`iframe-${stepIndex}`) as HTMLIFrameElement | null;
@@ -49,7 +50,9 @@ function ClickableDemoModal({ clickableDemoWithSteps, space, onClose }: Clickabl
     if (event.data.completeButton) {
       localStorage.setItem(
         LocalStorageKeys.COMPLETED_CLICKABLE_DEMOS,
-        JSON.stringify(union([...JSON.parse(localStorage.getItem(LocalStorageKeys.COMPLETED_CLICKABLE_DEMOS) || '[]'), clickableDemoWithSteps.id]))
+        JSON.stringify(
+          union([...(JSON.parse(localStorage.getItem(LocalStorageKeys.COMPLETED_CLICKABLE_DEMOS) || '[]') as string[]), clickableDemoWithSteps.id])
+        )
       );
       showNotification({
         type: 'success',
@@ -66,6 +69,41 @@ function ClickableDemoModal({ clickableDemoWithSteps, space, onClose }: Clickabl
       window.removeEventListener('message', receiveMessage);
     };
   }, []);
+
+  useEffect(() => {
+    // Initialize loadedSteps with the first six steps
+    setLoadedSteps((prevLoadedSteps) => {
+      const initialStepsToLoad = [];
+      for (let i = 0; i <= 5 && i < clickableDemoWithSteps.steps.length; i++) {
+        if (!prevLoadedSteps.includes(i)) {
+          initialStepsToLoad.push(i);
+        }
+      }
+      if (initialStepsToLoad.length > 0) {
+        return [...prevLoadedSteps, ...initialStepsToLoad];
+      } else {
+        return prevLoadedSteps;
+      }
+    });
+  }, [clickableDemoWithSteps.steps.length]);
+
+  useEffect(() => {
+    // Update loadedSteps when selectedStepNumber changes
+    setLoadedSteps((prevLoadedSteps) => {
+      const maxToLoad = selectedStepNumber + 5;
+      const stepsToLoad = [];
+      for (let i = selectedStepNumber; i <= maxToLoad && i < clickableDemoWithSteps.steps.length; i++) {
+        if (!prevLoadedSteps.includes(i)) {
+          stepsToLoad.push(i);
+        }
+      }
+      if (stepsToLoad.length > 0) {
+        return [...prevLoadedSteps, ...stepsToLoad];
+      } else {
+        return prevLoadedSteps;
+      }
+    });
+  }, [selectedStepNumber, clickableDemoWithSteps.steps.length]);
 
   const parentStyles = window.getComputedStyle(document.body);
   // Collect CSS variables
@@ -88,9 +126,8 @@ function ClickableDemoModal({ clickableDemoWithSteps, space, onClose }: Clickabl
   return (
     <FullScreenModal open={true} onClose={onClose} title={clickableDemoWithSteps.title}>
       <div id="iframe-container" className="relative w-full h-[93vh]">
-        {clickableDemoWithSteps.steps.map((step, index) => {
-          // Set the CSS variables in the iframe
-
+        {loadedSteps.map((index) => {
+          const step = clickableDemoWithSteps.steps[index];
           const data = {
             cssValues, // your collected CSS variables
             elementXPath: step.selector,
