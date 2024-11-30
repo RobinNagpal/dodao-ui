d62f1259c1a357a84649bc88c657de879886c8c9
robin <robinnagpal.tiet@gmail.com>
2024-11-30 15:05:18 -0500

    Update Footer to include Echofex name

--- academy-ui/src/components/home/DoDAOHome/components/Footer.tsx
+++ academy-ui/src/components/home/DoDAOHome/components/Footer.tsx
@@ -42,11 +42,7 @@ export function Footer() {
             ))}
           </div>
           <p className="mt-8 text-xs leading-5 text-gray-500 md:order-1 md:mt-0">
-            Copyright &copy; {new Date().getFullYear()}{' '}
-            <a href={'https://dodao.io/'} target="_blank" className="underline cursor-pointer">
-              DoDAO
-            </a>
-            . All rights reserved.
+            Copyright &copy; {new Date().getFullYear()} Echofex Inc, Canada. All rights reserved.
           </p>
         </div>
       </div>
