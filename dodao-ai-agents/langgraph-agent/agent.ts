34b7bda1e8781a78d5726dfa8545b0f4bd67151d
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2024-12-01 21:11:55 +0500

    added initial code

---
+++ dodao-ai-agents/langgraph-agent/agent.ts
@@ -0,0 +1,43 @@
+// Load environment variables from the .env file
+import dotenv from "dotenv";
+dotenv.config();
+
+import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
+import { ChatOpenAI } from "@langchain/openai";
+import { MemorySaver } from "@langchain/langgraph";
+import { HumanMessage } from "@langchain/core/messages";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+
+(async () => {
+  // Define the tools for the agent to use
+  console.log("OPENAI_API_KEY:", process.env.OPENAI_API_KEY);
+  const agentTools = [new TavilySearchResults({ maxResults: 3 })];
+  const agentModel = new ChatOpenAI({ temperature: 0 });
+
+  // Initialize memory to persist state between graph runs
+  const agentCheckpointer = new MemorySaver();
+  const agent = createReactAgent({
+    llm: agentModel,
+    tools: agentTools,
+    checkpointSaver: agentCheckpointer,
+  });
+
+  // Now it's time to use!
+  const agentFinalState = await agent.invoke(
+    { messages: [new HumanMessage("what is the current weather in sf")] },
+    { configurable: { thread_id: "42" } }
+  );
+
+  console.log(
+    agentFinalState.messages[agentFinalState.messages.length - 1].content
+  );
+
+  const agentNextState = await agent.invoke(
+    { messages: [new HumanMessage("what about ny")] },
+    { configurable: { thread_id: "42" } }
+  );
+
+  console.log(
+    agentNextState.messages[agentNextState.messages.length - 1].content
+  );
+})();
