ade5df7972a00ca1d22f293c7dc675a2a833e0ab
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2025-01-22 22:32:35 +0500

    revert back to normal bucket

--- insights-ui/src/util/insights-constants.ts
+++ insights-ui/src/util/insights-constants.ts
@@ -1,4 +1,4 @@
 export const InsightsConstants = {
-  S3_BUCKET_NAME: 'dodao-ai-insights-agent-backup',
+  S3_BUCKET_NAME: 'dodao-ai-insights-agent',
   CROWDFUND_ANALYSIS_PREFIX: 'crowd-fund-analysis',
 };
