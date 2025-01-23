6907190b78ce71111e22569140bc88799a7fffe9
robin <robinnagpal.tiet@gmail.com>
2025-01-23 14:48:11 -0500

    Add tracking for dodao home page

--- shared/web-core/src/utils/analytics/getGTagId.ts
+++ shared/web-core/src/utils/analytics/getGTagId.ts
@@ -1,6 +1,8 @@
 export function getGTagId(space?: { id: string } | null) {
   if (space?.id === 'uniswap-eth-1') {
     return 'G-GHYGJ5DYRF';
+  } else if (space?.id === 'dodao-1') {
+    return 'G-4WB58RV063';
   } else {
     return 'G-BJX2V8FE7L';
   }
