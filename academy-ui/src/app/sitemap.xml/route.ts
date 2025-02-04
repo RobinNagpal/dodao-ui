2d551c4933cdb40ba36e92395eff6761d1073738
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2025-02-04 23:22:03 +0500

    added guide urls

--- academy-ui/src/app/sitemap.xml/route.ts
+++ academy-ui/src/app/sitemap.xml/route.ts
@@ -1,10 +1,11 @@
-import { CourseDetailsFragment, CourseTopicFragment, GuideFragment } from '@/graphql/generated/generated-types';
-import { SpaceWithIntegrationsDto } from '@/types/space/SpaceDto';
+import { CourseDetailsFragment, CourseTopicFragment, GuideFragment, GuideSummaryFragment } from '@/graphql/generated/generated-types';
+import { SpaceWithIntegrationsDto, SpaceTypes } from '@/types/space/SpaceDto';
 import { getSpaceBasedOnHostHeader } from '@/utils/space/getSpaceServerSide';
 import { PredefinedSpaces } from '@dodao/web-core/src/utils/constants/constants';
 import axios from 'axios';
 import { NextRequest, NextResponse } from 'next/server';
 import { SitemapStream, streamToPromise } from 'sitemap';
+import { ByteCollectionSummary } from '@/types/byteCollections/byteCollection';
 
 interface SiteMapUrl {
   url: string;
@@ -12,12 +13,16 @@ interface SiteMapUrl {
   priority?: number;
 }
 
-async function getAllGuidesWithSteps(spaceId: string) {
-  const guidesUrl = process.env.V2_API_SERVER_URL?.replace('/graphql', '') + `/${spaceId}/guides`;
+async function getAllGuidesWithSteps(host: string, spaceId: string) {
+  const baseUrl = `http://${host}`;
 
-  const response = await axios.get(guidesUrl);
+  const response = await axios.get(`${baseUrl}/api/guide/guides`, {
+    params: {
+      spaceId,
+    },
+  });
 
-  return response?.data as GuideFragment[];
+  return response.data.guides as GuideSummaryFragment[];
 }
 
 async function getAllCourses(spaceId: string) {
@@ -27,20 +32,39 @@ async function getAllCourses(spaceId: string) {
   return response?.data as CourseDetailsFragment[];
 }
 
-async function getGuideUrlsForAcademy(spaceId: string): Promise<SiteMapUrl[]> {
-  const allGuides: GuideFragment[] = await getAllGuidesWithSteps(spaceId);
+async function getGuideUrlsForAcademy(host: string, spaceId: string): Promise<SiteMapUrl[]> {
+  const allGuides: GuideSummaryFragment[] = await getAllGuidesWithSteps(host, spaceId);
   const urls: SiteMapUrl[] = [];
   for (const guide of allGuides) {
-    guide.steps.forEach((s, index) => {
-      if (s.content.length > 50) {
-        const sitemapUrl = {
-          url: `/guides/view/${guide.uuid}/${index}`,
-          changefreq: 'weekly',
-        };
-        urls.push(sitemapUrl);
-      }
+    urls.push({
+      url: `/guides/view/${guide.id}/0`,
+      changefreq: 'monthly',
+      priority: 0.7,
     });
   }
+  return urls;
+}
+
+async function getTidbitCollectionUrlsForAcademy(host: string, spaceId: string): Promise<SiteMapUrl[]> {
+  const baseUrl = `http://${host}`;
+
+  const response = await axios.get<ByteCollectionSummary[]>(`${baseUrl}/api/${spaceId}/byte-collections`);
+  const collections = response.data;
+
+  const urls: SiteMapUrl[] = [];
+
+  for (const collection of collections) {
+    if (collection.bytes) {
+      for (const byte of collection.bytes) {
+        urls.push({
+          url: `/tidbit-collections/view/${collection.id}/${byte.byteId}`,
+          changefreq: 'monthly',
+          priority: 0.7,
+        });
+      }
+    }
+  }
+
   return urls;
 }
 
@@ -88,38 +112,70 @@ async function getDoDAOSiteMapUrls(): Promise<SiteMapUrl[]> {
 }
 
 async function writeDoDAOSiteMapToStream(space: SpaceWithIntegrationsDto, host: string, smStream: SitemapStream) {
-  if (space.id === PredefinedSpaces.DODAO_HOME) {
-    const dodaoUrls = await getDoDAOSiteMapUrls();
-    for (const url of dodaoUrls) {
-      smStream.write(url);
-    }
-  } else {
-    return [];
+  const dodaoUrls = await getDoDAOSiteMapUrls();
+  for (const url of dodaoUrls) {
+    smStream.write(url);
+  }
+}
+
+async function getTidbitsHubSiteMapUrls(): Promise<SiteMapUrl[]> {
+  const urls: SiteMapUrl[] = [{ url: '/', changefreq: 'daily', priority: 1.0 }];
+
+  return urls;
+}
+
+async function writeTidbitsHubSiteMapToStream(space: SpaceWithIntegrationsDto, host: string, smStream: SitemapStream) {
+  const dodaoUrls = await getTidbitsHubSiteMapUrls();
+  for (const url of dodaoUrls) {
+    smStream.write(url);
   }
 }
 
 async function writeUrlsToStream(space: SpaceWithIntegrationsDto, host: string, smStream: SitemapStream) {
-  const guideUrls = await getGuideUrlsForAcademy(space.id);
+  const guideUrls = await getGuideUrlsForAcademy(host, space.id);
 
   for (const guideUrl of guideUrls) {
     smStream.write(guideUrl);
   }
 
-  const courseUrls = await getCourseUrlsForAcademy(space);
-
-  for (const courseUrl of courseUrls) {
-    smStream.write(courseUrl);
+  const tidbitUrls = await getTidbitCollectionUrlsForAcademy(host, space.id);
+  for (const tidbitUrl of tidbitUrls) {
+    smStream.write(tidbitUrl);
   }
+
+  // const courseUrls = await getCourseUrlsForAcademy(space);
+
+  // for (const courseUrl of courseUrls) {
+  //   smStream.write(courseUrl);
+  // }
 }
+
 async function GET(req: NextRequest): Promise<NextResponse<Buffer>> {
   const host = req.headers.get('host') as string;
   const space = (await getSpaceBasedOnHostHeader(req.headers))!;
 
   const smStream = new SitemapStream({ hostname: 'https://' + host });
 
+  // console.log(space);
+  // console.log('-------------------');
+  // console.log(host);
+
   // pipe your entries or directly write them.
+  if (space.id === PredefinedSpaces.DODAO_HOME) {
+    await writeDoDAOSiteMapToStream(space, host, smStream);
+  }
+  if (space.id === PredefinedSpaces.TIDBITS_HUB) {
+    await writeTidbitsHubSiteMapToStream(space, host, smStream);
+  }
+  if (space.type === SpaceTypes.AcademySite) {
+    smStream.write({ url: '/', changefreq: 'daily', priority: 1.0 }), smStream.write({ url: '/guides', changefreq: 'weekly', priority: 0.9 });
+    smStream.write({ url: '/tidbit-collections', changefreq: 'weekly', priority: 0.9 });
+    smStream.write({ url: '/clickable-demos', changefreq: 'weekly', priority: 0.9 });
+    smStream.write({ url: '/courses', changefreq: 'weekly', priority: 0.9 });
+    await writeUrlsToStream(space, host, smStream);
+  }
+
   // await writeUrlsToStream(space!, host, smStream);
-  await writeDoDAOSiteMapToStream(space, host, smStream);
 
   smStream.end();
   // cache the response
