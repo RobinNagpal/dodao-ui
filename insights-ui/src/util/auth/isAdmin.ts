ab5fb955aa0b09730ae3f3fc15b1f80f479d2cfe
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2025-02-06 17:31:44 +0500

    fix build fail

--- insights-ui/src/util/auth/isAdmin.ts
+++ insights-ui/src/util/auth/isAdmin.ts
@@ -1,5 +1,7 @@
 import { InsightsConstants } from '../insights-constants';
 
 export function isAdmin(): boolean {
+  if (typeof window === 'undefined') return false; // Avoid SSR issues
   return !!localStorage.getItem(InsightsConstants.AUTHENTICATION_KEY);
 }
+
