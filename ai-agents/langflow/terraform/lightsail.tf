b2109d87743ad71215deff0c3133774528eb0376
robin <robinnagpal.tiet@gmail.com>
2025-02-08 05:32:50 -0500

    working state

--- ai-agents/langflow/terraform/lightsail.tf
+++ ai-agents/langflow/terraform/lightsail.tf
@@ -2,7 +2,7 @@
 # terraform/lightsail.tf
 ###############################################################################
 
-resource "aws_lightsail_certificate" "ai_insights_certificate" {
+resource "aws_lightsail_certificate" "langflow_certificate" {
   name        = "langflow-certificate"
   domain_name = "langflow.dodao.io"
 }
@@ -20,18 +20,18 @@ resource "aws_lightsail_container_service" "langflow" {
 
   public_domain_names {
     certificate {
-      certificate_name = aws_lightsail_certificate.ai_insights_certificate.name
+      certificate_name = aws_lightsail_certificate.langflow_certificate.name
       domain_names = [
-        "ai-insights.dodao.io",
+        "langflow.dodao.io",
         # maybe another domain name
       ]
     }
   }
-  private_registry_access {
-    ecr_image_puller_role {
-      is_active = true
-    }
-  }
+  # private_registry_access {
+  #   ecr_image_puller_role {
+  #     is_active = true
+  #   }
+  # }
 }
 
 # Once the service is created, we deploy a container to it
@@ -41,36 +41,40 @@ resource "aws_lightsail_container_service_deployment_version" "langflow_deployme
   container {
     # Must match the "name" from the container endpoint you define
     container_name = "langflow-container"
-    image = "${aws_ecr_repository.langflow.repository_url}:latest"
+    image = "langflowai/langflow:latest"
     # We can set environment variables for Langflow here
+    command = ["python", "-m", "langflow", "run"]
     environment = {
       LANGFLOW_AUTO_LOGIN         = "False"
       LANGFLOW_SUPERUSER          = var.langflow_superuser
       LANGFLOW_SUPERUSER_PASSWORD = var.langflow_superuser_password
       LANGFLOW_SECRET_KEY         = var.langflow_secret_key
-      LANGFLOW_DATABASE_URL = var.postgres_url
+      LANGFLOW_DATABASE_URL       = var.postgres_url
 
       # Optional: override host/port if you want to run on 0.0.0.0
-      LANGFLOW_HOST = "0.0.0.0"
-      LANGFLOW_PORT = "7860"
+      # LANGFLOW_HOST = "0.0.0.0"
+      # LANGFLOW_PORT = "7860"
     }
-    # ports = {
-    #   "7860" = "HTTP"
-    # }
-    # Or if you want to map port 80 to container 7860 externally:
     ports = {
-      "80" = "HTTP"
+      "7860" = "HTTP"
     }
+    # Or if you want to map port 80 to container 7860 externally:
+    # ports = {
+    #   "80" = "HTTP"
+    # }
   }
 
   public_endpoint {
-    container_name = "langflow"
+    container_name = "langflow-container"
     container_port = 7860
+
     health_check {
       healthy_threshold   = 2
       unhealthy_threshold = 2
-      timeout_seconds     = 2
-      interval_seconds    = 5
+      timeout_seconds     = 5
+      interval_seconds    = 10
+      path                = "/"
+      success_codes       = "200-499"
     }
   }
 }
