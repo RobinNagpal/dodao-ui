70ebbcc13bdee1552ff2328b84d9109251bd57bf
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2025-02-18 18:29:28 +0500

    fix debug report page

--- insights-ui/src/app/crowd-funding/debug/reports/[reportType]/page.tsx
+++ insights-ui/src/app/crowd-funding/debug/reports/[reportType]/page.tsx
@@ -1,66 +1,24 @@
-import { REPORT_TYPES_TO_DISPLAY, ReportInterface } from '@/types/project/project';
-import Button from '@dodao/web-core/components/core/buttons/Button';
-import Link from 'next/link';
+import ReportDebugPage from '@/components/projects/ReportDebugPage';
+import { ReportInterface } from '@/types/project/project';
+import getBaseUrl from '@dodao/web-core/utils/api/getBaseURL';
 import React from 'react';
 
-export default function DebugReportPage() {
-  // This is projectId -> report
-  const allReportsOfATypeForAllProjects: Record<string, ReportInterface> = {};
+export default async function DebugReportPage({ params }: { params: Promise<{ reportType: string }> }) {
+  const { reportType } = await params;
+
+  const res = await fetch(`${getBaseUrl()}/api/crowd-funding/debug/reports/${reportType}`);
+  const data = await res.json();
+
+  const reportsData: Record<string, ReportInterface> = {};
+  data.projects.forEach((project: { projectId: string; report: ReportInterface | null }) => {
+    if (project.report) {
+      reportsData[project.projectId] = project.report;
+    }
+  });
+
   return (
     <div>
-      <div>
-        <div>Choose a report to debug:</div>
-        {REPORT_TYPES_TO_DISPLAY.map((reportType) => {
-          return (
-            <Link href={`/crowd-funding/debug/reports/${reportType}`} key={reportType}>
-              <a>{reportType}</a>
-            </Link>
-          );
-        })}
-      </div>
-      <div className="w-full">
-        <Button className="float-right" onClick={() => console.log(allReportsOfATypeForAllProjects)}>
-          Retrigger For All Projects
-        </Button>
-      </div>
-      <div>
-        <table>
-          <tr>
-            <th>Project Id</th>
-            <th>Evaluation Items</th>
-          </tr>
-          {Object.entries(allReportsOfATypeForAllProjects).map(([projectId, report]) => {
-            return (
-              <tr key={projectId}>
-                <td>{projectId}</td>
-                <td>
-                  {report && report.performanceChecklist && report.performanceChecklist.length > 0 && (
-                    <>
-                      <dd className="text-color text-sm">
-                        {report.performanceChecklist?.length && (
-                          <ul className="list-disc mt-2">
-                            {report.performanceChecklist.map((item, index) => (
-                              <li key={index} className="mb-1 flex items-start">
-                                <span className="mr-2">{item.score === 1 ? '✅' : '❌'}</span>
-                                <span>{item.checklistItem}</span>
-                              </li>
-                            ))}
-                          </ul>
-                        )}
-                      </dd>
-                      <div>
-                        <Link target="_blank" href={`/crowd-funding/projects/${projectId}/reports/${reportType}`} className="link-color text-sm mt-4">
-                          See Full Report &rarr;
-                        </Link>
-                      </div>
-                    </>
-                  )}
-                </td>
-              </tr>
-            );
-          })}
-        </table>
-      </div>
+      <ReportDebugPage reportsData={reportsData} reportType={reportType} />
     </div>
   );
 }
--- insights-ui/src/app/crowd-funding/debug/reports/page.tsx
+++ insights-ui/src/app/crowd-funding/debug/reports/page.tsx
@@ -1,20 +1,21 @@
 import { REPORT_TYPES_TO_DISPLAY } from '@/types/project/project';
+import PageWrapper from '@dodao/web-core/components/core/page/PageWrapper';
 import Link from 'next/link';
 import React from 'react';
 
 export default function DebugReportsPage() {
   return (
-    <div>
-      <div>
+    <PageWrapper>
+      <div className="text-color">
         <div>Choose a report to debug:</div>
         {REPORT_TYPES_TO_DISPLAY.map((reportType) => {
           return (
             <Link href={`/crowd-funding/debug/reports/${reportType}`} key={reportType}>
-              <a>{reportType}</a>
+              <div className="link-color hover:underline">{reportType}</div>
             </Link>
           );
         })}
       </div>
-    </div>
+    </PageWrapper>
   );
 }
