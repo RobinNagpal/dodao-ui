c017c3343fe7a4f42373d6f9606a169f5917d943
robin <robinnagpal.tiet@gmail.com>
2025-02-19 16:26:52 -0500

    Fix footer

--- insights-ui/src/components/home-page/Footer.tsx
+++ insights-ui/src/components/home-page/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="relative bg-gray-800 pt-5 pb-20 sm:pt-14 sm:pb-32">
+    <footer className="relative bg-gray-800 pt-5 pb-8 sm:pt-14 sm:pb-16">
       <div className="relative text-center text-sm text-gray-400">
         <p>
           Copyright &copy; {new Date().getFullYear()} <strong className="text-white">KoalaGains.com</strong>. All rights reserved.
