b6f6709524d5d49ca4d4910f99795b3e3e97e1e1
robin <robinnagpal.tiet@gmail.com>
2025-02-22 14:26:15 -0500

    Cleanups and sector information

--- insights-ui/src/app/page.tsx
+++ insights-ui/src/app/page.tsx
@@ -6,24 +6,12 @@ import { Footer } from '@/components/home-page/Footer';
 import FromTheBlog from '@/components/home-page/FromTheBlog';
 import { Hero } from '@/components/home-page/Hero';
 import { getPostsData } from '@/util/blog-utils';
-import { CSSProperties } from 'react';
-
-const style: CSSProperties = {
-  '--primary-color': '#4F46E5', // Indigo-600 for primary actions
-  '--primary-text-color': '#ffffff', // Crisp white text on primary elements
-  '--bg-color': '#1F2937', // Gray-800 for the main background
-  '--text-color': '#D1D5DB', // Gray-300 for body text for good contrast
-  '--link-color': '#4F46E5', // Matching the primary color for links
-  '--heading-color': '#ffffff', // White for headings
-  '--border-color': '#374151', // Gray-700 for subtle borders
-  '--block-bg': '#374151', // A slightly lighter dark for block backgrounds
-  '--swiper-theme-color': '#4F46E5', // Consistent with the primary color for Swiper components
-} as CSSProperties;
+import { themeColors } from '@/util/theme-colors';
 
 export default async function Home() {
   const posts = await getPostsData();
   return (
-    <div style={{ ...style, backgroundColor: 'var(--bg-color)' }}>
+    <div style={{ ...themeColors, backgroundColor: 'var(--bg-color)' }}>
       <Hero />
       <Architecture />
       <AllInOnePlatform />
