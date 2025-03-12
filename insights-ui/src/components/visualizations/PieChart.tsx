42f4e876180bc252ee14d21cc032d97d28335617
DawoodMehmood <dawoodmehmood52222@gmail.com>
2025-03-12 20:19:45 +0500

    minor fixes

--- insights-ui/src/components/visualizations/PieChart.tsx
+++ insights-ui/src/components/visualizations/PieChart.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client';
+
 import { Pie } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend, ChartOptions, ChartData } from 'chart.js';
 
