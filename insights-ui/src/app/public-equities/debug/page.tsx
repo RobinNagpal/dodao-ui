f9ffa83a90f0b57894bbc5c5a3a39c0f6cb6b1c7
robin <robinnagpal.tiet@gmail.com>
2025-03-25 07:46:10 -0400

    cleanup

--- insights-ui/src/app/public-equities/debug/page.tsx
+++ insights-ui/src/app/public-equities/debug/page.tsx
@@ -1,5 +1,15 @@
-import DebugPublicEquitiesPage from '@/app/public-equities/debug/DebugPublicEquitiesPage';
+import TickersTable from '@/app/public-equities/debug/tickers/TickersTable';
+import PageWrapper from '@dodao/web-core/components/core/page/PageWrapper';
+import getBaseUrl from '@dodao/web-core/utils/api/getBaseURL';
+import { Ticker } from '@prisma/client';
 
-export default function DebugPage() {
-  return <DebugPublicEquitiesPage />;
+export default async function DebugPage() {
+  const response = await fetch(`${getBaseUrl()}/api/tickers`, { cache: 'no-cache' });
+  const tickers: Ticker[] = (await response.json()) as Ticker[];
+  return (
+    <PageWrapper>
+      <h1 className="text-3xl">Ticker</h1>
+      <TickersTable tickers={tickers} />
+    </PageWrapper>
+  );
 }
