1676fc613d4092971e0a49fb64a28ee5f8a6b1e2
robin <robinnagpal.tiet@gmail.com>
2025-03-25 10:31:55 -0400

    Show criteria link

--- insights-ui/src/app/public-equities/debug/page.tsx
+++ insights-ui/src/app/public-equities/debug/page.tsx
@@ -2,12 +2,23 @@ import TickersTable from '@/app/public-equities/debug/tickers/TickersTable';
 import PageWrapper from '@dodao/web-core/components/core/page/PageWrapper';
 import getBaseUrl from '@dodao/web-core/utils/api/getBaseURL';
 import { Ticker } from '@prisma/client';
+import Link from 'next/link';
 
 export default async function DebugPage() {
   const response = await fetch(`${getBaseUrl()}/api/tickers`, { cache: 'no-cache' });
   const tickers: Ticker[] = (await response.json()) as Ticker[];
   return (
     <PageWrapper>
+      <div className="my-16">
+        <h1>Criteria</h1>
+        <ul>
+          <ol>
+            <Link href="/public-equities/industry-group-criteria" className="link-color underline text-xl" target="_blank">
+              Criteria Table
+            </Link>
+          </ol>
+        </ul>
+      </div>
       <h1 className="text-3xl">Ticker</h1>
       <TickersTable tickers={tickers} />
     </PageWrapper>
