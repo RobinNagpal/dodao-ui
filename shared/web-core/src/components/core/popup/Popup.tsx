7b8ffb2131cb8499786f61d4ebf5a5bad9847248
DawoodMehmood <dawoodmehmood52222@gmail.com>
2025-04-11 22:58:21 +0500

    popup fix

--- shared/web-core/src/components/core/popup/Popup.tsx
+++ shared/web-core/src/components/core/popup/Popup.tsx
@@ -9,13 +9,10 @@ export default function Popup({ children, IconComponent }: PopupProps) {
   return (
     <Popover className="relative inline">
       <PopoverButton className="text-color focus:outline-none">
-        <IconComponent className="size-5" />
+        <IconComponent className="size-5 mt-1" />
       </PopoverButton>
 
-      <PopoverPanel
-        transition
-        className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
-      >
+      <PopoverPanel className="absolute z-10 p-4" anchor="top">
         <div className="w-screen max-w-sm flex-auto rounded-3xl background-color p-4 text-sm/6 ring-1 text-left">{children}</div>
       </PopoverPanel>
     </Popover>
