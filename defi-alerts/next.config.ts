6e3b9d44983a25ff9c27fd56ff2cf256383a5e67
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2025-04-22 23:34:26 +0500

    config

--- defi-alerts/next.config.ts
+++ defi-alerts/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    silenceDeprecations: ["legacy-js-api"],
+    // This allows you to use variables defined `"app/styles/variables.scss` to be used in scss module files without
+    // importing them explicitly
+    includePaths: ["./src"],
+  },
 };
 
 export default nextConfig;
