fc19a4a65aaeffa8176c455890154884530b429b
robin <robinnagpal.tiet@gmail.com>
2025-05-13 08:37:58 -0400

    Add defi-alerts project

--- rubrics-ui/src/app/api/helpers/space/getSpaceById.ts
+++
@@ -1,6 +0,0 @@
-import { prisma } from '@/prisma';
-import { RubricSpace } from '@prisma/client';
-
-export async function getSpaceById(spaceId: string): Promise<RubricSpace> {
-  return prisma.rubricSpace.findUniqueOrThrow({ where: { id: spaceId } });
-}
