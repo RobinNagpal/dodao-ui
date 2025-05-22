2beb69cedc258311ef4597b507fb83c1493b6fec
robin <robinnagpal.tiet@gmail.com>
2025-05-22 14:21:48 -0400

    Fix errors

--- defi-alerts/src/app/layout.tsx
+++ defi-alerts/src/app/layout.tsx
@@ -5,6 +5,8 @@ import { NotificationProvider } from '@dodao/web-core/ui/contexts/NotificationCo
 import type { Metadata } from 'next';
 import { getServerSession } from 'next-auth';
 import { Inter, JetBrains_Mono } from 'next/font/google';
+import { redirect } from 'next/navigation';
+import { headers } from 'next/headers';
 import './globals.scss';
 
 const inter = Inter({
@@ -29,6 +31,19 @@ export default async function RootLayout({
 }>) {
   const session = await getServerSession();
 
+  // Redirect to login if not logged in
+  if (!session) {
+    // We need to exclude the login page to avoid redirect loops
+    const headersList = await headers();
+    const path = headersList?.get('x-current-path') || '';
+    console.log('path', path);
+
+    if (path !== '/login' && path !== '/auth/email/verify') {
+      console.log(`User is not logged in, redirecting to login page: ${path}`);
+      redirect('/login');
+    }
+  }
+
   return (
     <html lang="en">
       <body className={`${inter.variable} ${jetbrainsMono.variable} antialiased`}>
@@ -37,7 +52,6 @@ export default async function RootLayout({
             <>
               <NotificationWrapper />
               <Navbar />
-              {children}
               {children}
             </>
           </NotificationProvider>
