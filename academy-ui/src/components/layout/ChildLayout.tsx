f78f363a77ee96c423a970fc271693d05b298e52
robin <robinnagpal.tiet@gmail.com>
2025-05-29 15:14:34 -0400

    rollback unnecessary change

--- academy-ui/src/components/layout/ChildLayout.tsx
+++ academy-ui/src/components/layout/ChildLayout.tsx
@@ -11,7 +11,6 @@ import { getGTagId } from '@dodao/web-core/utils/analytics/getGTagId';
 import { useNavigationEvent } from '@dodao/web-core/utils/analytics/useNavigationEvent';
 import { getAuthenticatedApolloClient } from '@dodao/web-core/utils/apolloClient';
 import { setDoDAOTokenInLocalStorage } from '@dodao/web-core/utils/auth/setDoDAOTokenInLocalStorage';
-import CustomApolloProvider from 'defi-alerts/src/providers/ApolloProvider';
 import { SessionProvider } from 'next-auth/react';
 import { useEffect, useMemo } from 'react';
 import ReactGA from 'react-ga4';
@@ -61,9 +60,7 @@ export function ChildLayout({
   return (
     <Web3ReactProviderWrapper>
       <SessionProvider session={session}>
-        <CustomApolloProvider>
-          <BasePage space={space!}>{children}</BasePage>
-        </CustomApolloProvider>
+        <BasePage space={space!}>{children}</BasePage>
       </SessionProvider>
       <NotificationWrapper />
     </Web3ReactProviderWrapper>
