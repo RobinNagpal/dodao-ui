d938bf57b271a00358433bbfb11a876239b6044d
Hassaan-Qaisar <hassaanqaisar2@gmail.com>
2025-06-05 16:08:16 +0500

    added unichain as well in morpho

--- defi-alerts/src/shared/migrator/morpho/config.ts
+++ defi-alerts/src/shared/migrator/morpho/config.ts
@@ -1,3 +1,3 @@
-import { base, mainnet, polygon } from 'wagmi/chains';
+import { base, mainnet, polygon, unichain } from 'wagmi/chains';
 
-export const MORPHO_CHAIN_IDS = [mainnet.id, base.id, polygon.id] as const;
+export const MORPHO_CHAIN_IDS = [mainnet.id, base.id, polygon.id, unichain.id] as const;
