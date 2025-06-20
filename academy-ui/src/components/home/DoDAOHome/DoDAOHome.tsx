3eed3080513204a7c0280f88f28d72ea847f094d
Hassaan-Qaisar <hassaanqaisar2@gmail.com>
2025-06-20 20:23:22 +0500

    added links in footer

--- academy-ui/src/components/home/DoDAOHome/DoDAOHome.tsx
+++ academy-ui/src/components/home/DoDAOHome/DoDAOHome.tsx
@@ -3,10 +3,7 @@ import CoreOfferings from './components/CoreOfferings';
 import DoDAOHelpButton from './components/DoDAOHelpButton';
 import DoDAOHomeHero from './components/DoDAOHomeHero';
 import DoDAOProducts from './components/DoDAOProducts';
-import { Education } from './components/Education';
 import { Footer } from './components/Footer';
-import Research from './components/Research';
-import Services from './components/Services';
 import { TrustedBy } from './components/TrustedBy';
 
 export default function DoDAOHome() {
