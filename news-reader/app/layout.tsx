e1138ff212b75d4c77239d55905a356fa144a9d9
robin <robinnagpal.tiet@gmail.com>
2025-08-08 13:44:54 -0400

    fix some tailwind errors

--- news-reader/app/layout.tsx
+++ news-reader/app/layout.tsx
@@ -1,18 +1,19 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import './globals.css'
+import type { Metadata } from 'next';
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+import './globals.scss';
+import './plugins.css';
 
 export const metadata: Metadata = {
   title: 'v0 App',
   description: 'Created with v0',
   generator: 'v0.dev',
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -27,5 +28,5 @@ html {
       </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
