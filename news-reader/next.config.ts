fa3ad2d6cda8a6b2fca8aabab04c4ab7fb0ceb2e
robin <robinnagpal.tiet@gmail.com>
2025-08-08 19:46:01 -0400

    fix some tailwind errors

---
+++ news-reader/next.config.ts
@@ -0,0 +1,24 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  sassOptions: {
+    implementation: 'sass',
+    silenceDeprecations: ['legacy-js-api'],
+    // This allows you to use variables defined `"app/styles/variables.scss` to be used in scss module files without
+    // importing them explicitly
+    includePaths: ['./src'],
+  },
+  images: {
+    domains: ['raw.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        pathname: '/RobinNagpal/dodao-ui/refs/heads/main/insights-ui/blogs',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
