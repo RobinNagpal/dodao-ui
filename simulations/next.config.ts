3ffd2616a006659b0341371ab80c7e94162975b5
Hassaan-Qaisar <hassaanqaisar2@gmail.com>
2025-08-12 19:22:55 +0500

    initialize the project

---
+++ simulations/next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  sassOptions: {
+    silenceDeprecations: ['legacy-js-api'],
+    // This allows you to use variables defined `"app/styles/variables.scss` to be used in scss module files without
+    // importing them explicitly
+    includePaths: ['./src'],
+  },
+};
+
+export default nextConfig;
