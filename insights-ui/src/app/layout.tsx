776f99b68076f6aafb7f059e5d04a72534ed5f6d
robin <robinnagpal.tiet@gmail.com>
2025-08-18 08:57:37 -0400

    Add hotjar

--- insights-ui/src/app/layout.tsx
+++ insights-ui/src/app/layout.tsx
@@ -82,6 +82,17 @@ export default function RootLayout({
  
           gtag('config', 'G-MYQG66ESX3');
         `}
+
+            <Script id="hotjar" strategy="afterInteractive">
+              {`(function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:6496201,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+            </Script>
           </Script>
         </>
       </body>
