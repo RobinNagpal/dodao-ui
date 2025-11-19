f81b53afc951f165507bfe2b9a9b75f3759e8b71
Hassaan-Qaisar <hassaanqaisar2@gmail.com>
2025-11-19 15:52:30 +0500

    minor changes

--- insights-ui/src/app/ClarityComponent.tsx
+++ insights-ui/src/app/ClarityComponent.tsx
@@ -4,11 +4,13 @@ import { useEffect } from 'react';
 import Clarity from '@microsoft/clarity';
 import { PROD_HOST } from './LogRocketComponent';
 
+const CLARITY_PROJECT_ID = 'u82d3rnsxw';
+
 export default function ClarityComponent(): null {
   useEffect(() => {
     // Only initialize on production
     if (typeof window !== 'undefined' && window.location.hostname === PROD_HOST) {
-      Clarity.init(process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID as string);
+      Clarity.init(CLARITY_PROJECT_ID);
     }
   }, []);
 
